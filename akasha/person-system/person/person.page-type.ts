5f80acb5a46955bca3d0247e67f5cd56e60a257b
Akasha <akasha@alanwalton.com>
2026-08-30 12:23:43 -0600

    A person states the number they are reached at

--- akasha/person-system/person/person.page-type.ts
+++ akasha/person-system/person/person.page-type.ts
@@ -1,9 +1,11 @@
 import type { Domain } from "../../domain-system/domain/domain.page-type.ts"
 import type { PageType } from "../../pages-system/page-type/page-type.page-type.ts"
 import type { AnsweredBy } from "./properties/answered-by.relation-property.ts"
+import type { Phone } from "./properties/phone.phone-number-property.ts"
 
 export type Person = Domain & {
   answeredBy: AnsweredBy
+  phone?: Phone
 }
 
 export const person = {
@@ -13,6 +15,9 @@ export const person = {
   definition: "a human this system reaches",
   pluralSlug: "people",
   extendsSlug: "page-type/domain",
-  partSlugs: ["relation-property/answered-by"],
-  properties: [{ pagePropertySlug: "answered-by", required: true, many: false }],
+  partSlugs: ["phone-number-property/phone", "relation-property/answered-by"],
+  properties: [
+    { pagePropertySlug: "answered-by", required: true, many: false },
+    { pagePropertySlug: "phone", required: false, many: false },
+  ],
 } as const satisfies PageType
---
+++ akasha/person-system/person/properties/phone.phone-number-property.ts
@@ -0,0 +1,11 @@
+import type { PhoneNumberProperty } from "../../../pages-system/phone-number-property/phone-number-property.page-type.ts"
+
+export type Phone = string
+
+export const phone = {
+  id: "01a053e9-be09-72ca-9b1b-117a48a04491",
+  pageTypeSlug: "phone-number-property",
+  slug: "phone",
+  propertySlug: "phone",
+  definition: "the number a person is reached at",
+} as const satisfies PhoneNumberProperty
