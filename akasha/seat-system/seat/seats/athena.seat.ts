934e1e366bf1f0f2b8d42caa092118c9845682aa
Akasha <akasha@alanwalton.com>
2026-08-30 16:01:18 -0600

    athena: the seat stands in akasha as what it states

--- akasha/seat-system/seat/seats/athena.seat.ts
+++ akasha/seat-system/seat/seats/athena.seat.ts
@@ -1,7 +1,7 @@
 import type { Seat } from "../seat.page-type.ts"
 
 export const athena = {
-  id: "01a054b0-184a-7000-a86b-c500ebce8ea0",
+  id: "01a054b0-dd1b-7000-9df3-226508a74c75",
   pageTypeSlug: "seat",
   slug: "athena",
   personaSlug: "athena",
