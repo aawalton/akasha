7d3f1cf7278b81b0a72cbd36f422499173d2ba3f
Akasha <akasha@alanwalton.com>
2026-08-31 14:42:42 -0600

    akasha-b1d2e3f4a5b6c7d8e: a subagent states the kind it was dispatched as

---
+++ akasha/seat-system/subagent/subagents/akasha-b1d2e3f4a5b6c7d8e.subagent.ts
@@ -0,0 +1,9 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const akashaB1d2e3f4a5b6c7d8e = {
+  id: "01a0598f-54b5-7d16-bd47-dbda0c036f82",
+  pageTypeSlug: "subagent",
+  slug: "akasha-b1d2e3f4a5b6c7d8e",
+  principalSeatName: "akasha",
+  dispatchedAs: "Explore",
+} as const satisfies Subagent
