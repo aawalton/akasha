95ec8c844630fbba58ab77f4c939f4e3c6be8bb4
Akasha <akasha@alanwalton.com>
2026-08-31 17:52:58 -0600

    thea-a63d2457d563387e7: a subagent states the kind it was dispatched as

---
+++ akasha/seat-system/subagent/subagents/thea-a63d2457d563387e7.subagent.ts
@@ -0,0 +1,10 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const theaA63d2457d563387e7 = {
+  id: "01a05a3d-89ba-7681-a342-45c89979599d",
+  pageTypeSlug: "subagent",
+  slug: "thea-a63d2457d563387e7",
+  principalSeatName: "thea",
+  assignmentSlug: "workspace-package/checks-system",
+  dispatchedAs: "Explore",
+} as const satisfies Subagent
