be673c60ff96f0471dd7b3b50af00db00b0a31ff
Akasha <akasha@alanwalton.com>
2026-08-31 15:40:23 -0600

    akasha-f00dcafe0badbead3: probing how many rounds a cold writer needs

---
+++ akasha/seat-system/subagent/subagents/akasha-f00dcafe0badbead3.subagent.ts
@@ -0,0 +1,10 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const akashaF00dcafe0badbead3 = {
+  id: "01a059c4-2780-7b0e-9823-109189014411",
+  pageTypeSlug: "subagent",
+  slug: "akasha-f00dcafe0badbead3",
+  principalSeatName: "akasha",
+  assignmentSlug: "initiative/akasha-subagent-reading",
+  dispatchedAs: "Explore",
+} as const satisfies Subagent
