50179a6528da68524a3168bded3b196b8deb9a76
Akasha <akasha@alanwalton.com>
2026-09-01 16:56:41 -0600

    the name-matching module sits under modules
    Checks-bypassed: no check ran: this landing was made by a program rather than by an agent

--- akasha/pages-system/name-format/modules/format-reaching/format-reaching.module.code.ts
+++ akasha/pages-system/name-format/modules/format-reaching/format-reaching.module.code.ts
@@ -4,7 +4,7 @@ import { listedAt } from "@akasha/indexes"
 import { slugIn } from "../../../page/page-address/page-address.module.code.ts"
 import { exportedAs } from "../../../page/page-export-name/page-export-name.module.code.ts"
 import { besideAt } from "../../../page/page-file-name/page-file-name.module.code.ts"
-import type { Matching } from "../../name-matching/name-matching.module.code.ts"
+import type { Matching } from "../name-matching/name-matching.module.code.ts"
 
 const NAME_FORMAT = "name-format"
 
