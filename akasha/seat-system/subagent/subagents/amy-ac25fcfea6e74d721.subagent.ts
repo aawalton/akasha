73c9dc887792355d87f2047948718e4763618991
Akasha <akasha@alanwalton.com>
2026-08-31 18:26:53 -0600

    amy-ac25fcfea6e74d721: a subagent states the kind it was dispatched as

---
+++ akasha/seat-system/subagent/subagents/amy-ac25fcfea6e74d721.subagent.ts
@@ -0,0 +1,10 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const amyAc25fcfea6e74d721 = {
+  id: "01a05a5c-95d7-7ec2-bda5-a17ea7c88ae8",
+  pageTypeSlug: "subagent",
+  slug: "amy-ac25fcfea6e74d721",
+  principalSeatName: "amy",
+  assignmentSlug: "initiative/amy-jenny-unreviewed-widget",
+  dispatchedAs: "Explore",
+} as const satisfies Subagent
