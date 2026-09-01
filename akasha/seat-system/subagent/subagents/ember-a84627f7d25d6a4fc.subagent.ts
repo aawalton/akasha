8421515c6887b378693e49021749c0c441ab6a89
Akasha <akasha@alanwalton.com>
2026-09-01 09:18:13 -0600

    ember-a84627f7d25d6a4fc: a subagent states the kind it was dispatched as
    Checks-bypassed: no check ran: this landing was made by a program rather than by an agent

---
+++ akasha/seat-system/subagent/subagents/ember-a84627f7d25d6a4fc.subagent.ts
@@ -0,0 +1,10 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const emberA84627f7d25d6a4fc = {
+  id: "01a05d8c-a374-73b1-9058-9fee710d2c68",
+  pageTypeSlug: "subagent",
+  slug: "ember-a84627f7d25d6a4fc",
+  principalSeatName: "ember",
+  assignmentSlug: "domain/temper",
+  dispatchedAs: "Explore",
+} as const satisfies Subagent
