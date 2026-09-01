961dd93db3da41b3733bf0c041dfb3849abef167
Akasha <akasha@alanwalton.com>
2026-09-01 08:40:26 -0600

    rename `FolderShapeStanding` to `Stage`

--- akasha/code-system/shape-standing/shape-standing.module.code.ts
+++ akasha/code-system/shape-standing/shape-standing.module.code.ts
@@ -4,8 +4,8 @@ export const CODED = "coded"
 
 export const ENFORCED = "enforced"
 
-export type FolderShapeStanding = typeof HYPOTHESIS | typeof CODED | typeof ENFORCED
+export type Stage = typeof HYPOTHESIS | typeof CODED | typeof ENFORCED
 
-export function standingOfShape(_slug: string, _root?: string): FolderShapeStanding {
+export function standingOfShape(_slug: string, _root?: string): Stage {
   return HYPOTHESIS
 }
