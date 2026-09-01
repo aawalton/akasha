5abc80a1bb6c4a3b61aec779108e859113a72bfa
Akasha <akasha@alanwalton.com>
2026-08-31 18:30:28 -0600

    amy-a516cf8cf36b39e5e: a subagent states the kind it was dispatched as

---
+++ akasha/seat-system/subagent/subagents/amy-a516cf8cf36b39e5e.subagent.ts
@@ -0,0 +1,10 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const amyA516cf8cf36b39e5e = {
+  id: "01a05a5f-d5e9-707f-ac20-7e8b12558f87",
+  pageTypeSlug: "subagent",
+  slug: "amy-a516cf8cf36b39e5e",
+  principalSeatName: "amy",
+  assignmentSlug: "initiative/amy-jenny-unreviewed-widget",
+  dispatchedAs: "Explore",
+} as const satisfies Subagent
