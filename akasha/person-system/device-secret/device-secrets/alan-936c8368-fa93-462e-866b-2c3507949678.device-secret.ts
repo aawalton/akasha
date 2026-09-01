0e557db6886e9152dc61d61634c9ff1b8a0f5891
Akasha <akasha@alanwalton.com>
2026-08-31 23:10:38 -0600

    write akasha/person-system/device-secret/device-secrets/alan-936c8368-fa93-462e-866b-2c3507949678.device-secret.ts

---
+++ akasha/person-system/device-secret/device-secrets/alan-936c8368-fa93-462e-866b-2c3507949678.device-secret.ts
@@ -0,0 +1,10 @@
+import type { DeviceSecret } from "../device-secret.page-type.ts"
+
+export const alan936c8368Fa93462e866b2c3507949678 = {
+  id: "01a05b5b-b5e0-7cdf-b1af-17f27e853568",
+  pageTypeSlug: "device-secret",
+  slug: "alan-936c8368-fa93-462e-866b-2c3507949678",
+  userId: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
+  deviceId: "936C8368-FA93-462E-866B-2C3507949678",
+  secretHash: "5910b3e11f5e55187713b47f5813c7f3ea9c379b7004c15d2b17dff6350680f7",
+} as const satisfies DeviceSecret
