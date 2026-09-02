a3117c5ae59c743c827ed78f6577f7bf506ba3da
Akasha <akasha@alanwalton.com>
2026-09-02 14:23:07 -0600

    seed a fault to prove the build comparison sees a change

--- akasha/temper/temper-catalog-addon/catalog-constants/catalog-constants.module.code.ts
+++ akasha/temper/temper-catalog-addon/catalog-constants/catalog-constants.module.code.ts
@@ -2,6 +2,6 @@ export const ADDON_NAME = "TemperCatalog"
 
 export const AUTO_START_DELAY = 10000
 
-export const DOMAIN_DELAY = 100
+export const DOMAIN_DELAY = 101
 
 export const DOMAIN_TIMEOUT = 60000
