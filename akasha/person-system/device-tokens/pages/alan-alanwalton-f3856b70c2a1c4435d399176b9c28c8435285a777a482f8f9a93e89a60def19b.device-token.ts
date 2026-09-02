713ddf0c2fa006e3cfeccc81b9087165574015fe
device token registration <push@alanwalton.com>
2026-09-02 07:17:08 -0600

    alan is reached on a alanwalton device
    Written-by: device token registration <push@alanwalton.com>

---
+++ akasha/person-system/device-tokens/pages/alan-alanwalton-f3856b70c2a1c4435d399176b9c28c8435285a777a482f8f9a93e89a60def19b.device-token.ts
@@ -0,0 +1,10 @@
+import type { DeviceToken } from "../device-token.page-type.ts"
+
+export const alanAlanwaltonF3856b70c2a1c4435d399176b9c28c8435285a777a482f8f9a93e89a60def19b = {
+  id: "01a06244-2290-76f7-8b58-355085abab49",
+  pageTypeSlug: "device-token",
+  slug: "alan-alanwalton-f3856b70c2a1c4435d399176b9c28c8435285a777a482f8f9a93e89a60def19b",
+  personSlug: "alan",
+  iosAppSlug: "alanwalton",
+  token: "F3856B70C2A1C4435D399176B9C28C8435285A777A482F8F9A93E89A60DEF19B",
+} as const satisfies DeviceToken
