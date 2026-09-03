8465c8fd5e94260f9d84218ecb4851befab5fb19
Akasha <akasha@alanwalton.com>
2026-09-03 10:57:48 -0600

    stop importing the unread text count

--- tools/lib/inbox-tracking/poll.ts
+++ tools/lib/inbox-tracking/poll.ts
@@ -1,6 +1,5 @@
 import { makeGmailClient } from "@akasha/google-email/client"
 import { INBOX_LABEL, listMessages } from "@akasha/google-email/messages"
-import { fetchUnreadCount } from "@akasha/imessage/remote"
 import { askComposed } from "../page-query-client.ts"
 import type { InboxKey } from "./keys.ts"
 
