fdc98f0e43d07958a7222e57f2cb4d79d048c662
error capture <errors@alanwalton.com>
2026-09-03 19:24:01 -0600

    alanwalton-3bae0412828f18d8 was met for the first time
    Written-by: error capture <errors@alanwalton.com>

---
+++ akasha/errors-core/errors/pages/alanwalton-3bae0412828f18d8.error.ts
@@ -0,0 +1,15 @@
+import type { Error } from "../error.page-type.ts"
+
+export const alanwalton3bae0412828f18d8 = {
+  id: "01a06a03-fbc1-7dd2-bcfd-bce724fb3436",
+  pageTypeSlug: "error",
+  slug: "alanwalton-3bae0412828f18d8",
+  fingerprint: "3bae0412828f18d8",
+  app: "alanwalton",
+  kind: "react-render",
+  message: "Unexpected Server Error",
+  url: "https://alanwalton.com/home",
+  userAgent:
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/149.0.7827.55 Safari/537.36",
+  firstSeenAt: "2026-09-04T01:24:00.266Z",
+} as const satisfies Error
