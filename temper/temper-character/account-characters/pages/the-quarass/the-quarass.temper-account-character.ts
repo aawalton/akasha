34cb4f5743e3da8c8ec0317df00542e906ca92f7
pages-access <pages-access@alanwalton.com>
2026-09-04 07:18:15 -0600

    upsertPage(temper-account-character): the-quarass
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-character/account-characters/pages/the-quarass/the-quarass.temper-account-character.ts
+++ temper/temper-character/account-characters/pages/the-quarass/the-quarass.temper-account-character.ts
@@ -5,9 +5,9 @@ export const theQuarass = {
   pageTypeSlug: "temper-account-character",
   slug: "the-quarass",
   title: "The Quarass",
-  firstName: "The Quarass",
-  esoCharacterId: "8796093041077793",
-  accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   displayOrder: 6,
   completion: "json",
+  esoCharacterId: "8796093041077793",
+  accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
+  firstName: "The Quarass",
 } as const satisfies TemperAccountCharacter
