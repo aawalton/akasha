20879b25c0227eab76a997f42c80da0d96c29b62
pages-access <pages-access@alanwalton.com>
2026-09-04 07:50:36 -0600

    patchPage(temper-catalog-domain): trait-research
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/trait-research.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/trait-research.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const traitResearch = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-04T13:33:59.674Z",
+  capturedAt: "2026-09-04T13:50:11.672Z",
   generatorRanForVersion: "eso.live.11.3.6.3240040",
   generatorRanForManifestApiVersion: 101049,
   dormant: true,
