7511a3682b8c8e8bac12f3a0afddbe647551e443
pages-access <pages-access@alanwalton.com>
2026-09-04 07:18:24 -0600

    upsertPage(temper-account-character): the-death-of-chains
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-character/account-characters/pages/the-death-of-chains/the-death-of-chains.temper-account-character.ts
+++ temper/temper-character/account-characters/pages/the-death-of-chains/the-death-of-chains.temper-account-character.ts
@@ -5,9 +5,9 @@ export const theDeathOfChains = {
   pageTypeSlug: "temper-account-character",
   slug: "the-death-of-chains",
   title: "The Death of Chains",
-  firstName: "The Death of Chains",
-  esoCharacterId: "8796093072550001",
-  accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   displayOrder: 20,
   completion: "json",
+  esoCharacterId: "8796093072550001",
+  accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
+  firstName: "The Death of Chains",
 } as const satisfies TemperAccountCharacter
