af0ecdd4856102e85d2bbe9105ea7ed3e1d916da
01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>
2026-09-05 13:10:54 -0600

    upsertPages(temper-inventory-rule): rule-2524af23
    Written-by: 01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>

--- temper/temper-progress/inventory-rules/pages/rule-2524af23/rule-2524af23.temper-inventory-rule.ts
+++ temper/temper-progress/inventory-rules/pages/rule-2524af23/rule-2524af23.temper-inventory-rule.ts
@@ -4,11 +4,11 @@ export const rule2524af23 = {
   id: "01a0728a-d6fd-79c4-b3f0-9c3ec3319faa",
   pageTypeSlug: "temper-inventory-rule",
   slug: "rule-2524af23",
+  destination: "character:8796093022338107",
   accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   categoryId: "survey-reports",
   displayOrder: 48,
   action: "move-to",
   active: true,
   updatedAt: "2026-06-02T20:31:51.801Z",
-  destination: "character:8796093022338107",
 } as const satisfies TemperInventoryRule
