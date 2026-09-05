d47ada4267c7fed27033e38140f89400be26193f
Ryn <ryn@alanwalton.com>
2026-09-05 11:38:27 -0600

    name the field in temper-inventory-buy-rule-update

--- temper/temper-commands/temper-inventory-buy-rule-update/temper-inventory-buy-rule-update.command.ts
+++ temper/temper-commands/temper-inventory-buy-rule-update/temper-inventory-buy-rule-update.command.ts
@@ -29,7 +29,7 @@ export const temperInventoryBuyRuleUpdate = {
     },
     {
       invariantKind: "departure",
-      statement: "A field the call names nothing for is left as it was.",
+      statement: "A field the call names nothing for is left as that field was.",
     },
     {
       invariantKind: "departure",
