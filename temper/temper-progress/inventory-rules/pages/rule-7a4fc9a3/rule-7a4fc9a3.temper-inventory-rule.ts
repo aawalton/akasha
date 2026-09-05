37b52fa07414438ec33f440f3f1d227fec357cd4
01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>
2026-09-05 13:07:13 -0600

    upsertPages(temper-inventory-rule): rule-7a4fc9a3
    Written-by: 01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>

--- temper/temper-progress/inventory-rules/pages/rule-7a4fc9a3/rule-7a4fc9a3.temper-inventory-rule.ts
+++ temper/temper-progress/inventory-rules/pages/rule-7a4fc9a3/rule-7a4fc9a3.temper-inventory-rule.ts
@@ -4,11 +4,11 @@ export const rule7a4fc9a3 = {
   id: "01a0728a-f56d-7b49-9a1f-ba92c867859d",
   pageTypeSlug: "temper-inventory-rule",
   slug: "rule-7a4fc9a3",
+  destination: "house-storage:4677",
   accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   categoryId: "crown-repair-kits",
   displayOrder: 2,
   action: "move-to",
   active: true,
   updatedAt: "2026-06-02T19:27:29.381Z",
-  destination: "house-storage:4677",
 } as const satisfies TemperInventoryRule
