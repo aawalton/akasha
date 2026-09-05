627a9770d0ce25bf6fe4d2f9921a88d85a89c466
01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>
2026-09-05 13:10:56 -0600

    upsertPages(temper-inventory-rule): rule-2ff0574b
    Written-by: 01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>

--- temper/temper-progress/inventory-rules/pages/rule-2ff0574b/rule-2ff0574b.temper-inventory-rule.ts
+++ temper/temper-progress/inventory-rules/pages/rule-2ff0574b/rule-2ff0574b.temper-inventory-rule.ts
@@ -4,11 +4,11 @@ export const rule2ff0574b = {
   id: "01a0728a-d6fe-7ddf-82fb-edd91ffff61f",
   pageTypeSlug: "temper-inventory-rule",
   slug: "rule-2ff0574b",
+  destination: "character:8796093022338107",
   accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   categoryId: "treasure-maps",
   displayOrder: 49,
   action: "move-to",
   active: true,
   updatedAt: "2026-06-02T20:15:45.094Z",
-  destination: "character:8796093022338107",
 } as const satisfies TemperInventoryRule
