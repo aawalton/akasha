65b5c14d44c8be262a265ba88425f2d37c6e1b17
Ryn <ryn@alanwalton.com>
2026-09-05 11:07:04 -0600

    name the disks in node

--- infrastructure/node.domain.ts
+++ infrastructure/node.domain.ts
@@ -13,7 +13,7 @@ export const node = {
     },
     {
       invariantKind: "departure",
-      statement: "A node's disks are matched by what they are rather than by device path.",
+      statement: "A node's disks are matched by what those disks are rather than by device path.",
     },
     {
       invariantKind: "departure",
