3a08782a4f0c117267c6485b6fcef0c93be3e7f0
01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>
2026-09-05 13:07:51 -0600

    upsertPages(temper-inventory-rule): rule-118d98dd
    Written-by: 01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>

--- temper/temper-progress/inventory-rules/pages/rule-118d98dd/rule-118d98dd.temper-inventory-rule.ts
+++ temper/temper-progress/inventory-rules/pages/rule-118d98dd/rule-118d98dd.temper-inventory-rule.ts
@@ -4,11 +4,11 @@ export const rule118d98dd = {
   id: "01a0728a-d6fd-73da-9111-6558285e3e83",
   pageTypeSlug: "temper-inventory-rule",
   slug: "rule-118d98dd",
+  destination: "character:8796093022338107",
   accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   categoryId: "container-stackable",
   displayOrder: 8,
   action: "move-to",
   active: true,
   updatedAt: "2026-06-02T20:27:33.382Z",
-  destination: "character:8796093022338107",
 } as const satisfies TemperInventoryRule
