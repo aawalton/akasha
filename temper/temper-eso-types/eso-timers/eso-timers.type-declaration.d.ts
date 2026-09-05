deb391d4e9e5b0f8cbe7ab165f3f90fa2387d5bd
Awen <awen@alanwalton.com>
2026-09-05 12:26:40 -0600

    the patch this agent drafted

--- temper/temper-eso-types/eso-timers/eso-timers.type-declaration.d.ts
+++ temper/temper-eso-types/eso-timers/eso-timers.type-declaration.d.ts
@@ -2,8 +2,8 @@ declare function setTimeout(this: void, fn: (this: void) => void, ms: number): n
 
 declare function setInterval(this: void, fn: (this: void) => void, ms: number): number
 
-declare const clearTimeout: (this: void, handle: number) => void
+declare function clearTimeout(this: void, handle: number): void
 
-declare const clearInterval: (this: void, handle: number) => void
+declare function clearInterval(this: void, handle: number): void
 
-declare const queueMicrotask: (this: void, fn: (this: void) => void) => void
+declare function queueMicrotask(this: void, fn: (this: void) => void): void
