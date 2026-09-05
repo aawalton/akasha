3b3e236b675ba1b92634577a16b26cde180977fd
01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>
2026-09-05 13:07:09 -0600

    upsertPages(temper-inventory-rule): rule-3535f831
    Written-by: 01a06c2c-aad6-7000-8795-883a8f1dbdc9 <01a06c2c-aad6-7000-8795-883a8f1dbdc9@alanwalton.com>

--- temper/temper-progress/inventory-rules/pages/rule-3535f831/rule-3535f831.temper-inventory-rule.ts
+++ temper/temper-progress/inventory-rules/pages/rule-3535f831/rule-3535f831.temper-inventory-rule.ts
@@ -4,11 +4,11 @@ export const rule3535f831 = {
   id: "01a0728a-d6fe-7f6d-ae7a-d57038b0e5ee",
   pageTypeSlug: "temper-inventory-rule",
   slug: "rule-3535f831",
+  destination: "house-storage:4677",
   accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   categoryId: "crown-items",
   displayOrder: 1,
   action: "move-to",
   active: true,
   updatedAt: "2026-06-02T19:27:28.980Z",
-  destination: "house-storage:4677",
 } as const satisfies TemperInventoryRule
