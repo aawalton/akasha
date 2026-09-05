7a17df06a13d3ecfa6fbe4b34580288a04dadc28
Astra <astra@alanwalton.com>
2026-09-05 11:59:08 -0600

    the workspace package beside a page type is slugged the page type's slug

--- akasha.domain.ts
+++ akasha.domain.ts
@@ -19,7 +19,7 @@ export const akasha = {
     "workspace-package/git",
     "workspace-package/shell",
     "workspace-package/pages",
-    "workspace-package/domains",
+    "workspace-package/domain",
     "workspace-package/graph",
     "domain/type",
     "domain/change",
--- domains/domain.workspace-package.ts
+++ domains/domain.workspace-package.ts
@@ -1,9 +1,9 @@
 import type { WorkspacePackage } from "@akasha/code-system/workspace-package"
 
-export const domains = {
+export const domain = {
   id: "01a04a26-9105-7001-a1cc-60a031152982",
   pageTypeSlug: "workspace-package",
-  slug: "domains",
+  slug: "domain",
   definition: "how we define how things should be",
   manifest: "json",
   partSlugs: [
