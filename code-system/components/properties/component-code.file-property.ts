4afe00126e7bc8ed3b15285c311ced4aa610003b
Ryn <ryn@alanwalton.com>
2026-09-05 11:42:42 -0600

    name the code in component-code

--- code-system/components/properties/component-code.file-property.ts
+++ code-system/components/properties/component-code.file-property.ts
@@ -11,7 +11,7 @@ export const componentCode = {
   invariants: [
     {
       invariantKind: "departure",
-      statement: "This code is written in TSX because it draws something for a browser.",
+      statement: "This code is written in TSX because this code draws something for a browser.",
     },
   ],
 } as const satisfies FileProperty
