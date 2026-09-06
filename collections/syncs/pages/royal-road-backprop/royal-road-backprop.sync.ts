b0517b8a1f07c58c22121d6bedb3219218cfcfd8
Vera <vera@alanwalton.com>
2026-09-06 05:07:55 -0600

    the patch this agent drafted

--- collections/syncs/pages/royal-road-backprop/royal-road-backprop.sync.ts
+++ collections/syncs/pages/royal-road-backprop/royal-road-backprop.sync.ts
@@ -1,4 +1,4 @@
-import type { Sync } from "../sync.page-type.ts"
+import type { Sync } from "../../sync.page-type.ts"
 
 export const royalRoadBackprop = {
   id: "01a06861-f664-7830-b8d5-cd7e09bb1bde",
--- collections/syncs/pages/royal-road/royal-road.sync.ts
+++ collections/syncs/pages/royal-road/royal-road.sync.ts
@@ -1,4 +1,4 @@
-import type { Sync } from "../sync.page-type.ts"
+import type { Sync } from "../../sync.page-type.ts"
 
 export const royalRoad = {
   id: "01a06861-f664-73fd-a5db-03ac63ef2331",
