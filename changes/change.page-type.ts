e59d45ba13a79f21bbc850f1314d4c63c304c728
Dalla <dalla@alanwalton.com>
2026-09-06 09:54:12 -0600

    the patch this agent drafted

--- changes/change.page-type.ts
+++ changes/change.page-type.ts
@@ -25,6 +25,7 @@ export const change = {
     "page-type/atomic-change",
     "page-type/refactor-change",
     "page-type/change-partial",
+    "page-type/change-command",
     "page-type/workflow-template",
     "page-type/change-kind",
     "workspace-package/workflow-language",
