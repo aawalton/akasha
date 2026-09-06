3299f5a6d2b0a270a427d20083c595b9821fbed7
pages-access <pages-access@alanwalton.com>
2026-09-06 16:03:09 -0600

    patchPage(temper-catalog-domain): item-set
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/item-set.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/item-set.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const itemSet = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-06T20:35:19.893Z",
+  capturedAt: "2026-09-06T22:02:58.118Z",
   generatorRanForVersion: "eso.live.11.3.6.3240040",
   generatorRanForManifestApiVersion: 101049,
   dormant: true,
