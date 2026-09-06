b1ab7fd03c9fda18228845be62f95d48fafd91c1
Abby <abby@alanwalton.com>
2026-09-06 12:06:28 -0600

    migrate being-the-one-who-knows's unsettled text into questions

---
+++ alan/books/all-about-alan/questions/pages/how-big-outbound-attending-nets-on-my-budget.all-about-alan-question.ts
@@ -0,0 +1,9 @@
+import type { AllAboutAlanQuestion } from "../all-about-alan-question.page-type.ts"
+
+export const howBigOutboundAttendingNetsOnMyBudget = {
+  id: "01a077e6-4df5-7836-a181-deb473cae47a",
+  pageTypeSlug: "all-about-alan-question",
+  slug: "how-big-outbound-attending-nets-on-my-budget",
+  topicSlug: "being-the-one-who-knows",
+  ask: "I know the sign of what outbound attending nets on my budget but not the size. How big is that net?",
+} as const satisfies AllAboutAlanQuestion
