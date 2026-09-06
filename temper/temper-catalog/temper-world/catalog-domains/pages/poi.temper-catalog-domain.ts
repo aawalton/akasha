38aec7558db7bea27cfba0185eb95aa484ee7190
pages-access <pages-access@alanwalton.com>
2026-09-06 08:33:29 -0600

    patchPage(temper-catalog-domain): poi
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/poi.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/poi.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const poi = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-06T14:25:45.180Z",
+  capturedAt: "2026-09-06T14:33:08.445Z",
   generatorRanForVersion: "eso.live.12.0.6.3274791",
   generatorRanForManifestApiVersion: 101050,
 } as const satisfies TemperCatalogDomain
