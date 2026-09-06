2e9c8a3e45ab610847fdd3a956f56e20fd9d77f6
Abby <abby@alanwalton.com>
2026-09-06 12:07:35 -0600

    migrate building-a-definition-i-can-run-on's unsettled text into a question

---
+++ alan/books/all-about-alan/questions/pages/whether-the-core-human-parts-can-be-enumerated.all-about-alan-question.ts
@@ -0,0 +1,9 @@
+import type { AllAboutAlanQuestion } from "../all-about-alan-question.page-type.ts"
+
+export const whetherTheCoreHumanPartsCanBeEnumerated = {
+  id: "01a077e7-4c1b-78ab-9ffd-b33aa0db5d63",
+  pageTypeSlug: "all-about-alan-question",
+  slug: "whether-the-core-human-parts-can-be-enumerated",
+  topicSlug: "building-a-definition-i-can-run-on",
+  ask: "Can the whole space of core human parts be enumerated on purpose, or does the same incompressibility that blocks a general method block the map too?",
+} as const satisfies AllAboutAlanQuestion
