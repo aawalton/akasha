42cdea8dbe94c72ed586252e315a1f5742d8b409
pages-access <pages-access@alanwalton.com>
2026-09-06 09:58:02 -0600

    patchPage(temper-catalog-domain): lore-library
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/lore-library.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/lore-library.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const loreLibrary = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-06T15:53:39.199Z",
+  capturedAt: "2026-09-06T15:57:57.575Z",
   generatorRanForVersion: "eso.live.12.0.6.3274791",
   generatorRanForManifestApiVersion: 101050,
 } as const satisfies TemperCatalogDomain
