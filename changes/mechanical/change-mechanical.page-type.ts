0e19009ae85a38323c8d3f86198d7f34cedc1e46
Nimue <nimue@alanwalton.com>
2026-09-06 17:45:42 -0600

    the patch this agent drafted

--- changes/mechanical/change-mechanical.page-type.ts
+++ changes/mechanical/change-mechanical.page-type.ts
@@ -1,6 +1,6 @@
 import type { PageType } from "../../pages/types/page-type.page-type.ts"
 import type { Change } from "../change.page-type.ts"
-import type { GuardSlugs } from "../properties/guard-slugs.relation-property.ts"
+import type { GuardSlugs } from "./properties/guard-slugs.relation-property.ts"
 
 export type ChangeMechanical = Change & {
   guardSlugs?: readonly GuardSlugs[]
--- changes/mechanical/properties/guard-slugs.relation-property.ts
+++ changes/mechanical/properties/guard-slugs.relation-property.ts
@@ -1,5 +1,5 @@
-import type { Slug } from "../../pages/properties/slug.text-property.ts"
-import type { RelationProperty } from "../../pages/relation-properties/relation-property.page-type.ts"
+import type { Slug } from "../../../pages/properties/slug.text-property.ts"
+import type { RelationProperty } from "../../../pages/relation-properties/relation-property.page-type.ts"
 
 export type GuardSlugs = Slug
 
