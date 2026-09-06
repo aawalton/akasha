8a31e0c35429852eb0fb617e32289e2293aec24d
pages-access <pages-access@alanwalton.com>
2026-09-06 07:16:02 -0600

    patchPage(temper-catalog-domain): antiquity-lore
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/antiquity-lore.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/antiquity-lore.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const antiquityLore = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-06T13:01:32.184Z",
+  capturedAt: "2026-09-06T13:15:56.342Z",
   generatorRanForVersion: "eso.live.12.0.6.3274791",
   generatorRanForManifestApiVersion: 101050,
 } as const satisfies TemperCatalogDomain
