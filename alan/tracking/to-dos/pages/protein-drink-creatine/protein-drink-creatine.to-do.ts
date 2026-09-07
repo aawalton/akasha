ae41001db7dca70732b7e99db60d6aed68a9f6d9
alanwalton-web <alanwalton-web@alanwalton.com>
2026-09-06 19:39:27 -0600

    patchPage(to-do): protein-drink-creatine
    Written-by: alanwalton-web <alanwalton-web@alanwalton.com>

--- alan/tracking/to-dos/pages/protein-drink-creatine/protein-drink-creatine.to-do.ts
+++ alan/tracking/to-dos/pages/protein-drink-creatine/protein-drink-creatine.to-do.ts
@@ -7,7 +7,7 @@ export const proteinDrinkCreatine = {
   title: "Protein drink + creatine",
   toDoCategory: "health",
   difficulty: "trivial",
-  toDoDueDate: "2026-09-06",
+  toDoDueDate: "2026-09-07",
   toDoPriority: "p3",
   toDoRecurrence: "FREQ=DAILY",
   toDoValueSlug: "health",
