6111c5b80079dfe350e2e9f3ead2d08f1af3085b
alanwalton-web <alanwalton-web@alanwalton.com>
2026-09-06 19:39:44 -0600

    patchPage(to-do): trim-toenails
    Written-by: alanwalton-web <alanwalton-web@alanwalton.com>

--- alan/tracking/to-dos/pages/trim-toenails/trim-toenails.to-do.ts
+++ alan/tracking/to-dos/pages/trim-toenails/trim-toenails.to-do.ts
@@ -7,7 +7,7 @@ export const trimToenails = {
   title: "Trim Toenails",
   toDoCategory: "health",
   difficulty: "light",
-  toDoDueDate: "2026-09-06",
+  toDoDueDate: "2026-09-07",
   toDoPriority: "p3",
   toDoRecurrence: "FREQ=MONTHLY;BYDAY=4MO",
   toDoSortOrder: 51,
