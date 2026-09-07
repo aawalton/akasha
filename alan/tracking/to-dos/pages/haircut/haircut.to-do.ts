e81b6db8bbfdc4b11de745b807c811272a507a0f
alanwalton-web <alanwalton-web@alanwalton.com>
2026-09-06 19:39:19 -0600

    patchPage(to-do): haircut
    Written-by: alanwalton-web <alanwalton-web@alanwalton.com>

--- alan/tracking/to-dos/pages/haircut/haircut.to-do.ts
+++ alan/tracking/to-dos/pages/haircut/haircut.to-do.ts
@@ -7,7 +7,7 @@ export const haircut = {
   title: "Haircut",
   toDoCategory: "health",
   difficulty: "major",
-  toDoDueDate: "2026-09-06",
+  toDoDueDate: "2026-09-07",
   toDoPriority: "p3",
   toDoRecurrence: "FREQ=WEEKLY;INTERVAL=6",
   toDoSortOrder: 53,
