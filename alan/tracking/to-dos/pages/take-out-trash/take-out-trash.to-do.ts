ab567f4e6dfd95da8e8919a331201dc9e66f0c9d
alanwalton-web <alanwalton-web@alanwalton.com>
2026-09-06 19:39:36 -0600

    patchPage(to-do): take-out-trash
    Written-by: alanwalton-web <alanwalton-web@alanwalton.com>

--- alan/tracking/to-dos/pages/take-out-trash/take-out-trash.to-do.ts
+++ alan/tracking/to-dos/pages/take-out-trash/take-out-trash.to-do.ts
@@ -7,7 +7,7 @@ export const takeOutTrash = {
   title: "Take out Trash",
   toDoCategory: "health",
   difficulty: "light",
-  toDoDueDate: "2026-09-06",
+  toDoDueDate: "2026-09-07",
   toDoPriority: "p3",
   toDoRecurrence: "FREQ=WEEKLY;BYDAY=TU,SA",
   toDoSortOrder: 35,
