7a7939b0edef148e06df0d3f7692a1d6f2ccc41d
Thea <thea@alanwalton.com>
2026-09-07 06:22:32 -0600

    the patch this agent drafted

--- changes/runners/pages/change-running/change-running.change-runner.addressed.ts
+++ changes/runners/pages/change-running/change-running.change-runner.addressed.ts
@@ -1,46 +1,4 @@
 export type Changes = {
-  "change-authored/add-file": Parameters<
-    typeof import("../../../authored/pages/add-file/add-file.change-authored.code.ts")["runChange"]
-  >[1]
-  "change-authored/change-file": Parameters<
-    typeof import("../../../authored/pages/change-file/change-file.change-authored.code.ts")["runChange"]
-  >[1]
-  "change-checked/add-property-value": Parameters<
-    typeof import("../../../checked/pages/add-property-value/add-property-value.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/change-domain-parent": Parameters<
-    typeof import("../../../checked/pages/change-domain-parent/change-domain-parent.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/change-page-page-type": Parameters<
-    typeof import("../../../checked/pages/change-page-page-type/change-page-page-type.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/change-page-property": Parameters<
-    typeof import("../../../checked/pages/change-page-property/change-page-property.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/move-page": Parameters<
-    typeof import("../../../checked/pages/move-page/move-page.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/remove-file": Parameters<
-    typeof import("../../../checked/pages/remove-file/remove-file.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/remove-page": Parameters<
-    typeof import("../../../checked/pages/remove-page/remove-page.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/remove-page-type": Parameters<
-    typeof import("../../../checked/pages/remove-page-type/remove-page-type.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/remove-property-value": Parameters<
-    typeof import("../../../checked/pages/remove-property-value/remove-property-value.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/rename-code-token": Parameters<
-    typeof import("../../../checked/pages/rename-code-token/rename-code-token.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/rename-package": Parameters<
-    typeof import("../../../checked/pages/rename-package/rename-package.change-checked.code.ts")["runChange"]
-  >[1]
-  "change-checked/rename-page": Parameters<
-    typeof import("../../../checked/pages/rename-page/rename-page.change-checked.code.ts")["runChange"]
-  >[1]
   "change-mechanical/add-code-file": Parameters<
     typeof import("../../../mechanical/pages/add-code-file/add-code-file.change-mechanical.code.ts")["runChange"]
   >[1]
@@ -112,8 +70,5 @@ export type Changes = {
   >[1]
   "change-mechanical/rename-property-signature": Parameters<
     typeof import("../../../mechanical/pages/rename-property-signature/rename-property-signature.change-mechanical.code.ts")["runChange"]
-  >[1]
-  "change-restated/change-page-property-text": Parameters<
-    typeof import("../../../restated/pages/change-page-property-text/change-page-property-text.change-restated.code.ts")["runChange"]
   >[1]
 }
