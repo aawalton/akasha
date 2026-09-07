0facdb8573318674e745ca77dba3af23b108abee
pages-access <pages-access@alanwalton.com>
2026-09-07 11:06:46 -0600

    patchPage(temper-catalog-domain): tribute
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/tribute.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/tribute.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const tribute = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-07T17:00:35.792Z",
+  capturedAt: "2026-09-07T17:06:26.351Z",
   generatorRanForVersion: "eso.live.12.0.6.3274791",
   generatorRanForManifestApiVersion: 101050,
 } as const satisfies TemperCatalogDomain
