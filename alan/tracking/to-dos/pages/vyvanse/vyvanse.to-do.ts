1c9c547465b5d16740fcb65cb62bcac699d7d444
alanwalton-web <alanwalton-web@alanwalton.com>
2026-09-07 05:53:40 -0600

    patchPage(to-do): vyvanse
    Written-by: alanwalton-web <alanwalton-web@alanwalton.com>

--- alan/tracking/to-dos/pages/vyvanse/vyvanse.to-do.ts
+++ alan/tracking/to-dos/pages/vyvanse/vyvanse.to-do.ts
@@ -6,10 +6,10 @@ export const vyvanse = {
   slug: "vyvanse",
   title: "Vyvanse",
   difficulty: "trivial",
-  toDoDueDate: "2026-09-07",
+  toDoDueDate: "2026-09-08",
   dueTime: "06:00",
   toDoPriority: "p1",
   toDoRecurrence: "FREQ=DAILY",
   toDoValueSlug: "health",
-  toDoLastCompletedAt: "2026-09-07T01:38:59.009Z",
+  toDoLastCompletedAt: "2026-09-07T11:53:39.168Z",
 } as const satisfies ToDo
