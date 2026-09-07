c8b0f8ba48052ba7a0153001e545fbbcee6da770
alanwalton-web <alanwalton-web@alanwalton.com>
2026-09-06 20:06:40 -0600

    patchPage(to-do): trim-fingernails
    Written-by: alanwalton-web <alanwalton-web@alanwalton.com>

--- alan/tracking/to-dos/pages/trim-fingernails/trim-fingernails.to-do.ts
+++ alan/tracking/to-dos/pages/trim-fingernails/trim-fingernails.to-do.ts
@@ -7,7 +7,7 @@ export const trimFingernails = {
   title: "Trim Fingernails",
   toDoCategory: "health",
   difficulty: "light",
-  toDoDueDate: "2026-09-06",
+  toDoDueDate: "2026-09-07",
   toDoPriority: "p3",
   toDoRecurrence: "FREQ=MONTHLY;BYDAY=2MO,4MO",
   toDoSortOrder: 50,
