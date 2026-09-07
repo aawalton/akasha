7d4a869d9c4d10cd89bea29bbaed1a7b868dfd4e
pages-access <pages-access@alanwalton.com>
2026-09-06 21:03:54 -0600

    patchPage(temper-catalog-domain): achievement
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/achievement.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/achievement.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const achievement = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-07T02:48:00.007Z",
+  capturedAt: "2026-09-07T03:03:53.148Z",
   generatorRanForVersion: "eso.live.12.0.6.3274791",
   generatorRanForManifestApiVersion: 101050,
 } as const satisfies TemperCatalogDomain
