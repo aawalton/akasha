a96f97baf2569e6d1bf25c16ed92f12afeecdad1
pages-access <pages-access@alanwalton.com>
2026-09-07 11:10:41 -0600

    patchPage(temper-catalog-domain): skill
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/skill.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/skill.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const skill = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-07T17:06:26.351Z",
+  capturedAt: "2026-09-07T17:10:02.668Z",
   generatorRanForVersion: "eso.live.11.3.6.3240040",
   generatorRanForManifestApiVersion: 101049,
   dormant: true,
