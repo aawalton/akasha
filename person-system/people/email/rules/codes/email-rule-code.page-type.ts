a9699772bdcfdd02783c2e6461cc9657bb0860e0
Astra <astra@alanwalton.com>
2026-09-07 08:12:18 -0600

    the person-system folder is named persons

--- person-system/people/email/rules/codes/email-rule-code.page-type.ts
+++ person-system/people/email/rules/codes/email-rule-code.page-type.ts
@@ -1,9 +1,9 @@
 import type { PageType } from "@akasha/pages/page-type"
+import type { EmailRuleCodeActions } from "../../../../../persons/people/email/rules/codes/properties/email-rule-code-actions.select-property.ts"
+import type { EmailRuleCodeDelay } from "../../../../../persons/people/email/rules/codes/properties/email-rule-code-delay.text-property.ts"
+import type { EmailRuleCodeFiling } from "../../../../../persons/people/email/rules/codes/properties/email-rule-code-filing.select-property.ts"
+import type { EmailRuleCodeForwardToSlug } from "../../../../../persons/people/email/rules/codes/properties/email-rule-code-forward-to-slug.relation-property.ts"
 import type { EmailRule } from "../email-rule.page-type.ts"
-import type { EmailRuleCodeActions } from "./properties/email-rule-code-actions.select-property.ts"
-import type { EmailRuleCodeDelay } from "./properties/email-rule-code-delay.text-property.ts"
-import type { EmailRuleCodeFiling } from "./properties/email-rule-code-filing.select-property.ts"
-import type { EmailRuleCodeForwardToSlug } from "./properties/email-rule-code-forward-to-slug.relation-property.ts"
 
 export type EmailRuleCode = EmailRule & {
   filing: EmailRuleCodeFiling
