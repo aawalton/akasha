895b62ca25a053ce881a86fd4af72566c3b57ea5
alanwalton-web <alanwalton-web@alanwalton.com>
2026-09-06 20:07:35 -0600

    patchPage(temper-task): golden-pursuits-straight-to-the-crate
    Written-by: alanwalton-web <alanwalton-web@alanwalton.com>

--- temper/temper-progress/tasks/pages/golden-pursuits-straight-to-the-crate/golden-pursuits-straight-to-the-crate.temper-task.ts
+++ temper/temper-progress/tasks/pages/golden-pursuits-straight-to-the-crate/golden-pursuits-straight-to-the-crate.temper-task.ts
@@ -13,6 +13,6 @@ export const goldenPursuitsStraightToTheCrate = {
   accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   scope: "account",
   priority: "p1",
-  lastCompletedAt: "2026-09-07T02:07:18.171Z",
-  completedAt: "2026-09-07T02:07:18.171Z",
+  lastCompletedAt: null,
+  completedAt: null,
 } as const satisfies TemperTask
