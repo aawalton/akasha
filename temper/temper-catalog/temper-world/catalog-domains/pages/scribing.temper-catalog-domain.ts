a91c7387eeed7d9b1a4c0251365533f46e351ed6
pages-access <pages-access@alanwalton.com>
2026-09-06 18:07:29 -0600

    patchPage(temper-catalog-domain): scribing
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/scribing.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/scribing.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const scribing = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-06T23:45:29.220Z",
+  capturedAt: "2026-09-07T00:07:16.464Z",
   generatorRanForVersion: "eso.live.11.3.6.3240040",
   generatorRanForManifestApiVersion: 101049,
   dormant: true,
