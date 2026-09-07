8bf690763bcda3f5f64ab0f12e1e92f0c0b647f4
alanwalton-web <alanwalton-web@alanwalton.com>
2026-09-06 19:39:15 -0600

    patchPage(to-do): clean-office
    Written-by: alanwalton-web <alanwalton-web@alanwalton.com>

--- alan/tracking/to-dos/pages/clean-office/clean-office.to-do.ts
+++ alan/tracking/to-dos/pages/clean-office/clean-office.to-do.ts
@@ -7,7 +7,7 @@ export const cleanOffice = {
   title: "Clean office",
   toDoCategory: "health",
   difficulty: "light",
-  toDoDueDate: "2026-09-06",
+  toDoDueDate: "2026-09-07",
   toDoPriority: "p3",
   toDoRecurrence: "FREQ=WEEKLY;BYDAY=SA",
   toDoSortOrder: 27,
