f9371b19eb2b417997bdaa47f5314dd0885f9c5f
Dalla <dalla@alanwalton.com>
2026-09-07 12:07:04 -0600

    the patch this agent drafted

--- changes/modules/change-answer/change-answer.module.types.ts
+++ changes/modules/change-answer/change-answer.module.types.ts
@@ -1,11 +1,41 @@
-export type Edit = {
+export type Reading = {
+  readonly readersOweReading?: boolean
+  readonly writerOwesReading?: boolean
+}
+
+export type Edit = Reading & {
+  readonly kind?: undefined
   readonly path: string
   readonly was: string | null
   readonly body: string | null
   readonly from?: string
-  readonly readersOweReading?: boolean
-  readonly writerOwesReading?: boolean
 }
+
+export type Adding = Reading & {
+  readonly kind: "add"
+  readonly path: string
+  readonly content: string
+}
+
+export type Replacing = Reading & {
+  readonly kind: "replace"
+  readonly path: string
+  readonly contentFrom: string
+  readonly contentTo: string
+}
+
+export type Removing = Reading & {
+  readonly kind: "remove"
+  readonly path: string
+}
+
+export type Moving = Reading & {
+  readonly kind: "move"
+  readonly pathFrom: string
+  readonly pathTo: string
+}
+
+export type Stated = Adding | Replacing | Removing | Moving
 
 export type Answer = {
   readonly edits: readonly Edit[]
