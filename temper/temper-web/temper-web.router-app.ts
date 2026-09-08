3e7cca3ec70b90ea84180a70221cb1bfa4acf3d7
Thea <thea@alanwalton.com>
2026-09-08 09:30:27 -0600

    apply temper/temper-web/temper-web.router-app.ts

--- temper/temper-web/temper-web.router-app.ts
+++ temper/temper-web/temper-web.router-app.ts
@@ -6,6 +6,7 @@ export const temperWeb = {
   slug: "temper-web",
   definition: "the routes Temper serves to a browser",
   manifest: "json",
+  tunnelRoutes: "ts",
   appLayout: "tsx",
   rootRoute: "tsx",
   routeTable: "ts",
