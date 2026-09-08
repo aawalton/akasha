7abdb736111bf58b87c4c5763763abc2eaedab2d
Akasha <akasha@alanwalton.com>
2026-09-08 12:10:32 -0600

    apply alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-jesu-joy-of-man-s-desiring-the-great-bach-choruses-2025-remastered/the-tabernacle-choir-at-temple-square-jesu-joy-of-man-s-desiring-the-great-bach-choruses-2025-remastered.release.ts, alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-jesu-joy-of-man-s-desiring-the-great-bach-choruses-2025-remastered/the-tabernacle-choir-at-temple-square-jesu-joy-of-man-s-desiring.release.ts

---
+++ alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-jesu-joy-of-man-s-desiring-the-great-bach-choruses-2025-remastered/the-tabernacle-choir-at-temple-square-jesu-joy-of-man-s-desiring.release.ts
@@ -0,0 +1,18 @@
+import type { Release } from "../../release.page-type.ts"
+
+export const theTabernacleChoirAtTempleSquareJesuJoyOfManSDesiring = {
+  id: "01a0676a-d722-7023-9b93-eb1015ebcce6",
+  pageTypeSlug: "release",
+  slug: "the-tabernacle-choir-at-temple-square-jesu-joy-of-man-s-desiring",
+  title: "Jesu, Joy of Man's Desiring - The Great Bach Choruses (2025 Remastered)",
+  partOfSlugs: ["the-tabernacle-choir-at-temple-square"],
+  position: 0,
+  ownLength: 51.48925,
+  ownProgress: 0,
+  unitSlug: "minutes",
+  status: "not-started",
+  publishedAt: "2025-04-04",
+  externalId: "6lxViNoDmVXWUREmK2Tx6X",
+  externalLink: "https://open.spotify.com/album/6lxViNoDmVXWUREmK2Tx6X",
+  lastSyncedAt: "2025-11-27",
+} as const satisfies Release
