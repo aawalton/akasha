aa47db7c09c1abac6ae7d47d5b00b9b91041b00c
Akasha <akasha@alanwalton.com>
2026-09-08 06:03:45 -0600

    royal road sync 1 page(s)

---
+++ story/story-chapters-read/pages/shapeshifter-a-non-human-litrpg-0044-chapter-39-stat-difference/shapeshifter-a-non-human-litrpg-0044-chapter-39-stat-difference.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const shapeshifterANonHumanLitrpg0044Chapter39StatDifference = {
+  id: "01a080e7-1bd4-7603-b868-a4b5f3735b84",
+  pageTypeSlug: "story-chapter-read",
+  slug: "shapeshifter-a-non-human-litrpg-0044-chapter-39-stat-difference",
+  partOfSlugs: ["story-read/shapeshifter-a-non-human-litrpg"],
+  position: 44,
+  publishedAt: "2026-09-08",
+  unitSlug: "words",
+  externalId: "3929467",
+  source: "royal-road",
+  title: "Chapter 39: Stat Difference",
+  ownLength: 1664,
+  externalLink:
+    "https://www.royalroad.com/fiction/123277/faceless-hunter-a-shapeshifter-litrpg/chapter/3929467/chapter-39-stat-difference",
+  prose: "txt",
+} as const satisfies StoryChapterRead
