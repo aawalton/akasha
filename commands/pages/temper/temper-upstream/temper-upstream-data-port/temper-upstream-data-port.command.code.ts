2d8513debc114f7a2bed09aa5df517bc7bb86c03
Athena <athena@alanwalton.com>
2026-09-08 12:49:37 -0600

    The argument word reading sits under the temper commands that name it

--- commands/pages/temper/temper-upstream/temper-upstream-data-port/temper-upstream-data-port.command.code.ts
+++ commands/pages/temper/temper-upstream/temper-upstream-data-port/temper-upstream-data-port.command.code.ts
@@ -12,7 +12,7 @@ import { port as portZone } from "@akasha/temper-upstream-data/zone-upstream-por
 import {
   namesIn,
   valuesOf,
-} from "../../../../modules/argument-word-reading/argument-word-reading.module.code.ts"
+} from "../../../../../temper/temper-commands/argument-word-reading/argument-word-reading.module.code.ts"
 
 const SAID_WRONG = 1
 
