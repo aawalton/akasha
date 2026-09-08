fb3b1446d8fe98a2c88e7a47c4f3148d63734ecf
Amy <amy@alanwalton.com>
2026-09-08 06:54:27 -0600

    apply alan/tracking/to-dos/pages/vyvanse/vyvanse.to-do.ts

--- alan/tracking/to-dos/pages/vyvanse/vyvanse.to-do.ts
+++ alan/tracking/to-dos/pages/vyvanse/vyvanse.to-do.ts
@@ -6,7 +6,7 @@ export const vyvanse = {
   slug: "vyvanse",
   title: "Vyvanse",
   difficulty: "trivial",
-  toDoDueDate: "2026-09-08",
+  toDoDueDate: "2026-09-09",
   dueTime: "06:00",
   toDoPriority: "p1",
   toDoRecurrence: "FREQ=DAILY",
