137317624d33423394d881530cfd5655362af5b7
Aelwyn <aelwyn@alanwalton.com>
2026-09-08 07:09:35 -0600

    apply command-system/namespaces/pages/exercise-constraint.namespace.ts, commands/pages/exercise-constraint-set/exercise-constraint-set.command.code.ts, commands/pages/exercise-constraint-set/exercise-constraint-set.command.ts

--- command-system/namespaces/pages/exercise-constraint.namespace.ts
+++ command-system/namespaces/pages/exercise-constraint.namespace.ts
@@ -5,5 +5,5 @@ export const exerciseConstraint = {
   pageTypeSlug: "namespace",
   slug: "exercise-constraint",
   definition: "a limit on what Alan's training may ask of him",
-  partSlugs: ["command/exercise-constraint-set"],
+  partSlugs: [],
 } as const satisfies Namespace
