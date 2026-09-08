85cf327b7beb40b49ba957d56d3fd60b6bdcccef
Akasha <akasha@alanwalton.com>
2026-09-08 12:06:44 -0600

    apply alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-stars-and-stripes-forever-the-mormon-tabernacle-choir-sings-march-favorites-and-college-songs/the-tabernacle-choir-at-temple-square-stars-and-stripes-forever-the-mormon-tabernacle-choir-sings-march-favorites-and-college-songs.release.ts, alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-stars-and-stripes-forever-the-mormon-tabernacle-choir-sings-march-favorites-and-college-songs/the-tabernacle-choir-at-temple-square-stars-and-stripes-forever.release.ts

---
+++ alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-stars-and-stripes-forever-the-mormon-tabernacle-choir-sings-march-favorites-and-college-songs/the-tabernacle-choir-at-temple-square-stars-and-stripes-forever.release.ts
@@ -0,0 +1,19 @@
+import type { Release } from "../../release.page-type.ts"
+
+export const theTabernacleChoirAtTempleSquareStarsAndStripesForever = {
+  id: "01a0676a-d72a-7014-8dc4-c22399150e9a",
+  pageTypeSlug: "release",
+  slug: "the-tabernacle-choir-at-temple-square-stars-and-stripes-forever",
+  title:
+    "Stars and Stripes Forever ! - The Mormon Tabernacle Choir sings March Favorites and College Songs",
+  partOfSlugs: ["the-tabernacle-choir-at-temple-square"],
+  position: 0,
+  ownLength: 66.240983,
+  ownProgress: 0,
+  unitSlug: "minutes",
+  status: "not-started",
+  publishedAt: "1973-01-01",
+  externalId: "5bHqlwvXgzeQ2nEFnwDGUg",
+  externalLink: "https://open.spotify.com/album/5bHqlwvXgzeQ2nEFnwDGUg",
+  lastSyncedAt: "2025-11-27",
+} as const satisfies Release
