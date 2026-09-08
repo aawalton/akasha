4daee215968b7f5f0a3ba674b8bd7ffdfbe74369
Akasha <akasha@alanwalton.com>
2026-09-08 12:06:14 -0600

    apply alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-rock-of-ages-30-favorite-hymns-with-the-philadelphia-brass-ensemble-percussion-alexander-schreiner/the-tabernacle-choir-at-temple-square-rock-of-ages-30-favorite-hymns-with-the-philadelphia-brass-ensemble-percussion-alexander-schreiner.release.ts, alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-rock-of-ages-30-favorite-hymns-with-the-philadelphia-brass-ensemble-percussion-alexander-schreiner/the-tabernacle-choir-at-temple-square-rock-of-ages-30-favorite-hymns.release.ts

---
+++ alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-rock-of-ages-30-favorite-hymns-with-the-philadelphia-brass-ensemble-percussion-alexander-schreiner/the-tabernacle-choir-at-temple-square-rock-of-ages-30-favorite-hymns.release.ts
@@ -0,0 +1,19 @@
+import type { Release } from "../../release.page-type.ts"
+
+export const theTabernacleChoirAtTempleSquareRockOfAges30FavoriteHymns = {
+  id: "01a0676a-d728-7019-9aba-f7fc2dc5384a",
+  pageTypeSlug: "release",
+  slug: "the-tabernacle-choir-at-temple-square-rock-of-ages-30-favorite-hymns",
+  title:
+    "Rock of Ages - 30 Favorite Hymns (with The Philadelphia Brass Ensemble & Percussion & Alexander Schreiner)",
+  partOfSlugs: ["the-tabernacle-choir-at-temple-square"],
+  position: 0,
+  ownLength: 74.579833,
+  ownProgress: 74.579833,
+  unitSlug: "minutes",
+  status: "completed",
+  publishedAt: "1960-01-01",
+  externalId: "235O9JdWj2FxUFXdjl22lo",
+  externalLink: "https://open.spotify.com/album/235O9JdWj2FxUFXdjl22lo",
+  lastSyncedAt: "2025-11-27",
+} as const satisfies Release
