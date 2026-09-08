4bf7679291b90488d9f5df1acf3db98539817821
Akasha <akasha@alanwalton.com>
2026-09-08 12:10:16 -0600

    apply alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-brahms-a-german-requiem-op-45-new-english-adaptation-by-robert-shaw/the-tabernacle-choir-at-temple-square-brahms-a-german-requiem-op-45-new-english-adaptation-by-robert-shaw.release.ts, alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-brahms-a-german-requiem-op-45-new-english-adaptation-by-robert-shaw/the-tabernacle-choir-at-temple-square-brahms-a-german-requiem-op-45.release.ts

---
+++ alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-brahms-a-german-requiem-op-45-new-english-adaptation-by-robert-shaw/the-tabernacle-choir-at-temple-square-brahms-a-german-requiem-op-45.release.ts
@@ -0,0 +1,18 @@
+import type { Release } from "../../release.page-type.ts"
+
+export const theTabernacleChoirAtTempleSquareBrahmsAGermanRequiemOp45 = {
+  id: "01a0676a-d719-702c-8ae6-8cebb20b5c2a",
+  pageTypeSlug: "release",
+  slug: "the-tabernacle-choir-at-temple-square-brahms-a-german-requiem-op-45",
+  title: "Brahms: A German Requiem, Op. 45 (New English Adaptation by Robert Shaw)",
+  partOfSlugs: ["the-tabernacle-choir-at-temple-square"],
+  position: 0,
+  ownLength: 68.82595,
+  ownProgress: 0,
+  unitSlug: "minutes",
+  status: "not-started",
+  publishedAt: "1999-09-01",
+  externalId: "0rB5SYhB3zx9KDFRyKgFi9",
+  externalLink: "https://open.spotify.com/album/0rB5SYhB3zx9KDFRyKgFi9",
+  lastSyncedAt: "2025-11-27",
+} as const satisfies Release
