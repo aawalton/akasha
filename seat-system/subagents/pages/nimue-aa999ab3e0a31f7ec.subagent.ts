9450614718ea67aa93db6f41d0790cc3865983f9
Akasha <akasha@alanwalton.com>
2026-09-08 10:41:06 -0600

    nimue-aa999ab3e0a31f7ec: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/nimue-aa999ab3e0a31f7ec.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const nimueAa999ab3e0a31f7ec = {
+  id: "01a081dd-5235-7281-91fc-f13add187cab",
+  pageTypeSlug: "subagent",
+  slug: "nimue-aa999ab3e0a31f7ec",
+  principalSeatName: "nimue",
+  assignmentSlug: "initiative/nimue-code-cleanup",
+  dispatchedAs: "Explore",
+  agentId: "01a081bf-5e17-7000-9dd9-4e001f01ce37--aa999ab3e0a31f7ec",
+} as const satisfies Subagent
