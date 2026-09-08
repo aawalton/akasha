9332fec4f56899d55a4d6249c86dc608a5bd39f1
Akasha <akasha@alanwalton.com>
2026-09-08 12:08:27 -0600

    apply alan/music/catalog/releases/pages/the-lord-of-the-rings-music-the-lord-of-the-rings-the-return-of-the-king-the-complete-recordings-limited-edition/the-lord-of-the-rings-music-the-lord-of-the-rings-the-return-of-the-king-the-complete-recordings-limited-edition.release.ts, alan/music/catalog/releases/pages/the-lord-of-the-rings-music-the-lord-of-the-rings-the-return-of-the-king-the-complete-recordings-limited-edition/the-lord-of-the-rings-music-the-return-of-the-king-the-complete-recordings.release.ts

---
+++ alan/music/catalog/releases/pages/the-lord-of-the-rings-music-the-lord-of-the-rings-the-return-of-the-king-the-complete-recordings-limited-edition/the-lord-of-the-rings-music-the-return-of-the-king-the-complete-recordings.release.ts
@@ -0,0 +1,19 @@
+import type { Release } from "../../release.page-type.ts"
+
+export const theLordOfTheRingsMusicTheReturnOfTheKingTheCompleteRecordings = {
+  id: "01a0676a-d72d-7037-a411-de5fb2945ac6",
+  pageTypeSlug: "release",
+  slug: "the-lord-of-the-rings-music-the-return-of-the-king-the-complete-recordings",
+  title:
+    "The Lord of the Rings - The Return of the King - The Complete Recordings (Limited Edition)",
+  partOfSlugs: ["the-lord-of-the-rings-music"],
+  position: 3,
+  ownLength: 229.664817,
+  ownProgress: 0,
+  unitSlug: "minutes",
+  status: "not-started",
+  publishedAt: "2003-11-24",
+  externalId: "2Re6RMBEfD8iv1HMxLMdzI",
+  externalLink: "https://open.spotify.com/album/2Re6RMBEfD8iv1HMxLMdzI",
+  lastSyncedAt: "2025-11-27",
+} as const satisfies Release
