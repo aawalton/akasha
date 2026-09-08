9c331ece9e5db5fc7f15ca5641ee16eb55025601
Thea <thea@alanwalton.com>
2026-09-08 06:55:31 -0600

    lua-language-extensions: the definition names the Lua compiler

--- temper/temper-eso-types/lua-language-extensions/lua-language-extensions.type-declaration.ts
+++ temper/temper-eso-types/lua-language-extensions/lua-language-extensions.type-declaration.ts
@@ -4,6 +4,6 @@ export const luaLanguageExtensions = {
   id: "01a0608f-b08f-7b36-8bab-1e0d5032d2e6",
   pageTypeSlug: "type-declaration",
   slug: "lua-language-extensions",
-  definition: "the extensions the Lua transpiler adds to TypeScript",
+  definition: "the extensions the Lua compiler adds to TypeScript",
   d: "ts",
 } as const satisfies TypeDeclaration
