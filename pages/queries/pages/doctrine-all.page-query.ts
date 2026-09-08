0f3b85bfd420e31651f07ca479b87f4c5e770c83
Awen <awen@alanwalton.com>
2026-09-08 13:14:13 -0600

    the query asking for every doctrine takes the name of the type it asks of

--- pages/queries/pages/doctrine-all.page-query.ts
+++ pages/queries/pages/doctrine-all.page-query.ts
@@ -1,8 +1,8 @@
 import type { PageQuery } from "../page-query.page-type.ts"
 
-export const gmDoctrinePackAll = {
+export const doctrineAll = {
   id: "01a063f9-220b-7fa5-a143-2f47aff88bd5",
   pageTypeSlug: "page-query",
-  slug: "gm-doctrine-pack-all",
-  asksOfSlug: "gm-doctrine-pack",
+  slug: "doctrine-all",
+  asksOfSlug: "doctrine",
 } as const satisfies PageQuery
