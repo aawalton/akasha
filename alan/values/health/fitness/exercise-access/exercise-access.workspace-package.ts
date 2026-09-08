876bcba624c31b5c450687bec54bb1a12c278e7b
Aelwyn <aelwyn@alanwalton.com>
2026-09-08 07:16:36 -0600

    apply alan/values/health/fitness/exercise-access/exercise-access.workspace-package.ts, alan/values/health/fitness/exercise-access/exercise-vocabulary/exercise-vocabulary.module.code.ts, alan/values/health/fitness/exercise-access/exercise-vocabulary/exercise-vocabulary.module.ts

--- alan/values/health/fitness/exercise-access/exercise-access.workspace-package.ts
+++ alan/values/health/fitness/exercise-access/exercise-access.workspace-package.ts
@@ -9,7 +9,6 @@ export const exerciseAccess = {
   partSlugs: [
     "module/exercise-load-model",
     "module/exercise-rows",
-    "module/exercise-vocabulary",
     "module/free-exercise-row",
     "module/set-history",
   ],
