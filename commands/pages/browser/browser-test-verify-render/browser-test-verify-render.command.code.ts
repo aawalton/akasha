4ae0941e720a69378b1d90c5e5dafdd4f47ac9b9
Athena <athena@alanwalton.com>
2026-09-08 12:24:02 -0600

    The render plan sits under the browser commands that name it

--- commands/pages/browser/browser-test-verify-render/browser-test-verify-render.command.code.ts
+++ commands/pages/browser/browser-test-verify-render/browser-test-verify-render.command.code.ts
@@ -20,18 +20,18 @@ import {
 import type { Answer } from "@akasha/command-system/calling"
 import { refusedBy } from "@akasha/command-system/command-answering"
 import {
-  countIn,
-  missingOf,
-  wordsIn,
-} from "../../../modules/browser-command-arguing/browser-command-arguing.module.code.ts"
-import {
   classifyExpectedText,
   decideDiscriminatingSignal,
   isRetryableSessionOpenTimeout,
   planRenderSettleWait,
   planTitleSettleWait,
   planVerifyRenderSession,
-} from "../../../modules/verify-render-plan/verify-render-plan.module.code.ts"
+} from "../../../../browser/browser-commands/verify-render-plan/verify-render-plan.module.code.ts"
+import {
+  countIn,
+  missingOf,
+  wordsIn,
+} from "../../../modules/browser-command-arguing/browser-command-arguing.module.code.ts"
 
 const URL_SAID = "--url"
 
