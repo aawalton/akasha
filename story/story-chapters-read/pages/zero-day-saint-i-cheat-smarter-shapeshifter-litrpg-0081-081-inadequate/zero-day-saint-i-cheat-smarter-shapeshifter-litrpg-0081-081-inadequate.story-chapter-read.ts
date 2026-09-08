2ccf6d10f027232fad7ded7c6892d1936b382889
Akasha <akasha@alanwalton.com>
2026-09-08 00:06:12 -0600

    royal road sync 1 page(s)

---
+++ story/story-chapters-read/pages/zero-day-saint-i-cheat-smarter-shapeshifter-litrpg-0081-081-inadequate/zero-day-saint-i-cheat-smarter-shapeshifter-litrpg-0081-081-inadequate.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const zeroDaySaintICheatSmarterShapeshifterLitrpg0081081Inadequate = {
+  id: "01a07f9f-bb09-7561-9dc0-01bdf8f44e15",
+  pageTypeSlug: "story-chapter-read",
+  slug: "zero-day-saint-i-cheat-smarter-shapeshifter-litrpg-0081-081-inadequate",
+  partOfSlugs: ["story-read/zero-day-saint-i-cheat-smarter-shapeshifter-litrpg"],
+  position: 81,
+  publishedAt: "2026-09-08",
+  unitSlug: "words",
+  externalId: "3928648",
+  source: "royal-road",
+  title: "081 - (In)adequate",
+  ownLength: 1905,
+  externalLink:
+    "https://www.royalroad.com/fiction/164273/zero-day-saint-litrpgvol-1-complete/chapter/3928648/081-inadequate",
+  prose: "txt",
+} as const satisfies StoryChapterRead
