1e4c34c34bc9c9b609152a3592866ae260c4b73a
Akasha <akasha@alanwalton.com>
2026-09-08 14:05:06 -0600

    apply pages/indexes/indexing/indexing.module.code.ts

--- pages/indexes/indexing/indexing.module.code.ts
+++ pages/indexes/indexing/indexing.module.code.ts
@@ -96,8 +96,8 @@ export function filingOf(
   for (const at of new Set([...withdrawn.keys(), ...added.keys()])) {
     const gone = new Set((withdrawn.get(at) ?? []).map((one) => one.line))
     const come = new Set((added.get(at) ?? []).map((one) => one.line))
-    const surviving = [...reading.lines(at)].filter((one) => !gone.has(one))
-    const coming = [...come].filter((one) => !surviving.includes(one))
+    const surviving = new Set([...reading.lines(at)].filter((one) => !gone.has(one)))
+    const coming = [...come].filter((one) => !surviving.has(one))
     said.push({ at, lines: [...surviving, ...coming].sort() })
   }
   return said
