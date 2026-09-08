721faaa77e6fee0b1e5f5ce0680db333c03131c3
Akasha <akasha@alanwalton.com>
2026-09-08 14:20:56 -0600

    ops-food: the food entry food-entry-2026-09-08-avocado-half

---
+++ alan/track/food-entries/pages/2026-09-08-avocado-half/food-entry-2026-09-08-avocado-half.food-entry.ts
@@ -0,0 +1,11 @@
+import type { FoodEntry } from "../../food-entry.page-type.ts"
+
+export const foodEntry20260908AvocadoHalf = {
+  id: "01a082ae-3aa2-7000-a84b-528c8555b7ec",
+  pageTypeSlug: "food-entry",
+  slug: "food-entry-2026-09-08-avocado-half",
+  title: "Avocado, half",
+  happenedAt: "2026-09-08T20:20:51.479Z",
+  plantGrams: 100,
+  estimatedCalories: 160,
+} as const satisfies FoodEntry
