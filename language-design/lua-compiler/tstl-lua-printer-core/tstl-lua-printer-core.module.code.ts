2e65b9491bfdffc0f9df73994cb2ae19db60c72e
Thea <thea@alanwalton.com>
2026-09-08 05:18:35 -0600

    tstl-lua-printer-core: the node builtin named with its protocol

--- language-design/lua-compiler/tstl-lua-printer-core/tstl-lua-printer-core.module.code.ts
+++ language-design/lua-compiler/tstl-lua-printer-core/tstl-lua-printer-core.module.code.ts
@@ -1,4 +1,4 @@
-import * as path from "path"
+import * as path from "node:path"
 import { SourceNode } from "source-map"
 import type * as ts from "typescript"
 import type { EmitHost } from "../transpile-emit-host/transpile-emit-host.module.code.ts"
