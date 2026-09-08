ad0646e9b61d98d90f02692570535026cab6ef25
Amy <amy@alanwalton.com>
2026-09-08 06:54:19 -0600

    apply alan/tracking/to-dos/pages/vyvanse/vyvanse.to-do.ts

--- alan/tracking/to-dos/pages/vyvanse/vyvanse.to-do.ts
+++ alan/tracking/to-dos/pages/vyvanse/vyvanse.to-do.ts
@@ -11,5 +11,5 @@ export const vyvanse = {
   toDoPriority: "p1",
   toDoRecurrence: "FREQ=DAILY",
   toDoValueSlug: "health",
-  toDoLastCompletedAt: "2026-09-07T11:53:39.168Z",
+  toDoLastCompletedAt: "2026-09-08T12:54:04.880Z",
 } as const satisfies ToDo
