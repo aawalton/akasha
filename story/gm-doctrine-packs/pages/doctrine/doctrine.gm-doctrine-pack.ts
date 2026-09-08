20b1b89ec627b991f959bd57b2a38dfb2fec642a
Awen <awen@alanwalton.com>
2026-09-08 13:04:11 -0600

    the one pack is called doctrine rather than the type it is

--- story/gm-doctrine-packs/pages/doctrine/doctrine.gm-doctrine-pack.ts
+++ story/gm-doctrine-packs/pages/doctrine/doctrine.gm-doctrine-pack.ts
@@ -1,9 +1,9 @@
 import type { GmDoctrinePack } from "../../gm-doctrine-pack.page-type.ts"
 
-export const gmDoctrinePack = {
+export const doctrine = {
   id: "01a06590-c57a-7618-b2b3-74a985855b84",
   pageTypeSlug: "gm-doctrine-pack",
-  slug: "gm-doctrine-pack",
+  slug: "doctrine",
   doctrineVersion: 33,
   policies: "json",
   gateDimensions: "json",
