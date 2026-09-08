0b8b869c4d38e9f161085a131d082826fabc041f
Akasha <akasha@alanwalton.com>
2026-09-08 12:03:36 -0600

    royal road sync 2 page(s)

---
+++ story/story-chapters-read/pages/defiance-of-the-fall-0138-chapter-1457-grand-unity/defiance-of-the-fall-0138-chapter-1457-grand-unity.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const defianceOfTheFall0138Chapter1457GrandUnity = {
+  id: "01a08230-8764-7b8f-8f91-48e86812b35a",
+  pageTypeSlug: "story-chapter-read",
+  slug: "defiance-of-the-fall-0138-chapter-1457-grand-unity",
+  partOfSlugs: ["story-read/defiance-of-the-fall"],
+  position: 138,
+  publishedAt: "2026-09-08",
+  unitSlug: "words",
+  externalId: "3930931",
+  source: "royal-road",
+  title: "Chapter 1,457 - Grand Unity",
+  ownLength: 2903,
+  externalLink:
+    "https://www.royalroad.com/fiction/24709/defiance-of-the-fall/chapter/3930931/chapter-1457-grand-unity",
+  prose: "txt",
+} as const satisfies StoryChapterRead
---
+++ story/story-chapters-read/pages/path-of-the-deathless-book-4-completed-0469-453-kayfabe/path-of-the-deathless-book-4-completed-0469-453-kayfabe.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const pathOfTheDeathlessBook4Completed0469453Kayfabe = {
+  id: "01a08230-8cc9-7bc6-ac65-92c5e5fe3908",
+  pageTypeSlug: "story-chapter-read",
+  slug: "path-of-the-deathless-book-4-completed-0469-453-kayfabe",
+  partOfSlugs: ["story-read/path-of-the-deathless-book-4-completed"],
+  position: 469,
+  publishedAt: "2026-09-08",
+  unitSlug: "words",
+  externalId: "3930923",
+  source: "royal-road",
+  title: "453 Kayfabe",
+  ownLength: 5070,
+  externalLink:
+    "https://www.royalroad.com/fiction/122502/path-of-the-deathless-book-3-stubbed/chapter/3930923/453-kayfabe",
+  prose: "txt",
+} as const satisfies StoryChapterRead
