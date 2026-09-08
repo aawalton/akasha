74ceda9d49ce2d15ff877211c3924f59dc03e4d9
Akasha <akasha@alanwalton.com>
2026-09-08 12:06:29 -0600

    apply alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-this-is-my-country-the-world-s-great-songs-of-patriotism-and-brotherhood-2023-remastered-version/the-tabernacle-choir-at-temple-square-this-is-my-country-the-world-s-great-songs-of-patriotism-and-brotherhood-2023-remastered-version.release.ts, alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-this-is-my-country-the-world-s-great-songs-of-patriotism-and-brotherhood-2023-remastered-version/the-tabernacle-choir-at-temple-square-this-is-my-country.release.ts

---
+++ alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-this-is-my-country-the-world-s-great-songs-of-patriotism-and-brotherhood-2023-remastered-version/the-tabernacle-choir-at-temple-square-this-is-my-country.release.ts
@@ -0,0 +1,19 @@
+import type { Release } from "../../release.page-type.ts"
+
+export const theTabernacleChoirAtTempleSquareThisIsMyCountry = {
+  id: "01a0676a-d72e-702d-899a-e57f7d801bfd",
+  pageTypeSlug: "release",
+  slug: "the-tabernacle-choir-at-temple-square-this-is-my-country",
+  title:
+    "This Is My Country - The World's Great Songs of Patriotism and Brotherhood (2023 Remastered Version)",
+  partOfSlugs: ["the-tabernacle-choir-at-temple-square"],
+  position: 0,
+  ownLength: 56.6606,
+  ownProgress: 56.6606,
+  unitSlug: "minutes",
+  status: "completed",
+  publishedAt: "1962-01-01",
+  externalId: "4CFPl1dwLMUbqZRh4TCee7",
+  externalLink: "https://open.spotify.com/album/4CFPl1dwLMUbqZRh4TCee7",
+  lastSyncedAt: "2025-11-27",
+} as const satisfies Release
