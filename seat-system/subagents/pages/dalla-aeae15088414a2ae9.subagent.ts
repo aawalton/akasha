d7663249b470e96a0a29f1889615ebd7d0067d12
Akasha <akasha@alanwalton.com>
2026-09-08 07:06:14 -0600

    dalla-aeae15088414a2ae9: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/dalla-aeae15088414a2ae9.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const dallaAeae15088414a2ae9 = {
+  id: "01a08120-505d-7a00-bbf5-7473a48d5cae",
+  pageTypeSlug: "subagent",
+  slug: "dalla-aeae15088414a2ae9",
+  principalSeatName: "dalla",
+  assignmentSlug: "initiative/dalla-draft-into-a-patch",
+  dispatchedAs: "Explore",
+  agentId: "01a07eb0-c517-7000-9ee4-cfc39576ac24--aeae15088414a2ae9",
+} as const satisfies Subagent
