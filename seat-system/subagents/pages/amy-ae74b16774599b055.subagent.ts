35b36d230f79a8394b9419b8fd2a532f3c2a447a
Akasha <akasha@alanwalton.com>
2026-09-08 10:48:55 -0600

    amy-ae74b16774599b055: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/amy-ae74b16774599b055.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const amyAe74b16774599b055 = {
+  id: "01a081df-469e-7bb9-a1a3-70b1d6880342",
+  pageTypeSlug: "subagent",
+  slug: "amy-ae74b16774599b055",
+  principalSeatName: "amy",
+  assignmentSlug: "initiative/amy-work-panel-editing",
+  dispatchedAs: "Explore",
+  agentId: "01a07e83-7b6c-7000-a876-5c2d08753493--ae74b16774599b055",
+} as const satisfies Subagent
