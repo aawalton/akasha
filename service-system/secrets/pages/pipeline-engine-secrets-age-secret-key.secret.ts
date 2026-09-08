3b8f175df1786e766d6405d595df2464d6a8a22e
Aranya <aranya@alanwalton.com>
2026-09-08 11:51:48 -0600

    apply 6 files

--- service-system/secrets/pages/pipeline-engine-secrets-age-secret-key.secret.ts
+++
@@ -1,8 +0,0 @@
-import type { Secret } from "../secret.page-type.ts"
-
-export const pipelineEngineSecretsAgeSecretKey = {
-  id: "01a076b8-884b-7b7b-9774-036c6cfd9e2c",
-  pageTypeSlug: "secret",
-  slug: "pipeline-engine-secrets-age-secret-key",
-  placements: [{ resourceName: "pipeline-engine-secrets", resourceKey: "AGE_SECRET_KEY" }],
-} as const satisfies Secret
--- service-system/secrets/pages/pipeline-engine-secrets-git-webhook-secret.secret.ts
+++
@@ -1,8 +0,0 @@
-import type { Secret } from "../secret.page-type.ts"
-
-export const pipelineEngineSecretsGitWebhookSecret = {
-  id: "01a076b8-b2da-7ca2-ae73-2de5220aed79",
-  pageTypeSlug: "secret",
-  slug: "pipeline-engine-secrets-git-webhook-secret",
-  placements: [{ resourceName: "pipeline-engine-secrets", resourceKey: "GIT_WEBHOOK_SECRET" }],
-} as const satisfies Secret
