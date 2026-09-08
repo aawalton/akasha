de10a348c7a201ede5068f2a5c7f5bda8b41581a
Akasha <akasha@alanwalton.com>
2026-09-08 01:08:11 -0600

    royal road sync 1 page(s)

---
+++ story/story-chapters-read/pages/bunny-girl-evolution-0034-222-tour-guide-elises-cavern-tour/bunny-girl-evolution-0034-222-tour-guide-elises-cavern-tour.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const bunnyGirlEvolution0034222TourGuideElisesCavernTour = {
+  id: "01a07fd8-7b74-79fa-b7ce-5857436a6ab4",
+  pageTypeSlug: "story-chapter-read",
+  slug: "bunny-girl-evolution-0034-222-tour-guide-elises-cavern-tour",
+  partOfSlugs: ["story-read/bunny-girl-evolution"],
+  position: 34,
+  publishedAt: "2026-09-08",
+  unitSlug: "words",
+  externalId: "3928777",
+  source: "royal-road",
+  title: "222 - Tour Guide Elise's Cavern Tour",
+  ownLength: 2524,
+  externalLink:
+    "https://www.royalroad.com/fiction/90435/bunny-girl-evolution/chapter/3928777/222-tour-guide-elises-cavern-tour",
+  prose: "txt",
+} as const satisfies StoryChapterRead
