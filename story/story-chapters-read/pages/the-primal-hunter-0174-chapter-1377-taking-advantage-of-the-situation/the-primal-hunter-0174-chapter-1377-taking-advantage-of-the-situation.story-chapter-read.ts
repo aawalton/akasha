eac548d36f2ec60638d7bec12a28d5faa4241450
Akasha <akasha@alanwalton.com>
2026-09-08 10:03:43 -0600

    royal road sync 1 page(s)

---
+++ story/story-chapters-read/pages/the-primal-hunter-0174-chapter-1377-taking-advantage-of-the-situation/the-primal-hunter-0174-chapter-1377-taking-advantage-of-the-situation.story-chapter-read.ts
@@ -0,0 +1,17 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const thePrimalHunter0174Chapter1377TakingAdvantageOfTheSituation = {
+  pageTypeSlug: "story-chapter-read",
+  slug: "the-primal-hunter-0174-chapter-1377-taking-advantage-of-the-situation",
+  partOfSlugs: ["story-read/the-primal-hunter"],
+  position: 174,
+  publishedAt: "2026-09-08",
+  unitSlug: "words",
+  externalId: "3930294",
+  source: "royal-road",
+  title: "Chapter 1377 - Taking Advantage of the Situation",
+  ownLength: 2699,
+  externalLink:
+    "https://www.royalroad.com/fiction/36049/the-primal-hunter/chapter/3930294/chapter-1377-taking-advantage-of-the-situation",
+  prose: "txt",
+} as const satisfies StoryChapterRead
