c904463db9ecb0f84f60fc6af77059cc1adfc6e6
Akasha <akasha@alanwalton.com>
2026-09-08 12:09:44 -0600

    apply alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-best-loved-american-folk-songs-this-land-is-your-land-2024-remastered/the-tabernacle-choir-at-temple-square-best-loved-american-folk-songs-this-land-is-your-land-2024-remastered.release.ts, alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-best-loved-american-folk-songs-this-land-is-your-land-2024-remastered/the-tabernacle-choir-at-temple-square-best-loved-american-folk-songs.release.ts

---
+++ alan/music/catalog/releases/pages/the-tabernacle-choir-at-temple-square-best-loved-american-folk-songs-this-land-is-your-land-2024-remastered/the-tabernacle-choir-at-temple-square-best-loved-american-folk-songs.release.ts
@@ -0,0 +1,18 @@
+import type { Release } from "../../release.page-type.ts"
+
+export const theTabernacleChoirAtTempleSquareBestLovedAmericanFolkSongs = {
+  id: "01a0676a-d718-7043-8775-61ef08154f16",
+  pageTypeSlug: "release",
+  slug: "the-tabernacle-choir-at-temple-square-best-loved-american-folk-songs",
+  title: "Best Loved American Folk Songs: This Land is your Land (2024 Remastered)",
+  partOfSlugs: ["the-tabernacle-choir-at-temple-square"],
+  position: 0,
+  ownLength: 42.859267,
+  ownProgress: 42.859267,
+  unitSlug: "minutes",
+  status: "completed",
+  publishedAt: "1966-08-16",
+  externalId: "0wSCGEsqHqTSChM7tyMsIZ",
+  externalLink: "https://open.spotify.com/album/0wSCGEsqHqTSChM7tyMsIZ",
+  lastSyncedAt: "2025-11-27",
+} as const satisfies Release
