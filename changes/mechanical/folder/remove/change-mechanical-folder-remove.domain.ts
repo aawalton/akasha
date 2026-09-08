cd7261a01c6eb7a9b05fe4fbfc12be76d13d2eb6
Aine <aine@alanwalton.com>
2026-09-08 11:44:49 -0600

    file a page type's removal under file rather than folder, its work being a file's

--- changes/mechanical/folder/remove/change-mechanical-folder-remove.domain.ts
+++ changes/mechanical/folder/remove/change-mechanical-folder-remove.domain.ts
@@ -5,5 +5,5 @@ export const changeMechanicalFolderRemove = {
   pageTypeSlug: "domain",
   slug: "change-mechanical-folder-remove",
   definition: "a mechanical change taking a folder away",
-  partSlugs: ["change-mechanical-folder/remove-page-type"],
+  partSlugs: [],
 } as const satisfies Domain
