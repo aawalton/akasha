24b8732cf58d7aa38b1888184018bb35f7af2989
Aine <aine@alanwalton.com>
2026-09-07 20:13:35 -0600

    the patch this agent drafted

--- changes/agent/file-content/add-property-value/add-property-value.change-checked.test.ts
+++ changes/agent/file-content/add-property-value/add-property-value.change-checked.test.ts
@@ -37,6 +37,7 @@ function worldTold(told: Told): World {
     root: "/nowhere",
     index: { knownIn: () => known, pageAt: () => ("page" in told ? told.page : PAGE) } as never,
     textOf: () => null,
+    base: () => null,
     over: NOTHING_OVER,
   }
 }
