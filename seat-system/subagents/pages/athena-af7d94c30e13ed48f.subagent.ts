0332110ceb893c48512f805cdc9441a793c134f3
Akasha <akasha@alanwalton.com>
2026-09-08 09:26:04 -0600

    athena-af7d94c30e13ed48f: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/athena-af7d94c30e13ed48f.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const athenaAf7d94c30e13ed48f = {
+  id: "01a081a0-541d-7cf8-835d-dce2be5392c8",
+  pageTypeSlug: "subagent",
+  slug: "athena-af7d94c30e13ed48f",
+  principalSeatName: "athena",
+  assignmentSlug: "initiative/athena-commands-at-the-root",
+  dispatchedAs: "Explore",
+  agentId: "01a08197-ca18-7000-96a6-c880b5f44819--af7d94c30e13ed48f",
+} as const satisfies Subagent
