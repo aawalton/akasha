ed533c867f69079d4a832e4d548f762ab19209de
Aranya <aranya@alanwalton.com>
2026-09-08 11:00:00 -0600

    the cluster-rbac-manifest command goes, the pipeline engine it wrote permissions for being ablated

--- command-system/commands/command.page-type.ts
+++ command-system/commands/command.page-type.ts
@@ -93,7 +93,6 @@ export const command = {
     "namespace/inference",
     "namespace/mobile",
     "namespace/talos",
-    "command/cluster-rbac-manifest",
     "namespace/page",
     "namespace/browser",
     "namespace/sms",
