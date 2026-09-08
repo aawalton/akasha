ddec496bd0e63c48e03af6ff38c517d243846d3b
Akasha <akasha@alanwalton.com>
2026-09-08 12:07:14 -0600

    apply alan/music/catalog/releases/pages/the-lord-of-the-rings-music-the-hobbit-the-battle-of-the-five-armies-original-motion-picture-soundtrack-special-edition/the-lord-of-the-rings-music-the-hobbit-the-battle-of-the-five-armies-original-motion-picture-soundtrack-special-edition.release.ts, alan/music/catalog/releases/pages/the-lord-of-the-rings-music-the-hobbit-the-battle-of-the-five-armies-original-motion-picture-soundtrack-special-edition/the-lord-of-the-rings-music-the-hobbit-the-battle-of-the-five-armies.release.ts

---
+++ alan/music/catalog/releases/pages/the-lord-of-the-rings-music-the-hobbit-the-battle-of-the-five-armies-original-motion-picture-soundtrack-special-edition/the-lord-of-the-rings-music-the-hobbit-the-battle-of-the-five-armies.release.ts
@@ -0,0 +1,19 @@
+import type { Release } from "../../release.page-type.ts"
+
+export const theLordOfTheRingsMusicTheHobbitTheBattleOfTheFiveArmies = {
+  id: "01a0676a-d72d-7025-98af-9285ee3fb8e3",
+  pageTypeSlug: "release",
+  slug: "the-lord-of-the-rings-music-the-hobbit-the-battle-of-the-five-armies",
+  title:
+    "The Hobbit: The Battle of the Five Armies (Original Motion Picture Soundtrack) [Special Edition]",
+  partOfSlugs: ["the-lord-of-the-rings-music"],
+  position: 6,
+  ownLength: 108.639217,
+  ownProgress: 0,
+  unitSlug: "minutes",
+  status: "not-started",
+  publishedAt: "2014-12-23",
+  externalId: "7o3egq23BKR7OCn5dlpFan",
+  externalLink: "https://open.spotify.com/album/7o3egq23BKR7OCn5dlpFan",
+  lastSyncedAt: "2025-11-27",
+} as const satisfies Release
