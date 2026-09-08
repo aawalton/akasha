45cad72666045f6af34a20e6475eafc27dd12594
Akasha <akasha@alanwalton.com>
2026-09-08 10:35:08 -0600

    amy-a6bf14cf477058996: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/amy-a6bf14cf477058996.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const amyA6bf14cf477058996 = {
+  id: "01a081df-9382-757e-ab07-6e3949a15283",
+  pageTypeSlug: "subagent",
+  slug: "amy-a6bf14cf477058996",
+  principalSeatName: "amy",
+  assignmentSlug: "initiative/amy-work-panel-editing",
+  dispatchedAs: "Explore",
+  agentId: "01a07e83-7b6c-7000-a876-5c2d08753493--a6bf14cf477058996",
+} as const satisfies Subagent
