7a4b1f7386f7483cdd6b7f0e71f8659bb518cc21
Athena <athena@alanwalton.com>
2026-09-08 10:12:12 -0600

    email-drafts' commands sit under their namespace's folder

--- commands/pages/email/email-drafts/email-drafts-list/email-drafts-list.command.code.ts
+++ commands/pages/email/email-drafts/email-drafts-list/email-drafts-list.command.code.ts
@@ -8,7 +8,7 @@ import {
   type Read,
   readTaking,
   refusing,
-} from "../../../google/email/commands/email-command-reading/email-command-reading.module.code.ts"
+} from "../../../../../google/email/commands/email-command-reading/email-command-reading.module.code.ts"
 
 const TAKING = { valued: [MAX], numbered: [MAX] } as const
 
