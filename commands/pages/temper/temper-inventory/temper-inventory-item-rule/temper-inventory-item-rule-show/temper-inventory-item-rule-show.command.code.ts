ccaf9e77ab9d643c2b9e0fc8630bf90f28468263
Athena <athena@alanwalton.com>
2026-09-08 10:18:30 -0600

    temper-inventory-item-rule's commands sit under their namespace's folder

--- commands/pages/temper/temper-inventory/temper-inventory-item-rule/temper-inventory-item-rule-show/temper-inventory-item-rule-show.command.code.ts
+++ commands/pages/temper/temper-inventory/temper-inventory-item-rule/temper-inventory-item-rule-show/temper-inventory-item-rule-show.command.code.ts
@@ -6,7 +6,7 @@ import {
   shapeOf,
   shownRule,
   TSV,
-} from "../../../temper/temper-commands/inventory-rule-calling/inventory-rule-calling.module.code.ts"
+} from "../../../../../../temper/temper-commands/inventory-rule-calling/inventory-rule-calling.module.code.ts"
 
 const CALLED_AS = "akasha temper-inventory-item-rule-show"
 
