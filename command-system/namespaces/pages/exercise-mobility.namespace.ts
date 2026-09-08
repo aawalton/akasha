e3e001f68a6c036ebb8ce895dc2c245d6b2384aa
Aelwyn <aelwyn@alanwalton.com>
2026-09-08 07:10:13 -0600

    apply command-system/namespaces/pages/exercise-mobility.namespace.ts, commands/pages/exercise-mobility-show/exercise-mobility-show.command.code.ts, commands/pages/exercise-mobility-show/exercise-mobility-show.command.ts

--- command-system/namespaces/pages/exercise-mobility.namespace.ts
+++ command-system/namespaces/pages/exercise-mobility.namespace.ts
@@ -5,5 +5,5 @@ export const exerciseMobility = {
   pageTypeSlug: "namespace",
   slug: "exercise-mobility",
   definition: "how far Alan's joints move",
-  partSlugs: ["command/exercise-mobility-show"],
+  partSlugs: [],
 } as const satisfies Namespace
