04b13da7775b4fa58ffd002ceda4404a43a60c39
Athena <athena@alanwalton.com>
2026-09-08 14:00:32 -0600

    the temper-inventory-bank commands sit in the commands folder at the root

--- commands/pages/temper/inventory/bank/trace/temper-inventory-bank-trace.command.code.ts
+++ commands/pages/temper/inventory/bank/trace/temper-inventory-bank-trace.command.code.ts
@@ -3,7 +3,7 @@ import type { Answer, Given } from "@akasha/command-system/calling"
 import { refused } from "@akasha/command-system/calling"
 import { whyOf } from "@akasha/command-system/fault-saying"
 import { savedVarsFile } from "@akasha/temper-eso-paths/eso-paths-resolve"
-import { readBankTrace } from "../bank-trace-reading/bank-trace-reading.module.code.ts"
+import { readBankTrace } from "../../../../../../temper/temper-commands/bank-trace-reading/bank-trace-reading.module.code.ts"
 
 const INPUT = 1
 
