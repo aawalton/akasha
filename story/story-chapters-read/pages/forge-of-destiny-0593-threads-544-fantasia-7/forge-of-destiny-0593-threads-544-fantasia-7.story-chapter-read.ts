c1c5512b9a54c630ff7ce68bd7c136ad9c060b1f
Akasha <akasha@alanwalton.com>
2026-09-07 22:07:13 -0600

    royal road sync 1 page(s)

---
+++ story/story-chapters-read/pages/forge-of-destiny-0593-threads-544-fantasia-7/forge-of-destiny-0593-threads-544-fantasia-7.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const forgeOfDestiny0593Threads544Fantasia7 = {
+  id: "01a07f32-cebd-7853-8fca-9504a2791204",
+  pageTypeSlug: "story-chapter-read",
+  slug: "forge-of-destiny-0593-threads-544-fantasia-7",
+  partOfSlugs: ["story-read/forge-of-destiny"],
+  position: 593,
+  publishedAt: "2026-09-08",
+  unitSlug: "words",
+  externalId: "3928325",
+  source: "royal-road",
+  title: "Threads 544-Fantasia 7",
+  ownLength: 2519,
+  externalLink:
+    "https://www.royalroad.com/fiction/21188/forge-of-destiny/chapter/3928325/threads-544-fantasia-7",
+  prose: "txt",
+} as const satisfies StoryChapterRead
