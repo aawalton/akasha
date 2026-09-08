4126075e875f60aadeaaf1dc340924c03d6a87dd
Thea <thea@alanwalton.com>
2026-09-08 06:10:37 -0600

    builtin-symbol: an unsupported Symbol method answers nothing rather than falling out

--- language-design/lua-compiler/builtin-symbol/builtin-symbol.module.code.ts
+++ language-design/lua-compiler/builtin-symbol/builtin-symbol.module.code.ts
@@ -25,5 +25,6 @@ export function transformSymbolConstructorCall(
     }
     default:
       context.addDiagnostic(unsupportedProperty(calledMethod.name, "Symbol", methodName))
+      return undefined
   }
 }
