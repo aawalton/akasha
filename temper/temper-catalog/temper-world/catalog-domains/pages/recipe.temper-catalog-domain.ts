febfc5cbbdf565075a93f55d5b06a9604ff72d4b
pages-access <pages-access@alanwalton.com>
2026-09-08 11:39:01 -0600

    patchPage(temper-catalog-domain): recipe
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/temper-catalog/temper-world/catalog-domains/pages/recipe.temper-catalog-domain.ts
+++ temper/temper-catalog/temper-world/catalog-domains/pages/recipe.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const recipe = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-08T16:45:08.474Z",
+  capturedAt: "2026-09-08T17:38:59.848Z",
   generatorRanForVersion: "eso.live.12.0.6.3274791",
   generatorRanForManifestApiVersion: 101050,
 } as const satisfies TemperCatalogDomain
