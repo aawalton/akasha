e1605116ba3abd065e5cbea8ece839ff6704927b
Athena <athena@alanwalton.com>
2026-09-08 10:06:45 -0600

    domain's commands sit under their namespace's folder

--- commands/pages/domain/domain.namespace.ts
+++ commands/pages/domain/domain.namespace.ts
@@ -1,4 +1,4 @@
-import type { Namespace } from "../namespace.page-type.ts"
+import type { Namespace } from "../../../command-system/namespaces/namespace.page-type.ts"
 
 export const domain = {
   id: "01a07c03-e381-7406-89c5-7c74061fd947",
