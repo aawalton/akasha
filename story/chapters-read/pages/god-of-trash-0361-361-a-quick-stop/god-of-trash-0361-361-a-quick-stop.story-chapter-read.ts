cb1463519575c209da08f2cd7d14f90b44fadbbc
Akasha <akasha@alanwalton.com>
2026-09-09 14:06:07 -0600

    royal road sync 2 page(s)

---
+++ story/chapters-read/pages/god-of-trash-0361-361-a-quick-stop/god-of-trash-0361-361-a-quick-stop.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const godOfTrash0361361AQuickStop = {
+  id: "01a087c7-12ee-7014-b16e-5070346f691b",
+  pageTypeSlug: "story-chapter-read",
+  slug: "god-of-trash-0361-361-a-quick-stop",
+  position: 361,
+  publishedAt: "2026-09-09",
+  unit: "words",
+  externalId: "3936264",
+  source: "royal-road",
+  title: "361. A Quick Stop",
+  story: "story-read/god-of-trash",
+  ownLength: 2045,
+  externalLink:
+    "https://www.royalroad.com/fiction/107252/god-of-trash-cultivation-litrpg-from-trash-tier/chapter/3936264/361-a-quick-stop",
+  prose: "txt",
+} as const satisfies StoryChapterRead
---
+++ story/chapters-read/pages/reborn-as-a-demonic-tree-0139-chapter-615-the-first-mythical-skill/reborn-as-a-demonic-tree-0139-chapter-615-the-first-mythical-skill.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const rebornAsADemonicTree0139Chapter615TheFirstMythicalSkill = {
+  id: "01a087c7-1436-7bdc-a6f8-242fd4446b31",
+  pageTypeSlug: "story-chapter-read",
+  slug: "reborn-as-a-demonic-tree-0139-chapter-615-the-first-mythical-skill",
+  position: 139,
+  publishedAt: "2026-09-09",
+  unit: "words",
+  externalId: "3936083",
+  source: "royal-road",
+  title: "Chapter 615: The First Mythical Skill",
+  story: "story-read/reborn-as-a-demonic-tree",
+  ownLength: 2254,
+  externalLink:
+    "https://www.royalroad.com/fiction/62881/reborn-as-a-demonic-tree/chapter/3936083/chapter-615-the-first-mythical-skill",
+  prose: "txt",
+} as const satisfies StoryChapterRead
