ddfa00446ec49244cf30d4b71846876d82c46478
Akasha <akasha@alanwalton.com>
2026-09-09 13:22:13 -0600

    stage temper/temper-catalog/temper-companions under temper/catalogs on the way to renaming the catalog folder

--- temper/catalogs/temper-companions/companion-skills/properties/effect-debuff.record-property.ts
+++ temper/catalogs/temper-companions/companion-skills/properties/effect-debuff.record-property.ts
@@ -1,6 +1,6 @@
 import type { RecordProperty } from "@akasha/pages/record-property"
-import type { EffectValue } from "../../../temper-catalog-things/properties/effect-value.number-property.ts"
-import type { ValueType } from "../../../temper-catalog-things/properties/value-type.text-property.ts"
+import type { EffectValue } from "../../../../temper-catalog/temper-catalog-things/properties/effect-value.number-property.ts"
+import type { ValueType } from "../../../../temper-catalog/temper-catalog-things/properties/value-type.text-property.ts"
 import type { DebuffName } from "./debuff-name.text-property.ts"
 import type { Duration } from "./duration.number-property.ts"
 
