e8462ee5141ea93ee2e7cc87d19855b741a2988c
Akasha <akasha@alanwalton.com>
2026-09-08 18:02:22 -0600

    supervisor-console-ryn-2026-09-09: one source's lines for one seat on one day

---
+++ seat-system/seat-log-days/pages/supervisor-console-ryn-2026-09-09.seat-log-day.ts
@@ -0,0 +1,10 @@
+import type { SeatLogDay } from "../seat-log-day.page-type.ts"
+
+export const supervisorConsoleRyn20260909 = {
+  id: "01a08379-06da-7671-b42f-8e35f9836e8a",
+  pageTypeSlug: "seat-log-day",
+  slug: "supervisor-console-ryn-2026-09-09",
+  sourceSlug: "supervisor-console",
+  seatName: "ryn",
+  date: "2026-09-09",
+} as const satisfies SeatLogDay
