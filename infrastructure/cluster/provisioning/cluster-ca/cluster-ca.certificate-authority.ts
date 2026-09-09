bd14aa9317ccf3d5b8466a1c71ad2e77bdd1f70e
Akasha <akasha@alanwalton.com>
2026-09-09 16:35:29 -0600

    certificate-authorities moves under certificates, the page that declares it a part

--- infrastructure/cluster/provisioning/cluster-ca/cluster-ca.certificate-authority.ts
+++ infrastructure/cluster/provisioning/cluster-ca/cluster-ca.certificate-authority.ts
@@ -1,4 +1,4 @@
-import type { CertificateAuthority } from "../../../certificate-authorities/certificate-authority.page-type.ts"
+import type { CertificateAuthority } from "../../../certificates/authorities/certificate-authority.page-type.ts"
 
 export const clusterCa = {
   id: "01a0685e-1c21-7b95-8c04-9d5a6e2f31b7",
