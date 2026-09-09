2dad73f98921cac903b978eadbfd5b8780d49058
Akasha <akasha@alanwalton.com>
2026-09-09 00:05:59 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/zero-day-saint-i-cheat-smarter-shapeshifter-litrpg-0082-082-the-gifted-child/zero-day-saint-i-cheat-smarter-shapeshifter-litrpg-0082-082-the-gifted-child.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const zeroDaySaintICheatSmarterShapeshifterLitrpg0082082TheGiftedChild = {
+  id: "01a084c5-ebfe-702f-a024-a53dbd920b69",
+  pageTypeSlug: "story-chapter-read",
+  slug: "zero-day-saint-i-cheat-smarter-shapeshifter-litrpg-0082-082-the-gifted-child",
+  position: 82,
+  publishedAt: "2026-09-09",
+  unitSlug: "words",
+  externalId: "3933137",
+  source: "royal-road",
+  title: "082 - The Gifted Child",
+  storySlug: "story-read/zero-day-saint-i-cheat-smarter-shapeshifter-litrpg",
+  ownLength: 2311,
+  externalLink:
+    "https://www.royalroad.com/fiction/164273/zero-day-saint-litrpgvol-1-complete/chapter/3933137/082-the-gifted-child",
+  prose: "txt",
+} as const satisfies StoryChapterRead
