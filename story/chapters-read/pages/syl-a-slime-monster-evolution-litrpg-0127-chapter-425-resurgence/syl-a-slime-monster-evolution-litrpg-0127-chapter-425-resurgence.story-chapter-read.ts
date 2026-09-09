02a425d97cc10e916216b92d8088d81ea8f9d0b9
Akasha <akasha@alanwalton.com>
2026-09-09 01:07:22 -0600

    royal road sync 2 page(s)

---
+++ story/chapters-read/pages/syl-a-slime-monster-evolution-litrpg-0127-chapter-425-resurgence/syl-a-slime-monster-evolution-litrpg-0127-chapter-425-resurgence.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const sylASlimeMonsterEvolutionLitrpg0127Chapter425Resurgence = {
+  id: "01a084fe-1c6c-7ed4-94bd-f43a1c69e24c",
+  pageTypeSlug: "story-chapter-read",
+  slug: "syl-a-slime-monster-evolution-litrpg-0127-chapter-425-resurgence",
+  position: 127,
+  publishedAt: "2026-09-09",
+  unitSlug: "words",
+  externalId: "3933175",
+  source: "royal-road",
+  title: "Chapter 425 : Resurgence",
+  storySlug: "story-read/syl-a-slime-monster-evolution-litrpg",
+  ownLength: 2198,
+  externalLink:
+    "https://www.royalroad.com/fiction/77972/syl-book-5-stub-sep-9th/chapter/3933175/chapter-425-resurgence",
+  prose: "txt",
+} as const satisfies StoryChapterRead
---
+++ story/chapters-read/pages/the-magpie-and-the-archmage-monster-evo-litrpg-0176-chapter-176-price-of-hospitality/the-magpie-and-the-archmage-monster-evo-litrpg-0176-chapter-176-price-of-hospitality.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const theMagpieAndTheArchmageMonsterEvoLitrpg0176Chapter176PriceOfHospitality = {
+  id: "01a084fe-1d93-7843-a468-262d9098ffb8",
+  pageTypeSlug: "story-chapter-read",
+  slug: "the-magpie-and-the-archmage-monster-evo-litrpg-0176-chapter-176-price-of-hospitality",
+  position: 176,
+  publishedAt: "2026-09-09",
+  unitSlug: "words",
+  externalId: "3933172",
+  source: "royal-road",
+  title: "Chapter 176 – Price of Hospitality",
+  storySlug: "story-read/the-magpie-and-the-archmage-monster-evo-litrpg",
+  ownLength: 1988,
+  externalLink:
+    "https://www.royalroad.com/fiction/129182/the-magpie-and-the-archmage-book-2-complete/chapter/3933172/chapter-176-price-of-hospitality",
+  prose: "txt",
+} as const satisfies StoryChapterRead
