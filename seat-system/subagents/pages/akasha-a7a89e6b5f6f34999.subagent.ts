ad31dd08009101708a6bc4e5479a4f84d93a1099
Akasha <akasha@alanwalton.com>
2026-09-09 16:03:19 -0600

    akasha-a7a89e6b5f6f34999: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/akasha-a7a89e6b5f6f34999.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const akashaA7a89e6b5f6f34999 = {
+  id: "01a08819-7e37-75ec-86c6-844d032d87cb",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "akasha-a7a89e6b5f6f34999",
+  principalSeatName: "akasha",
+  assignmentSlug: "initiative/akasha-folder-shape",
+  dispatchedAs: "general-purpose",
+  agentId: "01a07c15-3e52-7000-ad7b-1c746247704d--a7a89e6b5f6f34999",
+} as const satisfies Subagent
