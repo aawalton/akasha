87998b448cc3f2dbd38fabfafcad31cad0e74c7b
Akasha <akasha@alanwalton.com>
2026-09-09 16:21:47 -0600

    aine-ad5ded817fe611048: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/aine-ad5ded817fe611048.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const aineAd5ded817fe611048 = {
+  id: "01a08843-4cd5-73f8-8b59-4598e0cc6224",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "aine-ad5ded817fe611048",
+  principalSeatName: "aine",
+  assignmentSlug: "page-type/finding",
+  dispatchedAs: "Explore",
+  agentId: "01a0872c-a53a-7000-96eb-4f0dd173f4c2--ad5ded817fe611048",
+} as const satisfies Subagent
