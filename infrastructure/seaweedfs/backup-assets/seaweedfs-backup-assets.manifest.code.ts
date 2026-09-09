bf51d71b9b72e162b60e7a9047485fd030d93e23
Akasha <akasha@alanwalton.com>
2026-09-09 15:35:22 -0600

    apply infrastructure/seaweedfs/backup-assets/seaweedfs-backup-assets.manifest.code.ts, infrastructure/seaweedfs/longtail-assets/seaweedfs-longtail-assets.module.code.ts, infrastructure/seaweedfs/longtail-assets/seaweedfs-longtail-assets.module.ts

--- infrastructure/seaweedfs/backup-assets/seaweedfs-backup-assets.manifest.code.ts
+++ infrastructure/seaweedfs/backup-assets/seaweedfs-backup-assets.manifest.code.ts
@@ -3,11 +3,11 @@ import {
   backupPvYaml,
   COMPONENT_BACKUP,
 } from "../backup-manifests/seaweedfs-backup-manifests.module.code.ts"
-import { namespaceYaml } from "../namespace/seaweedfs-namespace.module.code.ts"
 import {
   ASSETS_NAMESPACE,
   backupAssetsCronJobYaml,
-} from "../seaweedfs-longtail-assets/seaweedfs-longtail-assets.module.code.ts"
+} from "../longtail-assets/seaweedfs-longtail-assets.module.code.ts"
+import { namespaceYaml } from "../namespace/seaweedfs-namespace.module.code.ts"
 
 export default function synth(): readonly { readonly name: string; readonly yaml: string }[] {
   return [
