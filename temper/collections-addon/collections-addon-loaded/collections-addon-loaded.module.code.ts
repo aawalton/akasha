b0ba66aaa140a31922beb6ff4cb7488e98530ebe
Akasha <akasha@alanwalton.com>
2026-09-09 16:22:32 -0600

    move temper-item-browser into temper/item-browser so the folder no longer opens with the slug of the page above it

--- temper/collections-addon/collections-addon-loaded/collections-addon-loaded.module.code.ts
+++ temper/collections-addon/collections-addon-loaded/collections-addon-loaded.module.code.ts
@@ -1,7 +1,7 @@
 import { initDungeonChampions } from "akasha/temper/dungeon-champions/dungeon-champion-start/dungeon-champion-start.module.code.ts"
+import { initItemBrowser } from "akasha/temper/item-browser/item-browser-start/item-browser-start.module.code.ts"
 import { initLoreBooks } from "akasha/temper/lorebooks/lorebooks-init/lorebooks-init.module.code.ts"
 import { initLostTreasure } from "akasha/temper/lost-treasure/lost-treasure-start/lost-treasure-start.module.code.ts"
-import { initItemBrowser } from "akasha/temper/temper-item-browser/item-browser-start/item-browser-start.module.code.ts"
 import { initSkyShards } from "akasha/temper/temper-skyshards/skyshards-start/skyshards-start.module.code.ts"
 
 export function onAddOnLoaded(this: void): undefined {
