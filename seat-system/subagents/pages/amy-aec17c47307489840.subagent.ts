7ae0528c13665fc14e59c9ef623d654227617ca2
Akasha <akasha@alanwalton.com>
2026-09-09 13:55:21 -0600

    amy-aec17c47307489840: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/amy-aec17c47307489840.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const amyAec17c47307489840 = {
+  id: "01a087ba-8624-7ce7-a788-37ac5ca17183",
+  pageTypeSlug: "subagent",
+  slug: "amy-aec17c47307489840",
+  principalSeatName: "amy",
+  assignmentSlug: "initiative/amy-harness-improvements",
+  dispatchedAs: "Explore",
+  agentId: "01a087b5-1ca3-7000-bf5c-16aae1620c39--aec17c47307489840",
+} as const satisfies Subagent
