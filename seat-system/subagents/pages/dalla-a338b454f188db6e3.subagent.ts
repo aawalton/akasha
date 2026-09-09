510981fd5dfcb7c8d60784c7a4cf10611911caa5
Akasha <akasha@alanwalton.com>
2026-09-08 18:07:59 -0600

    dalla-a338b454f188db6e3: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/dalla-a338b454f188db6e3.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "../subagent.page-type.ts"
+
+export const dallaA338b454f188db6e3 = {
+  id: "01a082d8-d717-7302-a94d-ecb7f53bebdb",
+  pageTypeSlug: "subagent",
+  slug: "dalla-a338b454f188db6e3",
+  principalSeatName: "dalla",
+  assignmentSlug: "initiative/dalla-draft-into-a-patch",
+  dispatchedAs: "Explore",
+  agentId: "01a07eb0-c517-7000-9ee4-cfc39576ac24--a338b454f188db6e3",
+} as const satisfies Subagent
