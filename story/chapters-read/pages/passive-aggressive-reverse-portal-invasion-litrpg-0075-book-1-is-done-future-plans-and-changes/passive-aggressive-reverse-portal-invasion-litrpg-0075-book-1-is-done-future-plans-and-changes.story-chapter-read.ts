2ae7013e3d95891130347e173f361c3100f9b8d8
Akasha <akasha@alanwalton.com>
2026-09-09 04:04:14 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/passive-aggressive-reverse-portal-invasion-litrpg-0075-book-1-is-done-future-plans-and-changes/passive-aggressive-reverse-portal-invasion-litrpg-0075-book-1-is-done-future-plans-and-changes.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const passiveAggressiveReversePortalInvasionLitrpg0075Book1IsDoneFuturePlansAndChanges = {
+  id: "01a085a0-09a6-7442-9f3c-53018436d94f",
+  pageTypeSlug: "story-chapter-read",
+  slug: "passive-aggressive-reverse-portal-invasion-litrpg-0075-book-1-is-done-future-plans-and-changes",
+  position: 75,
+  publishedAt: "2026-09-09",
+  unitSlug: "words",
+  externalId: "3933672",
+  source: "royal-road",
+  title: "Book 1 is done! Future plans and changes",
+  storySlug: "story-read/passive-aggressive-reverse-portal-invasion-litrpg",
+  ownLength: 255,
+  externalLink:
+    "https://www.royalroad.com/fiction/170563/passive-aggressive-reverse-portal-invasion-litrpg/chapter/3933672/book-1-is-done-future-plans-and-changes",
+  prose: "txt",
+} as const satisfies StoryChapterRead
