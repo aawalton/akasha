232c3c2d49106b0726f7b46ebd00d8f99de7dfaf
temper watcher <watcher@alanwalton.com>
2026-09-09 14:18:23 -0600

    temper: cadwell-s-almanac came round again on what its characters did
    Written-by: temper watcher <watcher@alanwalton.com>

--- temper/temper-progress/tasks/pages/cadwell-s-almanac/cadwell-s-almanac.temper-task.ts
+++ temper/temper-progress/tasks/pages/cadwell-s-almanac/cadwell-s-almanac.temper-task.ts
@@ -10,7 +10,7 @@ export const cadwellSAlmanac = {
   esoCharacterId: "8796093041077613",
   scope: "next_character",
   priority: "p3",
-  dueDate: "2026-09-09",
+  dueDate: "2026-09-10",
   displayOrder: 13,
   completionCardId: "cadwells-almanac",
   lastCompletedAt: "2026-08-19T14:32:40.000Z",
