5f88f8eb014d6cfa8ac321781ee45bc12809e018
Akasha <akasha@alanwalton.com>
2026-09-09 15:23:40 -0600

    name the mine page at the path it now has in the two modules reading it

--- temper/temper-addon-data/mined-restore-potions/mined-restore-potions.module.code.ts
+++ temper/temper-addon-data/mined-restore-potions/mined-restore-potions.module.code.ts
@@ -5,7 +5,7 @@ import { partAt } from "@akasha/pages/page-file-parts"
 import type { MinedRestorePotion } from "../potion-restore-metrics/potion-restore-metrics.module.code.ts"
 
 const MINE_PAGES: readonly string[] = [
-  "temper/characters/mines/pages/eso/eso.temper-mine.ts",
+  "temper/characters/temper-mines/pages/eso/eso.temper-mine.ts",
   "pages/temper-mine/eso.temper-mine.md",
 ]
 
