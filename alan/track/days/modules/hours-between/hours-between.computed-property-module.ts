8db17d01c176d3382e81670944614bf04b1e34bf
Akasha <akasha@alanwalton.com>
2026-09-09 17:09:55 -0600

    apply alan/track/days/modules/hours-between/hours-between.computed-property-module.ts

--- alan/track/days/modules/hours-between/hours-between.computed-property-module.ts
+++ alan/track/days/modules/hours-between/hours-between.computed-property-module.ts
@@ -3,6 +3,7 @@ import type { ComputedPropertyModule } from "@akasha/pages/computed-property-mod
 export const hoursBetween = {
   id: "01a0821f-8bbc-7772-a9fa-052b5f4ffb2b",
   pageTypeSlug: "computed-property-module",
+  type: "computed-property-module",
   slug: "hours-between",
   definition: "the hours from one instant to another",
   code: "ts",
