24b11342d9888041ae1f3dfc62009d4050081f14
Athena <athena@alanwalton.com>
2026-09-09 14:30:22 -0600

    the command-measuring module sits in a folder named for its slug, which is what the folder shape asks

--- commands/pages/measure/commands/measure-commands.command.code.ts
+++ commands/pages/measure/commands/measure-commands.command.code.ts
@@ -1,6 +1,9 @@
 import type { Answer, Given } from "../../../../command-system/calling/calling.module.code.ts"
 import { linesOf, windowIn } from "../../../modules/check-measuring/check-measuring.module.code.ts"
-import { COMMAND, costsIn } from "../../../modules/measuring/command-measuring.module.code.ts"
+import {
+  COMMAND,
+  costsIn,
+} from "../../../modules/command-measuring/command-measuring.module.code.ts"
 
 export function measureCommands(argv: readonly string[], given: Given): Answer {
   const chose = windowIn(argv)
