89574587f878fe6840c63c3ec42fd23ad203c3c4
alanwalton web <web@alanwalton.com>
2026-09-09 16:14:05 -0600

    a device secret is minted for alan
    Written-by: alanwalton web <web@alanwalton.com>

--- persons/device-secrets/pages/alan-ea94e1f6-d302-429c-b53e-38255a23abfe.device-secret.ts
+++ persons/device-secrets/pages/alan-ea94e1f6-d302-429c-b53e-38255a23abfe.device-secret.ts
@@ -6,5 +6,5 @@ export const alanEa94e1f6D302429cB53e38255a23abfe = {
   slug: "alan-ea94e1f6-d302-429c-b53e-38255a23abfe",
   userId: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   deviceId: "EA94E1F6-D302-429C-B53E-38255A23ABFE",
-  secretHash: "8572590cef27fc9aed2bab6358ce60242df2cab6ddd83adc9d08da0181318e84",
+  secretHash: "e9d562dd0c2c38dc07497cc0c8c1bdfa195387e5749bdbbfa48aff2502c89d29",
 } as const satisfies DeviceSecret
