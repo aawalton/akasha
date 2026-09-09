726fdd83bbcf22ff9548f81f64a7e78fba87f042
Akasha <akasha@alanwalton.com>
2026-09-09 17:05:20 -0600

    royal road sync 2 page(s)

---
+++ story/chapters-read/pages/five-element-overlord-i-can-upgrade-everything-0138-ch138-weighing-options/five-element-overlord-i-can-upgrade-everything-0138-ch138-weighing-options.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const fiveElementOverlordICanUpgradeEverything0138Ch138WeighingOptions = {
+  id: "01a0886b-28e5-7872-8502-ca3f3fa1aaf5",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "five-element-overlord-i-can-upgrade-everything-0138-ch138-weighing-options",
+  position: 138,
+  publishedAt: "2026-09-09",
+  unit: "words",
+  externalId: "3936872",
+  source: "royal-road",
+  title: "Ch138 Weighing Options",
+  story: "story-read/five-element-overlord-i-can-upgrade-everything",
+  ownLength: 1519,
+  externalLink:
+    "https://www.royalroad.com/fiction/159989/five-element-overlord-i-can-upgrade-everything/chapter/3936872/ch138-weighing-options",
+  prose: "txt",
+} as const satisfies StoryChapterRead
---
+++ story/chapters-read/pages/hell-difficulty-tutorial-0207-chapter-879-i-totally-forgot-about/hell-difficulty-tutorial-0207-chapter-879-i-totally-forgot-about.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const hellDifficultyTutorial0207Chapter879ITotallyForgotAbout = {
+  id: "01a0886b-2a43-7f44-a035-2b2db68b775a",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "hell-difficulty-tutorial-0207-chapter-879-i-totally-forgot-about",
+  position: 207,
+  publishedAt: "2026-09-09",
+  unit: "words",
+  externalId: "3936687",
+  source: "royal-road",
+  title: "Chapter 879 - I totally forgot about",
+  story: "story-read/hell-difficulty-tutorial",
+  ownLength: 2435,
+  externalLink:
+    "https://www.royalroad.com/fiction/64916/hell-difficulty-tutorial/chapter/3936687/chapter-879-i-totally-forgot-about",
+  prose: "txt",
+} as const satisfies StoryChapterRead
