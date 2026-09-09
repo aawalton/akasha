0f163ad2c4d744cfbd1233fc6670d93f19e4d09f
Akasha <akasha@alanwalton.com>
2026-09-08 21:03:24 -0600

    royal road sync 2 page(s)

---
+++ story/chapters-read/pages/demonic-city-builder-litrpg-city-kingdom-building-0027-chapter-65-primal/demonic-city-builder-litrpg-city-kingdom-building-0027-chapter-65-primal.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const demonicCityBuilderLitrpgCityKingdomBuilding0027Chapter65Primal = {
+  id: "01a0841e-bfee-7474-8de1-8a38cdefcd96",
+  pageTypeSlug: "story-chapter-read",
+  slug: "demonic-city-builder-litrpg-city-kingdom-building-0027-chapter-65-primal",
+  position: 27,
+  publishedAt: "2026-09-09",
+  unitSlug: "words",
+  externalId: "3932763",
+  source: "royal-road",
+  title: "Chapter 65 - Primal",
+  storySlug: "story-read/demonic-city-builder-litrpg-city-kingdom-building",
+  ownLength: 2282,
+  externalLink:
+    "https://www.royalroad.com/fiction/168862/demonic-city-builder-litrpg-city-kingdom-building/chapter/3932763/chapter-65-primal",
+  prose: "txt",
+} as const satisfies StoryChapterRead
---
+++ story/chapters-read/pages/every-trope-everywhere-all-at-once-litrpg-progression-0103-102-souls-intent/every-trope-everywhere-all-at-once-litrpg-progression-0103-102-souls-intent.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const everyTropeEverywhereAllAtOnceLitrpgProgression0103102SoulsIntent = {
+  id: "01a0841e-c129-7b1a-a29e-6f091029b097",
+  pageTypeSlug: "story-chapter-read",
+  slug: "every-trope-everywhere-all-at-once-litrpg-progression-0103-102-souls-intent",
+  position: 103,
+  publishedAt: "2026-09-09",
+  unitSlug: "words",
+  externalId: "3932668",
+  source: "royal-road",
+  title: "102 - Soul's Intent",
+  storySlug: "story-read/every-trope-everywhere-all-at-once-litrpg-progression",
+  ownLength: 1662,
+  externalLink:
+    "https://www.royalroad.com/fiction/166757/every-trope-everywhere-all-at-once-litrpg-progression/chapter/3932668/102-souls-intent",
+  prose: "txt",
+} as const satisfies StoryChapterRead
