5412d66b5140b0a0a42aef9e1193e8560c3076e6
Akasha <akasha@alanwalton.com>
2026-09-09 15:35:16 -0600

    apply infrastructure/seaweedfs/etcd-snapshot-manifests/seaweedfs-etcd-snapshot-manifests.module.code.ts, infrastructure/seaweedfs/etcd-snapshot-manifests/seaweedfs-etcd-snapshot-manifests.module.ts, infrastructure/seaweedfs/etcd-snapshot/seaweedfs-etcd-snapshot.manifest.code.ts

--- infrastructure/seaweedfs/etcd-snapshot/seaweedfs-etcd-snapshot.manifest.code.ts
+++ infrastructure/seaweedfs/etcd-snapshot/seaweedfs-etcd-snapshot.manifest.code.ts
@@ -1,4 +1,4 @@
-import { etcdSnapshotCronJobYaml } from "../seaweedfs-etcd-snapshot-manifests/seaweedfs-etcd-snapshot-manifests.module.code.ts"
+import { etcdSnapshotCronJobYaml } from "../etcd-snapshot-manifests/seaweedfs-etcd-snapshot-manifests.module.code.ts"
 
 export default function synth(): readonly { readonly name: string; readonly yaml: string }[] {
   return [{ name: "etcd-snapshot", yaml: etcdSnapshotCronJobYaml() }]
