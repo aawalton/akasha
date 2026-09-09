4b269daafff8c673d0ca0867a2aa64553d93522f
Akasha <akasha@alanwalton.com>
2026-09-09 16:23:08 -0600

    move temper-skyshards into temper/skyshards so the folder no longer opens with the slug of the page above it

--- temper/collections-addon/collections-addon-loaded/collections-addon-loaded.module.code.ts
+++ temper/collections-addon/collections-addon-loaded/collections-addon-loaded.module.code.ts
@@ -2,7 +2,7 @@ import { initDungeonChampions } from "akasha/temper/dungeon-champions/dungeon-ch
 import { initItemBrowser } from "akasha/temper/item-browser/item-browser-start/item-browser-start.module.code.ts"
 import { initLoreBooks } from "akasha/temper/lorebooks/lorebooks-init/lorebooks-init.module.code.ts"
 import { initLostTreasure } from "akasha/temper/lost-treasure/lost-treasure-start/lost-treasure-start.module.code.ts"
-import { initSkyShards } from "akasha/temper/temper-skyshards/skyshards-start/skyshards-start.module.code.ts"
+import { initSkyShards } from "akasha/temper/skyshards/skyshards-start/skyshards-start.module.code.ts"
 
 export function onAddOnLoaded(this: void): undefined {
   initSkyShards()
