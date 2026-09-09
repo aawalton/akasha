7a6371918b01d8429547229b49fb93efb4f9e5fa
Akasha <akasha@alanwalton.com>
2026-09-09 15:23:28 -0600

    name the temper-mine folder what its page calls its folder

--- temper/holdings-sets/sales/temper-sale.page-type.ts
+++ temper/holdings-sets/sales/temper-sale.page-type.ts
@@ -1,5 +1,5 @@
 import type { PageType } from "@akasha/pages/page-type"
-import type { ItemName } from "../../characters/mines/properties/item-name.text-property.ts"
+import type { ItemName } from "../../characters/temper-mines/properties/item-name.text-property.ts"
 import type { ItemId } from "../../temper-catalog/things/properties/item-id.number-property.ts"
 import type { TemperThing } from "../../things/temper-thing.page-type.ts"
 import type { BuyerName } from "./properties/buyer-name.text-property.ts"
