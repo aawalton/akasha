3a348982e7ec0d20cd4e0133a060ec1ed885667d
Akasha <akasha@alanwalton.com>
2026-09-09 16:21:53 -0600

    carry the page type onto every calendar time property page under the new `type` key

--- temper/temper-progress/temper-tasks/properties/due-time.calendar-time-property.ts
+++ temper/temper-progress/temper-tasks/properties/due-time.calendar-time-property.ts
@@ -5,6 +5,7 @@ export type DueTime = string
 export const dueTime = {
   id: "01a06d87-f1ed-793e-bf8f-a6f094353e9f",
   pageTypeSlug: "calendar-time-property",
+  type: "calendar-time-property",
   slug: "due-time",
   propertySlug: "due-time",
   definition: "the time of day a task is next wanted",
