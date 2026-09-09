28aa1b8875b9a54f34bb183e53112405a50a98aa
Akasha <akasha@alanwalton.com>
2026-09-09 02:06:51 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/flesh-eater-demon-evolution-litrpg-0125-chapter-122-clues/flesh-eater-demon-evolution-litrpg-0125-chapter-122-clues.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const fleshEaterDemonEvolutionLitrpg0125Chapter122Clues = {
+  id: "01a08534-943e-762b-ae8b-a09fcbebc062",
+  pageTypeSlug: "story-chapter-read",
+  slug: "flesh-eater-demon-evolution-litrpg-0125-chapter-122-clues",
+  position: 125,
+  publishedAt: "2026-09-09",
+  unitSlug: "words",
+  externalId: "3933381",
+  source: "royal-road",
+  title: "Chapter 122: Clues",
+  storySlug: "story-read/flesh-eater-demon-evolution-litrpg",
+  ownLength: 2364,
+  externalLink:
+    "https://www.royalroad.com/fiction/129187/flesh-eater-demon-evolution-litrpg-book-1-complete/chapter/3933381/chapter-122-clues",
+  prose: "txt",
+} as const satisfies StoryChapterRead
