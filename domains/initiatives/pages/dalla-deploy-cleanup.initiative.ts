f2d0d3bf05658b6b5588b4b8d27b7ce4933e987a
Dalla <dalla@alanwalton.com>
2026-09-10 13:38:39 -0600

    file the dalla-deploy-cleanup initiative

---
+++ domains/initiatives/pages/dalla-deploy-cleanup.initiative.ts
@@ -0,0 +1,10 @@
+import type { Initiative } from "../initiative.page-type.types.ts"
+
+export const dallaDeployCleanup = {
+  id: "01a08cd4-2398-7e2b-8600-bc6002b41fe7",
+  pageTypeSlug: "initiative",
+  type: "initiative",
+  slug: "dalla-deploy-cleanup",
+  domain: "page-type/change",
+  persona: "dalla",
+} as const satisfies Initiative
