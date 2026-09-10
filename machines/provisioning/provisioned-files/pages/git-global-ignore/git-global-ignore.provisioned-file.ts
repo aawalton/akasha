2890cf92b2d75c0d507adc602c65532f53aeed01
Akasha <akasha@alanwalton.com>
2026-09-09 19:16:05 -0600

    put the sixteen provisioned-file page folders back under the pages folder they belong in, where the move before this one left them at the repository root

---
+++ machines/provisioning/provisioned-files/pages/git-global-ignore/git-global-ignore.provisioned-file.ts
@@ -0,0 +1,13 @@
+import type { ProvisionedFile } from "../../provisioned-file.page-type.ts"
+
+export const gitGlobalIgnore = {
+  id: "01a06862-af5c-7db9-86b1-d4785579006f",
+  pageTypeSlug: "provisioned-file",
+  type: "provisioned-file",
+  slug: "git-global-ignore",
+  definition: "what git leaves untracked in every repository",
+  content: "conf",
+  placedBy: "link",
+  onlyOn: "any",
+  installPath: "~/.config/git/ignore",
+} as const satisfies ProvisionedFile
---
+++ machines/provisioning/provisioned-files/pages/repos-editor-settings/repos-editor-settings.provisioned-file.ts
@@ -0,0 +1,13 @@
+import type { ProvisionedFile } from "../../provisioned-file.page-type.ts"
+
+export const reposEditorSettings = {
+  id: "01a06862-af5d-708d-a19c-4050c0370e70",
+  pageTypeSlug: "provisioned-file",
+  type: "provisioned-file",
+  slug: "repos-editor-settings",
+  definition: "how the editor behaves in a window opened over the repos folder",
+  content: "json",
+  placedBy: "link",
+  onlyOn: "any",
+  installPath: "$REPOS/.vscode/settings.json",
+} as const satisfies ProvisionedFile
