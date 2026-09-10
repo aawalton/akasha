131579e9dbab93f601b515276305c829f2158bec
Astra <astra@alanwalton.com>
2026-09-09 19:17:43 -0600

    write world-species's type

--- story/world-mechanics/world-species/world-species.page-type.ts
+++ story/world-mechanics/world-species/world-species.page-type.ts
@@ -1,7 +1,4 @@
 import type { PageType } from "@akasha/pages/page-type"
-import type { WorldMechanic } from "../world-mechanic.page-type.types.ts"
-
-export type WorldSpecies = WorldMechanic
 
 export const worldSpecies = {
   id: "01a06558-a991-724e-8d8a-217efd5250c6",
@@ -12,4 +9,5 @@ export const worldSpecies = {
   pluralSlug: "world-species",
   extends: ["page-type/world-mechanic"],
   runsTabooCheck: false,
+  types: "ts",
 } as const satisfies PageType
