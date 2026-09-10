8f6701625122327354bad6dc530e5f46cdc211a3
Astra <astra@alanwalton.com>
2026-09-09 19:17:24 -0600

    write world-curse's type

--- story/world-mechanics/world-curses/world-curse.page-type.ts
+++ story/world-mechanics/world-curses/world-curse.page-type.ts
@@ -1,7 +1,4 @@
 import type { PageType } from "@akasha/pages/page-type"
-import type { WorldMechanic } from "../world-mechanic.page-type.types.ts"
-
-export type WorldCurse = WorldMechanic
 
 export const worldCurse = {
   id: "01a06558-a991-70f3-9df8-3f1468231807",
@@ -12,4 +9,5 @@ export const worldCurse = {
   pluralSlug: "world-curses",
   extends: ["page-type/world-mechanic"],
   runsTabooCheck: false,
+  types: "ts",
 } as const satisfies PageType
