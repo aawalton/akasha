81a49d3764809ff3e4221cdeec4b510cf408bec1
Akasha <akasha@alanwalton.com>
2026-09-10 09:04:24 -0600

    royal road sync 2 page(s)

---
+++ story/chapters-read/pages/just-add-mana-0189-184-book-4-chapter-28-the-grand-design-pt-1/just-add-mana-0189-184-book-4-chapter-28-the-grand-design-pt-1.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const justAddMana0189184Book4Chapter28TheGrandDesignPt1 = {
+  id: "01a08bd9-3085-76b6-8845-88d1d7a411ed",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "just-add-mana-0189-184-book-4-chapter-28-the-grand-design-pt-1",
+  position: 189,
+  publishedAt: "2026-09-10",
+  unit: "words",
+  externalId: "3939321",
+  source: "royal-road",
+  title: "184 — Book 4, Chapter 28 — The Grand Design, Pt 1",
+  story: "story-read/just-add-mana",
+  ownLength: 3259,
+  externalLink:
+    "https://www.royalroad.com/fiction/125163/just-add-mana/chapter/3939321/184-book-4-chapter-28-the-grand-design-pt-1",
+  prose: "txt",
+} as const satisfies StoryChapterRead
---
+++ story/chapters-read/pages/rise-of-the-living-forge-0118-chapter-648-home/rise-of-the-living-forge-0118-chapter-648-home.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const riseOfTheLivingForge0118Chapter648Home = {
+  id: "01a08bd9-31d7-737e-9e68-8cd738839559",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "rise-of-the-living-forge-0118-chapter-648-home",
+  position: 118,
+  publishedAt: "2026-09-10",
+  unit: "words",
+  externalId: "3939481",
+  source: "royal-road",
+  title: "Chapter 648: Home",
+  story: "story-read/rise-of-the-living-forge",
+  ownLength: 1947,
+  externalLink:
+    "https://www.royalroad.com/fiction/79094/rise-of-the-living-forge/chapter/3939481/chapter-648-home",
+  prose: "txt",
+} as const satisfies StoryChapterRead
