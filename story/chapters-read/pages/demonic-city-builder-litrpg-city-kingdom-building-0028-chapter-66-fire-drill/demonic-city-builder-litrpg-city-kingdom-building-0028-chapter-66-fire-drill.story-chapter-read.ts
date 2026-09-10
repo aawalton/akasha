d54da91dc1c6b94806ad0b31ea25bf6748fb79dc
Akasha <akasha@alanwalton.com>
2026-09-09 21:04:27 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/demonic-city-builder-litrpg-city-kingdom-building-0028-chapter-66-fire-drill/demonic-city-builder-litrpg-city-kingdom-building-0028-chapter-66-fire-drill.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const demonicCityBuilderLitrpgCityKingdomBuilding0028Chapter66FireDrill = {
+  id: "01a08946-1557-7a46-bb13-d701fdc07bb7",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "demonic-city-builder-litrpg-city-kingdom-building-0028-chapter-66-fire-drill",
+  position: 28,
+  publishedAt: "2026-09-10",
+  unit: "words",
+  externalId: "3937544",
+  source: "royal-road",
+  title: "Chapter 66 - Fire Drill",
+  story: "story-read/demonic-city-builder-litrpg-city-kingdom-building",
+  ownLength: 2169,
+  externalLink:
+    "https://www.royalroad.com/fiction/168862/demonic-city-builder-litrpg-city-kingdom-building/chapter/3937544/chapter-66-fire-drill",
+  prose: "txt",
+} as const satisfies StoryChapterRead
