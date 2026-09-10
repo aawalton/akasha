3ff32e48c20a02f4d92e6bbec294e2bbeea885ee
Astra <astra@alanwalton.com>
2026-09-09 20:23:43 -0600

    write world-enchantment's type

--- story/world-mechanics/world-enchantments/world-enchantment.page-type.ts
+++ story/world-mechanics/world-enchantments/world-enchantment.page-type.ts
@@ -1,7 +1,4 @@
 import type { PageType } from "@akasha/pages/page-type"
-import type { WorldMechanic } from "../world-mechanic.page-type.types.ts"
-
-export type WorldEnchantment = WorldMechanic
 
 export const worldEnchantment = {
   id: "01a06558-a991-75d7-9cdc-b80a17f534a1",
@@ -12,4 +9,5 @@ export const worldEnchantment = {
   pluralSlug: "world-enchantments",
   extends: ["page-type/world-mechanic"],
   runsTabooCheck: false,
+  types: "ts",
 } as const satisfies PageType
