b97c6a291890781b9e5bd0cddfb76a72af577507
Thea <thea@alanwalton.com>
2026-09-10 07:55:11 -0600

    a group page type's properties name that group's members, so a ceiling declared there is read by nothing

--- code-system/module-property-groups/module-property-group.page-type.types.ts
+++ code-system/module-property-groups/module-property-group.page-type.types.ts
@@ -1,6 +1,3 @@
-import type { MaxCpuSeconds } from "../../pages/code-file-properties/properties/max-cpu-seconds.number-property.ts"
-import type { MaxMemoryMb } from "../../pages/code-file-properties/properties/max-memory-mb.number-property.ts"
-import type { MaxWallSeconds } from "../../pages/code-file-properties/properties/max-wall-seconds.number-property.ts"
 import type { FilePropertyGroup } from "../../pages/file-property-groups/file-property-group.page-type.types.ts"
 import type { Code } from "../modules/properties/code.code-file-property.ts"
 import type { Test } from "../modules/properties/test.code-file-property.ts"
@@ -10,7 +7,4 @@ export type ModulePropertyGroup = FilePropertyGroup & {
   code?: Code
   test?: Test
   testFixtures?: TestFixtures
-  maxCpuSeconds?: MaxCpuSeconds
-  maxWallSeconds?: MaxWallSeconds
-  maxMemoryMb?: MaxMemoryMb
 }
