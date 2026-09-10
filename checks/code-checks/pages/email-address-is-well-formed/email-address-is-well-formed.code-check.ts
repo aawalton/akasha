877793963510461da4d7df6f4d7a5fbc760bd646
Thea <thea@alanwalton.com>
2026-09-10 13:14:47 -0600

    turn email-address-is-well-formed on at audit

--- checks/code-checks/pages/email-address-is-well-formed/email-address-is-well-formed.code-check.ts
+++ checks/code-checks/pages/email-address-is-well-formed/email-address-is-well-formed.code-check.ts
@@ -9,7 +9,7 @@ export const emailAddressIsWellFormed = {
   runsOnChange: true,
   runsOnWorktree: false,
   runsOnDeploy: false,
-  runsOnAudit: false,
+  runsOnAudit: true,
   invariants: [
     {
       invariantKind: "departure",
