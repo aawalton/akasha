62abdb2924c74e7f4d49c855482792bf9fe49eec
Akasha <akasha@alanwalton.com>
2026-09-10 08:40:32 -0600

    amy-ace31b8bb31e7b471: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/amy-ace31b8bb31e7b471.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../subagent.page-type.types.ts"
+
+export const amyAce31b8bb31e7b471 = {
+  id: "01a08bb6-ac58-757d-934f-d326f63f52d7",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "amy-ace31b8bb31e7b471",
+  principalSeatName: "amy",
+  assignmentSlug: "initiative/amy-harness-improvements",
+  dispatchedAs: "Explore",
+  agentId: "01a087b5-1ca3-7000-bf5c-16aae1620c39--ace31b8bb31e7b471",
+} as const satisfies Subagent
