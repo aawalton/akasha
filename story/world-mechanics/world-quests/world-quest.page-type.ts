fe1db5862bbfe27501212e150aff029e4ea32368
Astra <astra@alanwalton.com>
2026-09-09 19:26:18 -0600

    write world-quest's type

--- story/world-mechanics/world-quests/world-quest.page-type.ts
+++ story/world-mechanics/world-quests/world-quest.page-type.ts
@@ -1,7 +1,4 @@
 import type { PageType } from "@akasha/pages/page-type"
-import type { WorldMechanic } from "../world-mechanic.page-type.types.ts"
-
-export type WorldQuest = WorldMechanic
 
 export const worldQuest = {
   id: "01a06558-a991-7cfc-a470-1adaf50096f8",
@@ -12,4 +9,5 @@ export const worldQuest = {
   pluralSlug: "world-quests",
   extends: ["page-type/world-mechanic"],
   runsTabooCheck: false,
+  types: "ts",
 } as const satisfies PageType
