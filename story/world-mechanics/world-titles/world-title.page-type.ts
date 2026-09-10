6efaf0f9752fbe6cd11150fe80f3e8b6946bf0c3
Astra <astra@alanwalton.com>
2026-09-09 20:27:37 -0600

    write world-title's type

--- story/world-mechanics/world-titles/world-title.page-type.ts
+++ story/world-mechanics/world-titles/world-title.page-type.ts
@@ -1,7 +1,4 @@
 import type { PageType } from "@akasha/pages/page-type"
-import type { WorldMechanic } from "../world-mechanic.page-type.types.ts"
-
-export type WorldTitle = WorldMechanic
 
 export const worldTitle = {
   id: "01a06558-a991-7a45-b681-fed7723c95e3",
@@ -12,4 +9,5 @@ export const worldTitle = {
   pluralSlug: "world-titles",
   extends: ["page-type/world-mechanic"],
   runsTabooCheck: false,
+  types: "ts",
 } as const satisfies PageType
