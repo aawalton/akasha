c1f5674223c92e82e136170a20a46ef76f3e8251
Akasha <akasha@alanwalton.com>
2026-09-10 08:38:37 -0600

    thea: the seat is in akasha as what it states

--- seat-system/seats/pages/thea/thea.seat.ts
+++ seat-system/seats/pages/thea/thea.seat.ts
@@ -1,10 +1,10 @@
 import type { Seat } from "../../seat.page-type.types.ts"
 
-export const thea/thea = {
+export const thea = {
   id: "01a06c31-1b01-7000-b602-fc1a3f96f3a4",
   pageTypeSlug: "seat",
   type: "seat",
-  slug: "thea/thea",
+  slug: "thea",
   persona: "thea",
   assignmentSlug: "initiative/thea-checks-system",
   role: "definer",
