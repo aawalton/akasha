0f29fc93ac4cd101bf2d200361da460949b09ecb
Astra <astra@alanwalton.com>
2026-09-09 18:28:06 -0600

    write world-legacy's type

--- story/world-mechanics/world-legacies/world-legacy.page-type.ts
+++ story/world-mechanics/world-legacies/world-legacy.page-type.ts
@@ -1,7 +1,4 @@
 import type { PageType } from "@akasha/pages/page-type"
-import type { WorldMechanic } from "../world-mechanic.page-type.types.ts"
-
-export type WorldLegacy = WorldMechanic
 
 export const worldLegacy = {
   id: "01a06558-a991-7854-b460-869fb15a06b9",
@@ -12,4 +9,5 @@ export const worldLegacy = {
   pluralSlug: "world-legacies",
   extends: ["page-type/world-mechanic"],
   runsTabooCheck: false,
+  types: "ts",
 } as const satisfies PageType
