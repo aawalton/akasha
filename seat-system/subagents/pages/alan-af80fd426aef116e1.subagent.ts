e80891532c4db31cc3d45761a4f37dbc9db1349a
Akasha <akasha@alanwalton.com>
2026-09-10 09:07:47 -0600

    alan-af80fd426aef116e1: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/alan-af80fd426aef116e1.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../subagent.page-type.types.ts"
+
+export const alanAf80fd426aef116e1 = {
+  id: "01a08bdc-4be6-7912-bfdb-58d493c50a26",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "alan-af80fd426aef116e1",
+  principalSeatName: "alan",
+  assignmentSlug: "domain/alan",
+  dispatchedAs: "Explore",
+  agentId: "01a0778b-113f-7000-9f19-d579bb607440--af80fd426aef116e1",
+} as const satisfies Subagent
