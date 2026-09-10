a001bec9d0a001412c7f5bb31d15304420cbbef7
Thea <thea@alanwalton.com>
2026-09-10 13:15:39 -0600

    turn no-second-spelling-of-a-name-format on at audit

--- checks/code-checks/pages/no-second-spelling-of-a-name-format/no-second-spelling-of-a-name-format.code-check.ts
+++ checks/code-checks/pages/no-second-spelling-of-a-name-format/no-second-spelling-of-a-name-format.code-check.ts
@@ -9,7 +9,7 @@ export const noSecondSpellingOfANameFormat = {
   runsOnChange: true,
   runsOnWorktree: false,
   runsOnDeploy: false,
-  runsOnAudit: false,
+  runsOnAudit: true,
   invariants: [
     {
       invariantKind: "departure",
