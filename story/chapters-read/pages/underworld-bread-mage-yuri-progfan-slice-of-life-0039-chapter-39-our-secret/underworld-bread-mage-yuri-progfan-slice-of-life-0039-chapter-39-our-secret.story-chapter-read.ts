d12bcab20ca83860b232e8c3a424694318a7a59f
Akasha <akasha@alanwalton.com>
2026-09-10 08:04:42 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/underworld-bread-mage-yuri-progfan-slice-of-life-0039-chapter-39-our-secret/underworld-bread-mage-yuri-progfan-slice-of-life-0039-chapter-39-our-secret.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const underworldBreadMageYuriProgfanSliceOfLife0039Chapter39OurSecret = {
+  id: "01a08ba2-86b0-70de-8ae9-bc01bf353d85",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "underworld-bread-mage-yuri-progfan-slice-of-life-0039-chapter-39-our-secret",
+  position: 39,
+  publishedAt: "2026-09-10",
+  unit: "words",
+  externalId: "3939098",
+  source: "royal-road",
+  title: "Chapter 39: Our Secret",
+  story: "story-read/underworld-bread-mage-yuri-progfan-slice-of-life",
+  ownLength: 1245,
+  externalLink:
+    "https://www.royalroad.com/fiction/179650/underworld-bread-mage-yuri-lit-rpg-slice-of-life/chapter/3939098/chapter-39-our-secret",
+  prose: "txt",
+} as const satisfies StoryChapterRead
