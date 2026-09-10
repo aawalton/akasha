cb47a3be0c8f6d3cfc888aee53014db65a6c6bcf
Thea <thea@alanwalton.com>
2026-09-10 13:15:29 -0600

    turn no-rule-in-two-files on at audit

--- checks/code-checks/pages/no-rule-in-two-files/no-rule-in-two-files.code-check.ts
+++ checks/code-checks/pages/no-rule-in-two-files/no-rule-in-two-files.code-check.ts
@@ -9,7 +9,7 @@ export const noRuleInTwoFiles = {
   runsOnChange: false,
   runsOnWorktree: false,
   runsOnDeploy: false,
-  runsOnAudit: false,
+  runsOnAudit: true,
   invariants: [
     {
       invariantKind: "departure",
