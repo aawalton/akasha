3d3d03aba896b53456add9984d6095804bd3ba2e
Akasha <akasha@alanwalton.com>
2026-09-10 17:33:14 -0600

    astra-a3697fda920afaac8: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/astra-a3697fda920afaac8/astra-a3697fda920afaac8.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../../subagent.page-type.types.ts"
+
+export const astraA3697fda920afaac8 = {
+  id: "01a08dab-09dc-7f87-bee4-898554038049",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "astra-a3697fda920afaac8",
+  principalSeatName: "astra",
+  assignmentSlug: "initiative/astra-generated-types",
+  dispatchedAs: "Explore",
+  agentId: "01a0873b-6b46-7000-8c31-d90227ba00fa--a3697fda920afaac8",
+} as const satisfies Subagent
