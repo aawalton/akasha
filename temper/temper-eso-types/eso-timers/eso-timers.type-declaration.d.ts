109dad4e78421905bf0d16775fde43203272ff06
Akasha <akasha@alanwalton.com>
2026-09-09 18:09:08 -0600

    the eso timer declarations return undefined rather than void

--- temper/temper-eso-types/eso-timers/eso-timers.type-declaration.d.ts
+++ temper/temper-eso-types/eso-timers/eso-timers.type-declaration.d.ts
@@ -2,8 +2,8 @@ declare function setTimeout(this: void, fn: (this: void) => void, ms: number): n
 
 declare function setInterval(this: void, fn: (this: void) => void, ms: number): number
 
-declare function clearTimeout(this: void, handle: number): void
+declare function clearTimeout(this: void, handle: number): undefined
 
-declare function clearInterval(this: void, handle: number): void
+declare function clearInterval(this: void, handle: number): undefined
 
-declare function queueMicrotask(this: void, fn: (this: void) => void): void
+declare function queueMicrotask(this: void, fn: (this: void) => void): undefined
