50756c183fd6a267a03af8b9da529bb8b271e32f
Akasha <akasha@alanwalton.com>
2026-09-09 23:07:25 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/every-trope-everywhere-all-at-once-litrpg-progression-0104-103-descendant/every-trope-everywhere-all-at-once-litrpg-progression-0104-103-descendant.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const everyTropeEverywhereAllAtOnceLitrpgProgression0104103Descendant = {
+  id: "01a089b6-aa40-7501-b59d-453ae460dfe3",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "every-trope-everywhere-all-at-once-litrpg-progression-0104-103-descendant",
+  position: 104,
+  publishedAt: "2026-09-10",
+  unit: "words",
+  externalId: "3937825",
+  source: "royal-road",
+  title: "103 - Descendant",
+  story: "story-read/every-trope-everywhere-all-at-once-litrpg-progression",
+  ownLength: 1483,
+  externalLink:
+    "https://www.royalroad.com/fiction/166757/every-trope-everywhere-all-at-once-litrpg-progression/chapter/3937825/103-descendant",
+  prose: "txt",
+} as const satisfies StoryChapterRead
