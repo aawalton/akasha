ab87b5d76525e501307288e2b577b99621770353
Astra <astra@alanwalton.com>
2026-09-09 18:18:41 -0600

    write world-religion's type

--- story/world-mechanics/world-religions/world-religion.page-type.ts
+++ story/world-mechanics/world-religions/world-religion.page-type.ts
@@ -1,7 +1,4 @@
 import type { PageType } from "@akasha/pages/page-type"
-import type { WorldMechanic } from "../world-mechanic.page-type.types.ts"
-
-export type WorldReligion = WorldMechanic
 
 export const worldReligion = {
   id: "01a06558-a991-75ad-97e7-2a55723fe665",
@@ -12,4 +9,5 @@ export const worldReligion = {
   pluralSlug: "world-religions",
   extends: ["page-type/world-mechanic"],
   runsTabooCheck: false,
+  types: "ts",
 } as const satisfies PageType
