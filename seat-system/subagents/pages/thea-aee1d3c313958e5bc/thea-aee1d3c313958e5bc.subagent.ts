1b89af7e675ee638c11ce69fe76d1b15c64441b5
Akasha <akasha@alanwalton.com>
2026-09-10 16:24:43 -0600

    thea-aee1d3c313958e5bc: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/thea-aee1d3c313958e5bc/thea-aee1d3c313958e5bc.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../../subagent.page-type.types.ts"
+
+export const theaAee1d3c313958e5bc = {
+  id: "01a08d6c-51b8-7fc5-a47d-ccc5b46ed714",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "thea-aee1d3c313958e5bc",
+  principalSeatName: "thea",
+  assignmentSlug: "initiative/thea-checks-system",
+  dispatchedAs: "general-purpose",
+  agentId: "01a06c31-1b01-7000-b602-fc1a3f96f3a4--aee1d3c313958e5bc",
+} as const satisfies Subagent
