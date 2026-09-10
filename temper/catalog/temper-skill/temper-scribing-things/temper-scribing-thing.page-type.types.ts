82b970fafa37ac8292854aecbb8f5a1607442ede
Akasha <akasha@alanwalton.com>
2026-09-10 12:12:29 -0600

    five skill parts move under the collective folder

--- temper/catalog/temper-skill/temper-scribing-things/temper-scribing-thing.page-type.types.ts
+++ temper/catalog/temper-skill/temper-scribing-things/temper-scribing-thing.page-type.types.ts
@@ -1,7 +1,7 @@
 import type { Key } from "../../../things/properties/key.text-property.ts"
+import type { UespId } from "../../temper-skills/properties/uesp-id.number-property.ts"
 import type { ItemId } from "../../things/properties/item-id.number-property.ts"
 import type { TemperCatalogThing } from "../../things/temper-catalog-thing.page-type.types.ts"
-import type { UespId } from "../properties/uesp-id.number-property.ts"
 
 export type TemperScribingThing = TemperCatalogThing & {
   key: Key
