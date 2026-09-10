f2e350d34133a84bbc38cf333feb7eac0b54f239
Akasha <akasha@alanwalton.com>
2026-09-10 10:08:04 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/the-primal-hunter-0176-chapter-1379-home-invasion/the-primal-hunter-0176-chapter-1379-home-invasion.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const thePrimalHunter0176Chapter1379HomeInvasion = {
+  id: "01a08c13-7aba-7699-af21-529b674a8fba",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "the-primal-hunter-0176-chapter-1379-home-invasion",
+  position: 176,
+  publishedAt: "2026-09-10",
+  unit: "words",
+  externalId: "3939690",
+  source: "royal-road",
+  title: "Chapter 1379 - Home Invasion",
+  story: "story-read/the-primal-hunter",
+  ownLength: 2594,
+  externalLink:
+    "https://www.royalroad.com/fiction/36049/the-primal-hunter/chapter/3939690/chapter-1379-home-invasion",
+  prose: "txt",
+} as const satisfies StoryChapterRead
