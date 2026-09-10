0ac3318660e4ca5fa6d42027cf3bf2b5dfb94fe3
Akasha <akasha@alanwalton.com>
2026-09-10 05:05:27 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/shapeshifter-a-non-human-litrpg-0045-chapter-40-gathering-intel/shapeshifter-a-non-human-litrpg-0045-chapter-40-gathering-intel.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "../../story-chapter-read.page-type.ts"
+
+export const shapeshifterANonHumanLitrpg0045Chapter40GatheringIntel = {
+  id: "01a08afe-739f-7403-8f5f-7e53b438ba30",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "shapeshifter-a-non-human-litrpg-0045-chapter-40-gathering-intel",
+  position: 45,
+  publishedAt: "2026-09-10",
+  unit: "words",
+  externalId: "3938574",
+  source: "royal-road",
+  title: "Chapter 40: Gathering Intel",
+  story: "story-read/shapeshifter-a-non-human-litrpg",
+  ownLength: 1666,
+  externalLink:
+    "https://www.royalroad.com/fiction/123277/faceless-hunter-a-shapeshifter-litrpg/chapter/3938574/chapter-40-gathering-intel",
+  prose: "txt",
+} as const satisfies StoryChapterRead
