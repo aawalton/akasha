b8622ba2495e655a01d8bbcc9c29635d2503ee2c
Thea <thea@alanwalton.com>
2026-09-10 13:15:21 -0600

    turn file-has-its-page on at audit

--- checks/code-checks/pages/file-has-its-page/file-has-its-page.code-check.ts
+++ checks/code-checks/pages/file-has-its-page/file-has-its-page.code-check.ts
@@ -9,7 +9,7 @@ export const fileHasItsPage = {
   runsOnChange: true,
   runsOnWorktree: false,
   runsOnDeploy: false,
-  runsOnAudit: false,
+  runsOnAudit: true,
   invariants: [
     {
       invariantKind: "departure",
