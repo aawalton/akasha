228712e78d0200df9e825978e209f4ccbf70f335
Thea <thea@alanwalton.com>
2026-09-10 13:15:59 -0600

    turn no-tmp on at audit

--- checks/code-checks/pages/no-tmp/no-tmp.code-check.ts
+++ checks/code-checks/pages/no-tmp/no-tmp.code-check.ts
@@ -9,7 +9,7 @@ export const noTmp = {
   runsOnChange: true,
   runsOnWorktree: false,
   runsOnDeploy: false,
-  runsOnAudit: false,
+  runsOnAudit: true,
   invariants: [
     {
       invariantKind: "departure",
