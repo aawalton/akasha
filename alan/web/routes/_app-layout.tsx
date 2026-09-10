d5e975f9be96b02dd01b9282c92492168774be2a
Aranya <aranya@alanwalton.com>
2026-09-10 08:13:10 -0600

    the app layout declares the loader it exports

--- alan/web/routes/_app-layout.tsx
+++ alan/web/routes/_app-layout.tsx
@@ -2,11 +2,12 @@ import { Toaster } from "@akasha/design-primitives/sonner"
 import { PlayingSessionProvider } from "@akasha/pages-ui/media/playing-session-context"
 import { SupabaseProvider } from "@akasha/supabase-rr/supabase-provider"
 import { Outlet } from "react-router"
+import { loader as appLayoutLoader } from "../.server/app-layout-loading/app-layout-loading.module.code.ts"
 import { AppShell } from "../alan-app-shell/alan-app-shell.module.code.tsx"
 import { AuthProvider } from "../alan-auth-provider/alan-auth-provider.module.code.tsx"
 import type { Route } from "./+types/_app-layout"
 
-export { loader } from "../.server/app-layout-loading/app-layout-loading.module.code.ts"
+export const loader = appLayoutLoader
 
 export default function AppLayout({ loaderData }: Route.ComponentProps) {
   return (
