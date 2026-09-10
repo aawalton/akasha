e2aac130d109ce9a2f4f0321713f15294f052c9f
Akasha <akasha@alanwalton.com>
2026-09-10 07:38:49 -0600

    amy-ad8d7c886d7e35a7c: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/amy-ad8d7c886d7e35a7c.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../subagent.page-type.types.ts"
+
+export const amyAd8d7c886d7e35a7c = {
+  id: "01a08b8a-d4aa-7eb4-81e2-b69adff03b04",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "amy-ad8d7c886d7e35a7c",
+  principalSeatName: "amy",
+  assignmentSlug: "initiative/amy-harness-improvements",
+  dispatchedAs: "Explore",
+  agentId: "01a087b5-1ca3-7000-bf5c-16aae1620c39--ad8d7c886d7e35a7c",
+} as const satisfies Subagent
