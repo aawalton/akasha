354e2a357d830982b48fce285a6aacef23b7fd56
temper watcher <watcher@alanwalton.com>
2026-09-10 10:02:10 -0600

    temper: dragonguard-daily-quests came round again on what its characters did
    Written-by: temper watcher <watcher@alanwalton.com>

--- temper/progressions/temper-tasks/pages/dragonguard-daily-quests/dragonguard-daily-quests.temper-task.ts
+++ temper/progressions/temper-tasks/pages/dragonguard-daily-quests/dragonguard-daily-quests.temper-task.ts
@@ -10,7 +10,7 @@ export const dragonguardDailyQuests = {
   character: "lyonette-du-marquin",
   completionCardId: "character-achievements",
   completionItemPath: ["Dragonhold", "Quests", "2612"],
-  dueDate: "2026-09-09",
+  dueDate: "2026-09-11",
   rruleRule: "FREQ=DAILY",
   rruleAnchorFromCompletion: false,
   accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
