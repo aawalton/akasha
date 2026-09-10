a31e2d25d1f88689f2ad3e27e199c7b5f8904dcd
temper watcher <watcher@alanwalton.com>
2026-09-10 14:14:00 -0600

    temper: a net worth reading of 548927256 gold at 2026-09-10T20:11:38.000Z
    Written-by: temper watcher <watcher@alanwalton.com>

---
+++ temper/holdings-sets/temper-net-worth-hours/pages/hour-2026-09-10-20/hour-2026-09-10-20.temper-net-worth-hour.ts
@@ -0,0 +1,10 @@
+import type { TemperNetWorthHour } from "../../temper-net-worth-hour.page-type.types.ts"
+
+export const hour2026091020 = {
+  id: "01a08cf4-a5cd-7000-a1bc-65728fd0a08b",
+  pageTypeSlug: "temper-net-worth-hour",
+  type: "temper-net-worth-hour",
+  slug: "hour-2026-09-10-20",
+  title: "2026-09-10 20:00 UTC",
+  snapshots: "jsonl",
+} as const satisfies TemperNetWorthHour
