4c38a007b5c15b0dd660c7f0c5faa6817b17e54d
Thea <thea@alanwalton.com>
2026-09-10 20:25:43 -0600

    lualib-pages takes textAt and Value from page-value-reading rather than through the page-value barrel

--- language-design/lua-compiler/lualib-pages/lualib-pages.module.code.ts
+++ language-design/lua-compiler/lualib-pages/lualib-pages.module.code.ts
@@ -3,7 +3,7 @@ import { basename, join } from "node:path"
 import { LuaLibFeature } from "akasha/language-design/lua-compiler/lualib-features/lualib-features.module.code.ts"
 import { akashaRoot } from "akasha/pages/checkout-roots/checkout-roots.module.code.ts"
 import { valuesOfType } from "akasha/pages/indexes/reading/index-reading.module.code.ts"
-import { textAt, type Value } from "akasha/pages/value/page-value.module.code.ts"
+import { textAt, type Value } from "akasha/pages/value-reading/page-value-reading.module.code.ts"
 
 const PAGE = ".lualib.ts"
 
