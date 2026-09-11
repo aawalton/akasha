a86a9d2917e5bd4859e865e8cfcb832b5e9a93d9
Aranya <aranya@alanwalton.com>
2026-09-10 20:21:07 -0600

    every body under pages/text-properties names a file from the root rather than by a relative path

--- pages/text-properties/properties/name-format.relation-property.ts
+++ pages/text-properties/properties/name-format.relation-property.ts
@@ -1,4 +1,4 @@
-import type { RelationProperty } from "../../relation-properties/relation-property.page-type.types.ts"
+import type { RelationProperty } from "akasha/pages/relation-properties/relation-property.page-type.types.ts"
 
 export const nameFormat = {
   id: "01a049b9-856c-73f0-bea2-c3036209aa09",
--- pages/text-properties/text-property.page-type.ts
+++ pages/text-properties/text-property.page-type.ts
@@ -1,4 +1,4 @@
-import type { PageType } from "../types/page-type.page-type.types.ts"
+import type { PageType } from "akasha/pages/types/page-type.page-type.types.ts"
 
 export const textProperty = {
   id: "01a04dff-9d7d-7b50-a58a-419207af8ec0",
