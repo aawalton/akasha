d9ace33e57e9e7c1728b06e32b9b91eb2cca279a
Akasha <akasha@alanwalton.com>
2026-09-10 19:24:18 -0600

    dalla-a98b288c20b2a72f6: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/dalla-a98b288c20b2a72f6/dalla-a98b288c20b2a72f6.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../../subagent.page-type.types.ts"
+
+export const dallaA98b288c20b2a72f6 = {
+  id: "01a08e10-b85c-7e16-8601-3781dea75b6e",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "dalla-a98b288c20b2a72f6",
+  principalSeatName: "dalla",
+  assignmentSlug: "initiative/dalla-deploy-cleanup",
+  dispatchedAs: "Explore",
+  agentId: "01a08cd2-1ea9-7000-bfad-e74cc7358c36--a98b288c20b2a72f6",
+} as const satisfies Subagent
