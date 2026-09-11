99321cef4e8eace7f38bf49abd85b0754599e7e0
Akasha <akasha@alanwalton.com>
2026-09-11 13:37:08 -0600

    aine: the seat is in akasha as what it states

--- seat-system/seats/pages/aine/aine.seat.ts
+++ seat-system/seats/pages/aine/aine.seat.ts
@@ -2,7 +2,6 @@ import type { Seat } from "akasha/seat-system/seats/seat.page-type.types.ts"
 
 export const aine = {
   id: "01a09102-97a5-7000-bdb9-f3ad7e8498f4",
-  pageTypeSlug: "seat",
   type: "seat",
   slug: "aine",
   persona: "aine",
