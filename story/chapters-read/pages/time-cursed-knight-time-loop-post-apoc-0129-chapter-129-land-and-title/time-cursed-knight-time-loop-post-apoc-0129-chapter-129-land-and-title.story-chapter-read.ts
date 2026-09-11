84af4c19deb8527992c8826a3e951debd9393813
Akasha <akasha@alanwalton.com>
2026-09-11 11:09:25 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/time-cursed-knight-time-loop-post-apoc-0129-chapter-129-land-and-title/time-cursed-knight-time-loop-post-apoc-0129-chapter-129-land-and-title.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const timeCursedKnightTimeLoopPostApoc0129Chapter129LandAndTitle = {
+  id: "01a09170-025e-7316-8f8a-28834eef0706",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "time-cursed-knight-time-loop-post-apoc-0129-chapter-129-land-and-title",
+  position: 129,
+  publishedAt: "2026-09-11",
+  unit: "words",
+  externalId: "3945007",
+  source: "royal-road",
+  title: "Chapter 129: Land and Title",
+  story: "story-read/time-cursed-knight-time-loop-post-apoc",
+  ownLength: 1981,
+  externalLink:
+    "https://www.royalroad.com/fiction/160012/time-cursed-knight-time-loop-post-apoc/chapter/3945007/chapter-129-land-and-title",
+  prose: "txt",
+} as const satisfies StoryChapterRead
