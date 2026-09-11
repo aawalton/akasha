9d604697b13d72984e22f7f50bb12508c3bb3c95
Akasha <akasha@alanwalton.com>
2026-09-11 09:14:38 -0600

    amy-a83089fb310dab65e: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/amy-a83089fb310dab65e/amy-a83089fb310dab65e.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const amyA83089fb310dab65e = {
+  id: "01a09108-e4c2-79d6-924f-22de0068162f",
+  type: "subagent",
+  slug: "amy-a83089fb310dab65e",
+  principalSeatName: "amy",
+  assignmentSlug: "initiative/amy-telnyx-approval",
+  dispatchedAs: "general-purpose",
+  agentId: "01a087b5-1ca3-7000-bf5c-16aae1620c39--a83089fb310dab65e",
+} as const satisfies Subagent
