ce53da0dffdb144c553e71e1823d25ace6071bbe
Ryn <ryn@alanwalton.com>
2026-09-11 11:24:34 -0600

    the email domain stands under persons rather than under the people folder

--- persons/email/gmail-mailboxes/gmail-mailbox.page-type.types.ts
+++ persons/email/gmail-mailboxes/gmail-mailbox.page-type.types.ts
@@ -1,7 +1,7 @@
 import type { Page } from "akasha/pages/page.page-type.types.ts"
 import type { Title } from "akasha/pages/properties/title.text-property.types.ts"
 import type { EmailAddress } from "akasha/personas/properties/email-address.email-address-property.types.ts"
-import type { ProcessedMessages } from "akasha/persons/people/email/gmail-mailboxes/properties/processed-messages.page-property-entry.types.ts"
+import type { ProcessedMessages } from "akasha/persons/email/gmail-mailboxes/properties/processed-messages.page-property-entry.types.ts"
 
 export type GmailMailbox = Page & {
   title: Title
