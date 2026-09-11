faa5c29c5abc1c6cf89f5edf54d1d4d5201f09c0
Akasha <akasha@alanwalton.com>
2026-09-11 17:05:42 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/five-element-overlord-i-can-upgrade-everything-0140-ch140-a-clash-of-souls/five-element-overlord-i-can-upgrade-everything-0140-ch140-a-clash-of-souls.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const fiveElementOverlordICanUpgradeEverything0140Ch140AClashOfSouls = {
+  id: "01a092b8-2d50-781a-8aa0-8a78b7d2b19f",
+  type: "story-chapter-read",
+  slug: "five-element-overlord-i-can-upgrade-everything-0140-ch140-a-clash-of-souls",
+  position: 140,
+  publishedAt: "2026-09-11",
+  unit: "words",
+  externalId: "3946330",
+  source: "royal-road",
+  title: "Ch140 A Clash of Souls",
+  story: "story-read/five-element-overlord-i-can-upgrade-everything",
+  ownLength: 1176,
+  externalLink:
+    "https://www.royalroad.com/fiction/159989/five-element-overlord-i-can-upgrade-everything/chapter/3946330/ch140-a-clash-of-souls",
+  prose: "txt",
+} as const satisfies StoryChapterRead
