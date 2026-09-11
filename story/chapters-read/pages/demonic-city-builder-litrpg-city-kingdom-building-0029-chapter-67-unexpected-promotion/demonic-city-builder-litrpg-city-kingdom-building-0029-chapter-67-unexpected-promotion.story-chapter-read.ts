7101ee3f6161d12649d48fc239a7518adadd09a9
Akasha <akasha@alanwalton.com>
2026-09-11 16:07:12 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/demonic-city-builder-litrpg-city-kingdom-building-0029-chapter-67-unexpected-promotion/demonic-city-builder-litrpg-city-kingdom-building-0029-chapter-67-unexpected-promotion.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const demonicCityBuilderLitrpgCityKingdomBuilding0029Chapter67UnexpectedPromotion = {
+  id: "01a09282-9eae-7d5e-8234-b9778b892148",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "demonic-city-builder-litrpg-city-kingdom-building-0029-chapter-67-unexpected-promotion",
+  position: 29,
+  publishedAt: "2026-09-11",
+  unit: "words",
+  externalId: "3945989",
+  source: "royal-road",
+  title: "Chapter 67 - Unexpected Promotion",
+  story: "story-read/demonic-city-builder-litrpg-city-kingdom-building",
+  ownLength: 2016,
+  externalLink:
+    "https://www.royalroad.com/fiction/168862/demonic-city-builder-litrpg-city-kingdom-building/chapter/3945989/chapter-67-unexpected-promotion",
+  prose: "txt",
+} as const satisfies StoryChapterRead
