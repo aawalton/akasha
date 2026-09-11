dcec891a79d43f303cfbf81ad161e6284a27f36e
Aranya <aranya@alanwalton.com>
2026-09-10 19:56:05 -0600

    every body under code-system/editor/extension/domain-tree-view names a file from the root rather than by a relative path

--- code-system/editor/extension/domain-tree-view/domain-tree-view.module.code.ts
+++ code-system/editor/extension/domain-tree-view/domain-tree-view.module.code.ts
@@ -1,6 +1,9 @@
 import { relative } from "node:path"
+import {
+  filterTree,
+  textMatches,
+} from "akasha/code-system/editor/extension/tree-filter/tree-filter.module.code.ts"
 import * as vscode from "vscode"
-import { filterTree, textMatches } from "../tree-filter/tree-filter.module.code.ts"
 
 const OPEN_COMMAND = "vscode.open"
 
