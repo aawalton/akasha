b8b594719943998ea4682e4f986edd990319f30c
pages-access <pages-access@alanwalton.com>
2026-09-10 19:33:07 -0600

    upsertPage(temper-account-character): maviola-el
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/characters/temper-account-characters/pages/maviola-el/maviola-el.temper-account-character.ts
+++ temper/characters/temper-account-characters/pages/maviola-el/maviola-el.temper-account-character.ts
@@ -1,4 +1,4 @@
-import type { TemperAccountCharacter } from "akasha/temper/characters/temper-account-characters/temper-account-character.page-type.types.ts"
+import type { TemperAccountCharacter } from "../../temper-account-character.page-type.types.ts"
 
 export const maviolaEl = {
   id: "019dda20-8c7c-772a-9cbd-3ae77bd7c136",
