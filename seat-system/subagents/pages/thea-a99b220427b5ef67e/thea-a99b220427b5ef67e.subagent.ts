5c614fae11cb96bf62c5745f844dbcb06b2ea27b
Akasha <akasha@alanwalton.com>
2026-09-10 19:57:57 -0600

    thea-a99b220427b5ef67e: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/thea-a99b220427b5ef67e/thea-a99b220427b5ef67e.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../../subagent.page-type.types.ts"
+
+export const theaA99b220427b5ef67e = {
+  id: "01a08e2f-88d3-710e-adbc-c6240ea5c3cb",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "thea-a99b220427b5ef67e",
+  principalSeatName: "thea",
+  assignmentSlug: "initiative/thea-checks-system",
+  dispatchedAs: "general-purpose",
+  agentId: "01a06c31-1b01-7000-b602-fc1a3f96f3a4--a99b220427b5ef67e",
+} as const satisfies Subagent
