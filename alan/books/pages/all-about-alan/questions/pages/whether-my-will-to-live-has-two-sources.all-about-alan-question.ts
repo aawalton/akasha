eb181037c5af9125e0cedc923ee21ef1a9e8c126
Akasha <akasha@alanwalton.com>
2026-09-11 07:31:37 -0600

    apply alan/books/pages/all-about-alan/questions/pages/whether-my-will-to-live-has-two-sources.all-about-alan-question.ts

--- alan/books/pages/all-about-alan/questions/pages/whether-my-will-to-live-has-two-sources.all-about-alan-question.ts
+++ alan/books/pages/all-about-alan/questions/pages/whether-my-will-to-live-has-two-sources.all-about-alan-question.ts
@@ -2,7 +2,6 @@ import type { AllAboutAlanQuestion } from "akasha/alan/books/pages/all-about-ala
 
 export const whetherMyWillToLiveHasTwoSources = {
   id: "01a077ef-20e8-7380-a3c1-2e261004e094",
-  pageTypeSlug: "all-about-alan-question",
   type: "all-about-alan-question",
   slug: "whether-my-will-to-live-has-two-sources",
   topic: "what-has-kept-me-here",
