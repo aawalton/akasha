a43efb4285bbc86ea3fb9969c165209a1a43a32f
Akasha <akasha@alanwalton.com>
2026-09-10 18:38:48 -0600

    akasha-afd8dfa450c6aa88f: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/akasha-afd8dfa450c6aa88f/akasha-afd8dfa450c6aa88f.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../../subagent.page-type.types.ts"
+
+export const akashaAfd8dfa450c6aa88f = {
+  id: "01a08de6-a7e8-7072-9c35-834e41ca0ff4",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "akasha-afd8dfa450c6aa88f",
+  principalSeatName: "akasha",
+  assignmentSlug: "initiative/akasha-folder-shape",
+  dispatchedAs: "Explore",
+  agentId: "01a07c15-3e52-7000-ad7b-1c746247704d--afd8dfa450c6aa88f",
+} as const satisfies Subagent
