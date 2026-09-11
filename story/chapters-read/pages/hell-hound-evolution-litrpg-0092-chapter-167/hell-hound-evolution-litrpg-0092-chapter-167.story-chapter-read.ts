78c17bcb46cd5ec87cb72ded1923cf2dbd5d1632
Akasha <akasha@alanwalton.com>
2026-09-11 15:04:48 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/hell-hound-evolution-litrpg-0092-chapter-167/hell-hound-evolution-litrpg-0092-chapter-167.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const hellHoundEvolutionLitrpg0092Chapter167 = {
+  id: "01a09249-7c81-7530-aac9-f491df2592bb",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "hell-hound-evolution-litrpg-0092-chapter-167",
+  position: 92,
+  publishedAt: "2026-09-11",
+  unit: "words",
+  externalId: "3945939",
+  source: "royal-road",
+  title: "chapter 167",
+  story: "story-read/hell-hound-evolution-litrpg",
+  ownLength: 2114,
+  externalLink:
+    "https://www.royalroad.com/fiction/129179/hell-hound-evolution-vol-1-stubbed/chapter/3945939/chapter-167",
+  prose: "txt",
+} as const satisfies StoryChapterRead
