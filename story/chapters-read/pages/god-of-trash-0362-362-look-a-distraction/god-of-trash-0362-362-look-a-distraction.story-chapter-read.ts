dd0f4f86a292a862e017f3803fa2d4ac7196a2a1
Akasha <akasha@alanwalton.com>
2026-09-11 14:04:04 -0600

    royal road sync 2 page(s)

---
+++ story/chapters-read/pages/god-of-trash-0362-362-look-a-distraction/god-of-trash-0362-362-look-a-distraction.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const godOfTrash0362362LookADistraction = {
+  id: "01a09211-e283-7a3b-9b40-6283d7c9a358",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "god-of-trash-0362-362-look-a-distraction",
+  position: 362,
+  publishedAt: "2026-09-11",
+  unit: "words",
+  externalId: "3945700",
+  source: "royal-road",
+  title: "362. Look, a Distraction",
+  story: "story-read/god-of-trash",
+  ownLength: 1758,
+  externalLink:
+    "https://www.royalroad.com/fiction/107252/god-of-trash-cultivation-litrpg-from-trash-tier/chapter/3945700/362-look-a-distraction",
+  prose: "txt",
+} as const satisfies StoryChapterRead
---
+++ story/chapters-read/pages/reborn-as-a-demonic-tree-0091-chapter-616-sun-god/reborn-as-a-demonic-tree-0091-chapter-616-sun-god.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const rebornAsADemonicTree0091Chapter616SunGod = {
+  id: "01a09211-e334-7160-acba-7b1027960570",
+  pageTypeSlug: "story-chapter-read",
+  type: "story-chapter-read",
+  slug: "reborn-as-a-demonic-tree-0091-chapter-616-sun-god",
+  position: 91,
+  publishedAt: "2026-09-11",
+  unit: "words",
+  externalId: "3945562",
+  source: "royal-road",
+  title: "Chapter 616: Sun God",
+  story: "story-read/reborn-as-a-demonic-tree",
+  ownLength: 2907,
+  externalLink:
+    "https://www.royalroad.com/fiction/62881/reborn-as-a-demonic-tree/chapter/3945562/chapter-616-sun-god",
+  prose: "txt",
+} as const satisfies StoryChapterRead
