bde458430000f8ef0c1bfa3b114b05953444ecf2
pages-access <pages-access@alanwalton.com>
2026-09-10 19:33:18 -0600

    upsertPage(temper-account-character): lyonette-du-marquin
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/characters/temper-account-characters/pages/lyonette-du-marquin/lyonette-du-marquin.temper-account-character.ts
+++ temper/characters/temper-account-characters/pages/lyonette-du-marquin/lyonette-du-marquin.temper-account-character.ts
@@ -1,4 +1,4 @@
-import type { TemperAccountCharacter } from "akasha/temper/characters/temper-account-characters/temper-account-character.page-type.types.ts"
+import type { TemperAccountCharacter } from "../../temper-account-character.page-type.types.ts"
 
 export const lyonetteDuMarquin = {
   id: "019dda20-810c-74b1-9fa4-350eb88bc5c5",
