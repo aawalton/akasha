b49a33676ee5f0b3cbc20f2a675ca72a8f249de7
pages-access <pages-access@alanwalton.com>
2026-09-10 19:33:02 -0600

    upsertPage(temper-account-character): mrsha-du-marquin
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/characters/temper-account-characters/pages/mrsha-du-marquin/mrsha-du-marquin.temper-account-character.ts
+++ temper/characters/temper-account-characters/pages/mrsha-du-marquin/mrsha-du-marquin.temper-account-character.ts
@@ -1,4 +1,4 @@
-import type { TemperAccountCharacter } from "akasha/temper/characters/temper-account-characters/temper-account-character.page-type.types.ts"
+import type { TemperAccountCharacter } from "../../temper-account-character.page-type.types.ts"
 
 export const mrshaDuMarquin = {
   id: "019dda20-a396-715c-b6a7-bf556e159ddb",
