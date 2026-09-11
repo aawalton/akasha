592212152ee3b75c0ff37e17811677023917db96
Akasha <akasha@alanwalton.com>
2026-09-10 21:31:38 -0600

    aranya-a3a894f29efb3b8c0: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/aranya-a3a894f29efb3b8c0/aranya-a3a894f29efb3b8c0.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../../subagent.page-type.types.ts"
+
+export const aranyaA3a894f29efb3b8c0 = {
+  id: "01a08e85-4f28-7573-aaaa-adfbb3d328f6",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "aranya-a3a894f29efb3b8c0",
+  principalSeatName: "aranya",
+  assignmentSlug: "initiative/aranya-one-package",
+  dispatchedAs: "general-purpose",
+  agentId: "01a06cf3-4196-7000-b027-4bdf82d518a6--a3a894f29efb3b8c0",
+} as const satisfies Subagent
