37f7ea64a61f6b91960ce4dd41f66c5202c351e6
Akasha <akasha@alanwalton.com>
2026-09-11 13:37:58 -0600

    eppie: the seat is in akasha as what it states

--- seat-system/seats/pages/eppie/eppie.seat.ts
+++ seat-system/seats/pages/eppie/eppie.seat.ts
@@ -2,7 +2,6 @@ import type { Seat } from "akasha/seat-system/seats/seat.page-type.types.ts"
 
 export const eppie = {
   id: "01a090f1-3fec-7000-b1ed-04747a27266e",
-  pageTypeSlug: "seat",
   type: "seat",
   slug: "eppie",
   persona: "eppie",
