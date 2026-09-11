3c5de38fb6f772c959a468aa8aea1111b010e9d7
Akasha <akasha@alanwalton.com>
2026-09-11 13:11:21 -0600

    astra-aafd6424570ee727d: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/astra-aafd6424570ee727d/astra-aafd6424570ee727d.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const astraAafd6424570ee727d = {
+  id: "01a091e1-a1bf-7846-9910-ca10753f7823",
+  type: "subagent",
+  slug: "astra-aafd6424570ee727d",
+  principalSeatName: "astra",
+  assignmentSlug: "initiative/astra-index-cleanup",
+  dispatchedAs: "general-purpose",
+  agentId: "01a09106-c92d-7000-a0c9-a91e9387843b--aafd6424570ee727d",
+} as const satisfies Subagent
