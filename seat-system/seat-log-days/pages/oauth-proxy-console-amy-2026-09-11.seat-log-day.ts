abb6f70e08455b51f0bb087d255cf7bb769a926d
Akasha <akasha@alanwalton.com>
2026-09-10 18:00:08 -0600

    oauth-proxy-console-amy-2026-09-11: one source's lines for one seat on one day

---
+++ seat-system/seat-log-days/pages/oauth-proxy-console-amy-2026-09-11.seat-log-day.ts
@@ -0,0 +1,11 @@
+import type { SeatLogDay } from "../seat-log-day.page-type.types.ts"
+
+export const oauthProxyConsoleAmy20260911 = {
+  id: "01a08dc3-a933-7a06-93b7-dd0823b584f4",
+  pageTypeSlug: "seat-log-day",
+  type: "seat-log-day",
+  slug: "oauth-proxy-console-amy-2026-09-11",
+  source: "oauth-proxy-console",
+  seatName: "amy",
+  date: "2026-09-11",
+} as const satisfies SeatLogDay
