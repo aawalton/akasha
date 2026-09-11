ce84a4ba9de2c617472de4355348d6e7c68b1104
Akasha <akasha@alanwalton.com>
2026-09-11 09:53:41 -0600

    nimue-aa2c19ea8b87f5c2a: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/nimue-aa2c19ea8b87f5c2a/nimue-aa2c19ea8b87f5c2a.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const nimueAa2c19ea8b87f5c2a = {
+  id: "01a0912c-92d2-7ee0-8f41-910a5c2405f1",
+  type: "subagent",
+  slug: "nimue-aa2c19ea8b87f5c2a",
+  principalSeatName: "nimue",
+  assignmentSlug: "initiative/nimue-code-cleanup",
+  dispatchedAs: "general-purpose",
+  agentId: "01a08cd2-f750-7000-a7d8-5b558d039e25--aa2c19ea8b87f5c2a",
+} as const satisfies Subagent
