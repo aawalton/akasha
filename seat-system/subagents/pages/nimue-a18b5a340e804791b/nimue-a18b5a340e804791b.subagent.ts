9d390d4c68d0baaea2ef696e98ef078f1fff3a89
Akasha <akasha@alanwalton.com>
2026-09-11 11:24:52 -0600

    nimue-a18b5a340e804791b: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/nimue-a18b5a340e804791b/nimue-a18b5a340e804791b.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const nimueA18b5a340e804791b = {
+  id: "01a0916c-478f-7636-a9f6-f60a743b79c4",
+  type: "subagent",
+  slug: "nimue-a18b5a340e804791b",
+  principalSeatName: "nimue",
+  assignmentSlug: "initiative/nimue-code-cleanup",
+  dispatchedAs: "general-purpose",
+  agentId: "01a08cd2-f750-7000-a7d8-5b558d039e25--a18b5a340e804791b",
+} as const satisfies Subagent
