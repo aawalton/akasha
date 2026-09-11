1ec85510862ae7e7434ca59e63f90895bf9998ac
Astra <astra@alanwalton.com>
2026-09-10 19:15:34 -0600

    a collection header's fields carry their own list

--- pages/types/properties/collection-header.record-property.ts
+++ pages/types/properties/collection-header.record-property.ts
@@ -1,11 +1,10 @@
 import type { RecordProperty } from "../../record-properties/record-property.page-type.types.ts"
-import type { List } from "../page-properties/page-property.page-type.ts"
 import type { HeaderFields } from "./header-fields.text-property.types.ts"
 import type { HeaderShowCover } from "./header-show-cover.boolean-property.types.ts"
 
 export type CollectionHeader = {
   showCover?: HeaderShowCover
-  fields: List<HeaderFields>
+  fields: HeaderFields
 }
 
 export const collectionHeader = {
