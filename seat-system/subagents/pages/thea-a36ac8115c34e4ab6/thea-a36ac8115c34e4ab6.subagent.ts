b9d423314a2fa5c2f7da393aae621b8d8561ea8f
Akasha <akasha@alanwalton.com>
2026-09-10 20:13:44 -0600

    thea-a36ac8115c34e4ab6: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/thea-a36ac8115c34e4ab6/thea-a36ac8115c34e4ab6.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../../subagent.page-type.types.ts"
+
+export const theaA36ac8115c34e4ab6 = {
+  id: "01a08e37-104f-77da-a064-3ffba13ae670",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "thea-a36ac8115c34e4ab6",
+  principalSeatName: "thea",
+  assignmentSlug: "initiative/thea-checks-system",
+  dispatchedAs: "general-purpose",
+  agentId: "01a06c31-1b01-7000-b602-fc1a3f96f3a4--a36ac8115c34e4ab6",
+} as const satisfies Subagent
