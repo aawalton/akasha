6b3c1a3283432bf164a7f5a411665533ba5ec0ea
Akasha <akasha@alanwalton.com>
2026-09-11 07:54:16 -0600

    astra-ae1eac71f6487d544: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/astra-ae1eac71f6487d544/astra-ae1eac71f6487d544.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const astraAe1eac71f6487d544 = {
+  id: "01a090b6-06d3-74a1-9451-8c35d19e0a6d",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "astra-ae1eac71f6487d544",
+  principalSeatName: "astra",
+  assignmentSlug: "initiative/astra-generated-types",
+  dispatchedAs: "Explore",
+  agentId: "01a0873b-6b46-7000-8c31-d90227ba00fa--ae1eac71f6487d544",
+} as const satisfies Subagent
