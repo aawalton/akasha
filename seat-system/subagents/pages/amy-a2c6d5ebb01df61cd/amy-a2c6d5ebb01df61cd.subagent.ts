ebdc060e42ba091b451382c6f5fd8e00e31180ad
Akasha <akasha@alanwalton.com>
2026-09-11 09:18:29 -0600

    amy-a2c6d5ebb01df61cd: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/amy-a2c6d5ebb01df61cd/amy-a2c6d5ebb01df61cd.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const amyA2c6d5ebb01df61cd = {
+  id: "01a090ea-bade-7f77-9483-f23b3604db36",
+  type: "subagent",
+  slug: "amy-a2c6d5ebb01df61cd",
+  principalSeatName: "amy",
+  assignmentSlug: "initiative/amy-telnyx-approval",
+  dispatchedAs: "general-purpose",
+  agentId: "01a087b5-1ca3-7000-bf5c-16aae1620c39--a2c6d5ebb01df61cd",
+} as const satisfies Subagent
