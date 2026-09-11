f659434420d5389ad7398754c62c5a418a176f3c
Akasha <akasha@alanwalton.com>
2026-09-11 13:36:37 -0600

    aelwyn: the seat is in akasha as what it states

--- seat-system/seats/pages/aelwyn/aelwyn.seat.ts
+++ seat-system/seats/pages/aelwyn/aelwyn.seat.ts
@@ -2,7 +2,6 @@ import type { Seat } from "akasha/seat-system/seats/seat.page-type.types.ts"
 
 export const aelwyn = {
   id: "01a090ed-0ccf-7000-b711-370c53dfcbb8",
-  pageTypeSlug: "seat",
   type: "seat",
   slug: "aelwyn",
   persona: "aelwyn",
