9d09f030b28b415d606db02c10a6af9b0f35388a
pages-access <pages-access@alanwalton.com>
2026-09-11 07:15:04 -0600

    patchPage(temper-catalog-domain): companion-equipment
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/catalog/temper-world/temper-catalog-domains/pages/companion-equipment.temper-catalog-domain.ts
+++ temper/catalog/temper-world/temper-catalog-domains/pages/companion-equipment.temper-catalog-domain.ts
@@ -9,7 +9,7 @@ export const companionEquipment = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-11T01:31:14.558Z",
+  capturedAt: "2026-09-11T13:14:13.671Z",
   generatorRanForVersion: "eso.live.11.3.6.3240040",
   generatorRanForManifestApiVersion: 101049,
   dormant: true,
