770490d16630bffc95eda87efbf039159d83bf0d
Aranya <aranya@alanwalton.com>
2026-09-10 19:03:36 -0600

    apply 9 files

--- temper/character-build/build-factory/build-factory.module.code.ts
+++ temper/character-build/build-factory/build-factory.module.code.ts
@@ -1,7 +1,7 @@
 import { championPoints } from "akasha/temper/champion-points/champion-point-source/champion-point-source.module.code.ts"
+import type { CharacterState } from "akasha/temper/character-build/build-types/build-types.module.code.ts"
+import { buildId } from "akasha/temper/formula-framework/branded-id/branded-id.module.code.ts"
 import { getSubcategory } from "akasha/utils/narrow/get-subcategory/get-subcategory.module.code.ts"
-import { buildId } from "../../formula-framework/branded-id/branded-id.module.code.ts"
-import type { CharacterState } from "../build-types/build-types.module.code.ts"
 
 export const createNewCharacter = (): CharacterState => ({
   id: buildId(""),
--- temper/character-build/character-state-schema/character-state-schema.module.ts
+++ temper/character-build/character-state-schema/character-state-schema.module.ts
@@ -1,4 +1,4 @@
-import type { Module } from "../../../code-system/modules/module.page-type.types.ts"
+import type { Module } from "akasha/code-system/modules/module.page-type.types.ts"
 
 export const characterStateSchema = {
   id: "01a06279-228f-7f7c-aad1-a8a7f5ac918c",
