7b92e9c29edbab5c46aebb3119a931c7d01feda2
Akasha <akasha@alanwalton.com>
2026-09-11 13:37:44 -0600

    astra: the seat is in akasha as what it states

--- seat-system/seats/pages/astra/astra.seat.ts
+++ seat-system/seats/pages/astra/astra.seat.ts
@@ -2,7 +2,6 @@ import type { Seat } from "akasha/seat-system/seats/seat.page-type.types.ts"
 
 export const astra = {
   id: "01a09106-c92d-7000-a0c9-a91e9387843b",
-  pageTypeSlug: "seat",
   type: "seat",
   slug: "astra",
   persona: "astra",
