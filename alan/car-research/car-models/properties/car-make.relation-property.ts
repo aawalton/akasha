c34fb7bd6126d5128c871dd4f71a3d8acf7de6e1
Aelwyn <aelwyn@alanwalton.com>
2026-09-11 12:46:25 -0600

    name the car page type car-make again

--- alan/car-research/car-models/properties/car-make.relation-property.ts
+++ alan/car-research/car-models/properties/car-make.relation-property.ts
@@ -7,6 +7,6 @@ export const carMake = {
   slug: "car-make",
   propertySlug: "car-make",
   definition: "the make that builds this nameplate",
-  targetPageType: "page-type/car-marque",
+  targetPageType: "page-type/car-make",
   types: "ts",
 } as const satisfies RelationProperty
