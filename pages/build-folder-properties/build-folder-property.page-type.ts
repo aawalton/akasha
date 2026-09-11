da80a8cfa1a8b48583aa3b7ff262a130033055c5
Aranya <aranya@alanwalton.com>
2026-09-10 21:07:10 -0600

    every body under pages/build-folder-properties names a file from the root rather than by a relative path

--- pages/build-folder-properties/build-folder-property.page-type.ts
+++ pages/build-folder-properties/build-folder-property.page-type.ts
@@ -1,4 +1,4 @@
-import type { PageType } from "../types/page-type.page-type.types.ts"
+import type { PageType } from "akasha/pages/types/page-type.page-type.types.ts"
 
 export const buildFolderProperty = {
   id: "01a081cd-5b5f-731e-95ed-0ea5ee3b352b",
