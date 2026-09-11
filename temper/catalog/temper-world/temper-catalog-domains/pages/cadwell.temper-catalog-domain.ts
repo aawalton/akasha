3bdb090888d0f58a46fdd774c2a3f9c7c037919b
pages-access <pages-access@alanwalton.com>
2026-09-11 12:43:16 -0600

    patchPage(temper-catalog-domain): cadwell
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/catalog/temper-world/temper-catalog-domains/pages/cadwell.temper-catalog-domain.ts
+++ temper/catalog/temper-world/temper-catalog-domains/pages/cadwell.temper-catalog-domain.ts
@@ -9,7 +9,7 @@ export const cadwell = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-11T18:39:10.600Z",
+  capturedAt: "2026-09-11T18:43:04.133Z",
   generatorRanForVersion: "eso.live.12.0.6.3274791",
   generatorRanForManifestApiVersion: 101050,
 } as const satisfies TemperCatalogDomain
