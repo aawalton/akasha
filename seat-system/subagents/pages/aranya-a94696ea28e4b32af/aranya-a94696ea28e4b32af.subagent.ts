392d0c4e71cceee432240e8a7555d2526eabc82b
Akasha <akasha@alanwalton.com>
2026-09-11 10:56:02 -0600

    aranya-a94696ea28e4b32af: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/aranya-a94696ea28e4b32af/aranya-a94696ea28e4b32af.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const aranyaA94696ea28e4b32af = {
+  id: "01a0915e-404a-7169-b2be-3ee955b6ec98",
+  type: "subagent",
+  slug: "aranya-a94696ea28e4b32af",
+  principalSeatName: "aranya",
+  assignmentSlug: "initiative/aranya-workstation-service-stability",
+  dispatchedAs: "Explore",
+  agentId: "01a0914e-f2be-7000-b6f3-ab55b8b5560a--a94696ea28e4b32af",
+} as const satisfies Subagent
