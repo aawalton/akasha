39f1d3ce0294c5ef7d8274945fa8312d7033af53
Akasha <akasha@alanwalton.com>
2026-09-10 21:09:35 -0600

    astra-affdb21855520c949: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/astra-affdb21855520c949/astra-affdb21855520c949.subagent.ts
@@ -0,0 +1,12 @@
+import type { Subagent } from "../../subagent.page-type.types.ts"
+
+export const astraAffdb21855520c949 = {
+  id: "01a08e71-1c6b-7408-b80b-3c1eeb4f949b",
+  pageTypeSlug: "subagent",
+  type: "subagent",
+  slug: "astra-affdb21855520c949",
+  principalSeatName: "astra",
+  assignmentSlug: "initiative/astra-generated-types",
+  dispatchedAs: "Explore",
+  agentId: "01a0873b-6b46-7000-8c31-d90227ba00fa--affdb21855520c949",
+} as const satisfies Subagent
