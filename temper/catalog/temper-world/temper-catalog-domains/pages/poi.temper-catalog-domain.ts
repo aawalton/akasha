2314cecfc3462ce30721fa62fd1bba0bd185a556
Aranya <aranya@alanwalton.com>
2026-09-10 22:20:17 -0600

    fold relative page-type imports to root-spelled in temper-catalog-domains pages

--- temper/catalog/temper-world/temper-catalog-domains/pages/poi.temper-catalog-domain.ts
+++ temper/catalog/temper-world/temper-catalog-domains/pages/poi.temper-catalog-domain.ts
@@ -1,4 +1,4 @@
-import type { TemperCatalogDomain } from "../temper-catalog-domain.page-type.types.ts"
+import type { TemperCatalogDomain } from "akasha/temper/catalog/temper-world/temper-catalog-domains/temper-catalog-domain.page-type.types.ts"
 
 export const poi = {
   id: "01a05fc5-7430-7333-91b4-aab03e8bd3cb",
--- temper/catalog/temper-world/temper-catalog-domains/pages/zone-completion.temper-catalog-domain.ts
+++ temper/catalog/temper-world/temper-catalog-domains/pages/zone-completion.temper-catalog-domain.ts
@@ -1,4 +1,4 @@
-import type { TemperCatalogDomain } from "../temper-catalog-domain.page-type.types.ts"
+import type { TemperCatalogDomain } from "akasha/temper/catalog/temper-world/temper-catalog-domains/temper-catalog-domain.page-type.types.ts"
 
 export const zoneCompletion = {
   id: "01a05fc5-7432-7be7-bcd6-72aa7861e704",
