34d12007da5efe622f3efc681f68dc2c6c9576f5
Aranya <aranya@alanwalton.com>
2026-09-10 20:02:09 -0600

    code-system/editor/editor.domain.ts names a file from the root rather than by a relative path

--- code-system/editor/editor.domain.ts
+++ code-system/editor/editor.domain.ts
@@ -1,4 +1,4 @@
-import type { Domain } from "../../domains/domain.page-type.types.ts"
+import type { Domain } from "akasha/domains/domain.page-type.types.ts"
 
 export const editor = {
   id: "01a08d6b-4306-7a19-8ee9-a2556ebf0cfa",
