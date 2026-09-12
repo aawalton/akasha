170f6243064c41d52531d142ceb63a5efdae50fb
pages-access <pages-access@alanwalton.com>
2026-09-12 07:01:52 -0600

    patchPage(temper-catalog-domain): zone-completion
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/catalog/temper-world/temper-catalog-domains/pages/zone-completion.temper-catalog-domain.ts
+++ temper/catalog/temper-world/temper-catalog-domains/pages/zone-completion.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const zoneCompletion = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-12T12:54:50.863Z",
+  capturedAt: "2026-09-12T13:00:29.161Z",
   generatorRanForVersion: "eso.live.12.0.6.3274791",
   generatorRanForManifestApiVersion: 101050,
 } as const satisfies TemperCatalogDomain
