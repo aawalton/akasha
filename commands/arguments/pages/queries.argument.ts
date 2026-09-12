8e3b9c538c7588b984b9838093f928ce3c160ca3
Athena <athena@alanwalton.com>
2026-09-12 02:48:37 -0600

    the two track-query pages carry the flag spelling they would have had, as a word argument does

--- commands/arguments/pages/queries.argument.ts
+++ commands/arguments/pages/queries.argument.ts
@@ -4,7 +4,7 @@ export const queries = {
   id: "01a094c1-fbe0-7bd5-b4d0-87494f2cff59",
   type: "argument",
   slug: "queries",
-  said: "<query>...",
+  said: "--queries",
   takes: "the track queries, played in the order they are said",
   value: "text",
   placeholder: "query",
--- commands/arguments/pages/query.argument.ts
+++ commands/arguments/pages/query.argument.ts
@@ -4,7 +4,7 @@ export const query = {
   id: "01a094c1-688c-7f20-a6f3-46b64ad1b922",
   type: "argument",
   slug: "query",
-  said: "<query>",
+  said: "--query",
   takes: "the track looked for",
   value: "text",
   placeholder: "query",
