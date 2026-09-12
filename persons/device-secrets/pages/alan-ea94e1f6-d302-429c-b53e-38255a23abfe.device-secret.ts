dac62c205a6ed261d9f7399aa1ed78e79ec9d591
alanwalton web <web@alanwalton.com>
2026-09-11 19:25:24 -0600

    a device secret is minted for alan
    Written-by: alanwalton web <web@alanwalton.com>

--- persons/device-secrets/pages/alan-ea94e1f6-d302-429c-b53e-38255a23abfe.device-secret.ts
+++ persons/device-secrets/pages/alan-ea94e1f6-d302-429c-b53e-38255a23abfe.device-secret.ts
@@ -6,5 +6,5 @@ export const alanEa94e1f6D302429cB53e38255a23abfe = {
   slug: "alan-ea94e1f6-d302-429c-b53e-38255a23abfe",
   userId: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   deviceId: "EA94E1F6-D302-429C-B53E-38255A23ABFE",
-  secretHash: "5a2a5c4e0e61d9416a068919684b16652d5476f923efda7fc1f144e5bb225599",
+  secretHash: "16619a955abd4a3f3594db035d774b90b5e8535cfaca3826c7e0991ff0a05559",
 } as const satisfies DeviceSecret
