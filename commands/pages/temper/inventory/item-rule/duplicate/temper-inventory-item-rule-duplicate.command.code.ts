cdd8fd54fd6c467711f7dc1a6cf9ca0f01f3c835
Athena <athena@alanwalton.com>
2026-09-12 03:41:21 -0600

    temper inventory item rule duplicate reads its argument through its page

--- commands/pages/temper/inventory/item-rule/duplicate/temper-inventory-item-rule-duplicate.command.code.ts
+++ commands/pages/temper/inventory/item-rule/duplicate/temper-inventory-item-rule-duplicate.command.code.ts
@@ -1,20 +1,18 @@
+import { takenFor } from "akasha/commands/arguments/argument-taking/argument-taking.module.code.ts"
+import { itemRuleId } from "akasha/commands/arguments/pages/item-rule-id.argument.ts"
 import { answering } from "akasha/commands/modules/answering/command-answering.module.code.ts"
 import type { Answer, Given } from "akasha/commands/modules/calling/calling.module.code.ts"
+import { temperInventoryItemRuleDuplicate as page } from "akasha/commands/pages/temper/inventory/item-rule/duplicate/temper-inventory-item-rule-duplicate.command.ts"
 import {
   copiedRule,
-  readIn,
   refusedAll,
-  shapeOf,
 } from "akasha/temper/commands/inventory-rule-calling/inventory-rule-calling.module.code.ts"
-
-const SHAPE = shapeOf([], { namesARule: true })
 
 export async function temperInventoryItemRuleDuplicate(
   argv: readonly string[],
   given: Given
 ): Promise<Answer> {
-  const read = readIn(argv, given.calledAs, SHAPE)
+  const read = takenFor(argv, given.calledAs, page, [itemRuleId])
   if ("refused" in read) return refusedAll(read.refused)
-  const id = read.id ?? ""
-  return await answering(() => copiedRule("item", id))
+  return await answering(() => copiedRule("item", read.taken.itemRuleId))
 }
