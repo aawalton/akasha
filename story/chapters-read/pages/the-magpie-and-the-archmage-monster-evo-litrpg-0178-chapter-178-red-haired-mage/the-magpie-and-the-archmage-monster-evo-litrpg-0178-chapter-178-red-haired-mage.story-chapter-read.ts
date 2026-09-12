25abf4b9118c526cada975c52166c4f6eda809d4
Akasha <akasha@alanwalton.com>
2026-09-12 01:04:11 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/the-magpie-and-the-archmage-monster-evo-litrpg-0178-chapter-178-red-haired-mage/the-magpie-and-the-archmage-monster-evo-litrpg-0178-chapter-178-red-haired-mage.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const theMagpieAndTheArchmageMonsterEvoLitrpg0178Chapter178RedHairedMage = {
+  id: "01a0946e-3772-727f-8fe0-109d095909c5",
+  type: "story-chapter-read",
+  slug: "the-magpie-and-the-archmage-monster-evo-litrpg-0178-chapter-178-red-haired-mage",
+  position: 178,
+  publishedAt: "2026-09-12",
+  unit: "words",
+  externalId: "3947524",
+  source: "royal-road",
+  title: "Chapter 178 – Red-Haired Mage",
+  story: "story-read/the-magpie-and-the-archmage-monster-evo-litrpg",
+  ownLength: 2268,
+  externalLink:
+    "https://www.royalroad.com/fiction/129182/the-magpie-and-the-archmage-book-2-complete/chapter/3947524/chapter-178-red-haired-mage",
+  prose: "txt",
+} as const satisfies StoryChapterRead
