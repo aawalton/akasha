ec6afd8a33827a59fd9fb8e0cf0a15718846fd49
Akasha <akasha@alanwalton.com>
2026-09-11 19:01:43 -0600

    ops-food: the food entry food-entry-2026-09-11-steak-burrito-bowl

--- alan/track/food-entries/pages/2026-09-11-steak-burrito-bowl/food-entry-2026-09-11-steak-burrito-bowl.food-entry.ts
+++ alan/track/food-entries/pages/2026-09-11-steak-burrito-bowl/food-entry-2026-09-11-steak-burrito-bowl.food-entry.ts
@@ -4,6 +4,7 @@ export const foodEntry20260911SteakBurritoBowl = {
   id: "01a09322-472b-7000-92e6-2cd47bd56c09",
   type: "food-entry",
   slug: "food-entry-2026-09-11-steak-burrito-bowl",
+  cover: "/api/image/01a09322-472b-7000-92e6-2cd47bd56c09",
   title: "Steak Burrito Bowl",
   happenedAt: "2026-09-12T01:01:32.268Z",
   plantGrams: 210,
