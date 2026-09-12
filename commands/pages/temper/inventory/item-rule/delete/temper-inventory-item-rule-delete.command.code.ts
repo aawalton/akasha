b1ade1aefbfd8724fc3b9e8d8e5b23ab01e3b91a
Athena <athena@alanwalton.com>
2026-09-12 03:41:42 -0600

    temper inventory item rule delete reads its arguments through its page

--- commands/pages/temper/inventory/item-rule/delete/temper-inventory-item-rule-delete.command.code.ts
+++ commands/pages/temper/inventory/item-rule/delete/temper-inventory-item-rule-delete.command.code.ts
@@ -1,21 +1,20 @@
+import { takenFor } from "akasha/commands/arguments/argument-taking/argument-taking.module.code.ts"
+import { force } from "akasha/commands/arguments/pages/force.argument.ts"
+import { itemRuleId } from "akasha/commands/arguments/pages/item-rule-id.argument.ts"
 import { answering } from "akasha/commands/modules/answering/command-answering.module.code.ts"
 import type { Answer, Given } from "akasha/commands/modules/calling/calling.module.code.ts"
+import { temperInventoryItemRuleDelete as page } from "akasha/commands/pages/temper/inventory/item-rule/delete/temper-inventory-item-rule-delete.command.ts"
 import {
   droppedRule,
-  FORCE,
-  readIn,
   refusedAll,
-  shapeOf,
 } from "akasha/temper/commands/inventory-rule-calling/inventory-rule-calling.module.code.ts"
-
-const SHAPE = shapeOf([FORCE], { alone: [FORCE], namesARule: true })
 
 export async function temperInventoryItemRuleDelete(
   argv: readonly string[],
   given: Given
 ): Promise<Answer> {
-  const read = readIn(argv, given.calledAs, SHAPE)
+  const read = takenFor(argv, given.calledAs, page, [force, itemRuleId])
   if ("refused" in read) return refusedAll(read.refused)
-  const id = read.id ?? ""
-  return await answering(() => droppedRule("item", id, read.said.has(FORCE)))
+  const taken = read.taken
+  return await answering(() => droppedRule("item", taken.itemRuleId, taken.force))
 }
