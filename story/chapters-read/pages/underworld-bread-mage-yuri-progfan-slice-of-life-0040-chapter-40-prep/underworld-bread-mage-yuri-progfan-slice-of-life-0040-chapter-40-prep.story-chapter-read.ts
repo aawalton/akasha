35e2043777cb2dcf179901bf4a89d4f372960fe8
Akasha <akasha@alanwalton.com>
2026-09-12 08:04:28 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/underworld-bread-mage-yuri-progfan-slice-of-life-0040-chapter-40-prep/underworld-bread-mage-yuri-progfan-slice-of-life-0040-chapter-40-prep.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const underworldBreadMageYuriProgfanSliceOfLife0040Chapter40Prep = {
+  id: "01a095ee-ef8b-7330-87b2-eb97484d307d",
+  type: "story-chapter-read",
+  slug: "underworld-bread-mage-yuri-progfan-slice-of-life-0040-chapter-40-prep",
+  position: 40,
+  publishedAt: "2026-09-12",
+  unit: "words",
+  externalId: "3948565",
+  source: "royal-road",
+  title: "Chapter 40: Prep",
+  story: "story-read/underworld-bread-mage-yuri-progfan-slice-of-life",
+  ownLength: 1472,
+  externalLink:
+    "https://www.royalroad.com/fiction/179650/underworld-bread-mage-yuri-lit-rpg-slice-of-life/chapter/3948565/chapter-40-prep",
+  prose: "txt",
+} as const satisfies StoryChapterRead
