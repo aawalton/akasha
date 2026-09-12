c7f068ef23b53cc09c905aa7503abe83f4a4fb9b
Athena <athena@alanwalton.com>
2026-09-12 00:23:41 -0600

    measure page exports the name its slug spells

--- commands/pages/measure/page/measure-page.command.code.ts
+++ commands/pages/measure/page/measure-page.command.code.ts
@@ -5,6 +5,6 @@ import {
   linesOf,
 } from "akasha/commands/pages/measure/page/page-measuring/page-measuring.module.code.ts"
 
-export function measurePages(_argv: readonly string[], given: Given): Answer {
+export function measurePage(_argv: readonly string[], given: Given): Answer {
   return told(linesOf(countsIn(given.root)))
 }
