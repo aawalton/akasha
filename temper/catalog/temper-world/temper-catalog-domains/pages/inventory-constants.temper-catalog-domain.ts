24cd09eaf33433d2cde010b67ffeb9f1721ceaf9
pages-access <pages-access@alanwalton.com>
2026-09-12 07:25:43 -0600

    patchPage(temper-catalog-domain): inventory-constants
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/catalog/temper-world/temper-catalog-domains/pages/inventory-constants.temper-catalog-domain.ts
+++ temper/catalog/temper-world/temper-catalog-domains/pages/inventory-constants.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const inventoryConstants = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-12T13:13:27.262Z",
+  capturedAt: "2026-09-12T13:23:35.730Z",
   generatorRanForVersion: "eso.live.11.3.6.3240040",
   generatorRanForManifestApiVersion: 101049,
   dormant: true,
