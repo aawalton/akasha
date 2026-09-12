5cba434090bae91e230848815e61dde4091a06bc
pages-access <pages-access@alanwalton.com>
2026-09-12 07:02:28 -0600

    patchPage(temper-catalog-domain): furniture
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/catalog/temper-world/temper-catalog-domains/pages/furniture.temper-catalog-domain.ts
+++ temper/catalog/temper-world/temper-catalog-domains/pages/furniture.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const furniture = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-12T12:54:50.863Z",
+  capturedAt: "2026-09-12T13:00:29.161Z",
   generatorRanForVersion: "eso.live.11.3.6.3240040",
   generatorRanForManifestApiVersion: 101049,
   dormant: true,
