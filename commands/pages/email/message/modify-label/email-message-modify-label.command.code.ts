a2bc810cbce69756c3f487dec07aa7c0e0c88d72
Athena <athena@alanwalton.com>
2026-09-12 00:16:41 -0600

    email message modify-label exports the name its slug spells

--- commands/pages/email/message/modify-label/email-message-modify-label.command.code.ts
+++ commands/pages/email/message/modify-label/email-message-modify-label.command.code.ts
@@ -25,7 +25,7 @@ export function readIn(argv: readonly string[]): Read {
   return readTaking(argv, TAKING)
 }
 
-export function emailMessagesModifyLabels(argv: readonly string[]): Promise<Answer> {
+export function emailMessageModifyLabel(argv: readonly string[]): Promise<Answer> {
   const said = readIn(argv)
   if ("refused" in said) return Promise.resolve(refusing(said.refused, 1))
   return answeredBy(async () => {
