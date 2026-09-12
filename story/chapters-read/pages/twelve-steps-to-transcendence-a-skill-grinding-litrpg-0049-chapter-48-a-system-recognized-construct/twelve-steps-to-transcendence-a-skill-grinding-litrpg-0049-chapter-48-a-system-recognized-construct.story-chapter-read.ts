05f29863e865110f06a9f5fe9ab6d5f0b0eff652
Akasha <akasha@alanwalton.com>
2026-09-12 16:07:06 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/twelve-steps-to-transcendence-a-skill-grinding-litrpg-0049-chapter-48-a-system-recognized-construct/twelve-steps-to-transcendence-a-skill-grinding-litrpg-0049-chapter-48-a-system-recognized-construct.story-chapter-read.ts
@@ -0,0 +1,19 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const twelveStepsToTranscendenceASkillGrindingLitrpg0049Chapter48ASystemRecognizedConstruct =
+  {
+    id: "01a097a8-dfc7-7293-9b5d-af8cf3b057b3",
+    type: "story-chapter-read",
+    slug: "twelve-steps-to-transcendence-a-skill-grinding-litrpg-0049-chapter-48-a-system-recognized-construct",
+    position: 49,
+    publishedAt: "2026-09-12",
+    unit: "words",
+    externalId: "3950249",
+    source: "royal-road",
+    title: "Chapter 48: A System-Recognized Construct",
+    story: "story-read/twelve-steps-to-transcendence-a-skill-grinding-litrpg",
+    ownLength: 2222,
+    externalLink:
+      "https://www.royalroad.com/fiction/181591/12-steps-to-transcendence-a-skill-grinding-litrpg/chapter/3950249/chapter-48-a-system-recognized-construct",
+    prose: "txt",
+  } as const satisfies StoryChapterRead
