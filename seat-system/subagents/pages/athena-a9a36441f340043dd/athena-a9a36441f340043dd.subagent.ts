0191f22543079ef88aebf3da2fe63a4d6cd08111
Akasha <akasha@alanwalton.com>
2026-09-12 04:39:12 -0600

    athena-a9a36441f340043dd: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/athena-a9a36441f340043dd/athena-a9a36441f340043dd.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const athenaA9a36441f340043dd = {
+  id: "01a0951f-1dfa-7f08-96e7-11f84169e383",
+  type: "subagent",
+  slug: "athena-a9a36441f340043dd",
+  principalSeatName: "athena",
+  assignmentSlug: "initiative/athena-commands-cleanup",
+  dispatchedAs: "general-purpose",
+  agentId: "01a090bf-384d-7000-a429-e00697781e56--a9a36441f340043dd",
+} as const satisfies Subagent
