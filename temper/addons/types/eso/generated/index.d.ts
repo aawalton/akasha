ecc02a02a0c324f5fc223757f28db602ceffa91c
Akasha <akasha@alanwalton.com>
2026-09-12 05:39:05 -0600

    the game's API declarations, read out of the game's own documentation

---
+++ temper/addons/types/eso/generated/index.d.ts
@@ -0,0 +1,6 @@
+// Generated from the ~/esoui clone by akasha temper eso generate declaration
+// ESO-API-Version: 101050  (source freshness marker; verified by check-eso-declaration-fresh)
+/// <reference path="./enums.d.ts" />
+/// <reference path="./functions.d.ts" />
+/// <reference path="./events.d.ts" />
+/// <reference path="./objects.d.ts" />
