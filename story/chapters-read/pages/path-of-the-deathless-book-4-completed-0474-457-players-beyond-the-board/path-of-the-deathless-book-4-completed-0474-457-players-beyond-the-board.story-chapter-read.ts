eb543aae3a733dad8838c58c3ee43f14b5935a87
Akasha <akasha@alanwalton.com>
2026-09-12 12:05:06 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/path-of-the-deathless-book-4-completed-0474-457-players-beyond-the-board/path-of-the-deathless-book-4-completed-0474-457-players-beyond-the-board.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const pathOfTheDeathlessBook4Completed0474457PlayersBeyondTheBoard = {
+  id: "01a096cb-52b3-7619-8766-aef8fa9c216a",
+  type: "story-chapter-read",
+  slug: "path-of-the-deathless-book-4-completed-0474-457-players-beyond-the-board",
+  position: 474,
+  publishedAt: "2026-09-12",
+  unit: "words",
+  externalId: "3949506",
+  source: "royal-road",
+  title: "457 Players Beyond the Board",
+  story: "story-read/path-of-the-deathless-book-4-completed",
+  ownLength: 5160,
+  externalLink:
+    "https://www.royalroad.com/fiction/122502/path-of-the-deathless-book-3-stubbed/chapter/3949506/457-players-beyond-the-board",
+  prose: "txt",
+} as const satisfies StoryChapterRead
