46f9d2644bcc53948431d03f4f64c91c86260dbb
Akasha <akasha@alanwalton.com>
2026-09-12 06:41:54 -0600

    athena-a95da8e27e36c338c: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/athena-a95da8e27e36c338c/athena-a95da8e27e36c338c.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const athenaA95da8e27e36c338c = {
+  id: "01a095a3-5e46-77bb-97be-93c4d28f7afb",
+  type: "subagent",
+  slug: "athena-a95da8e27e36c338c",
+  principalSeatName: "athena",
+  assignmentSlug: "initiative/athena-commands-cleanup",
+  dispatchedAs: "Explore",
+  agentId: "01a090bf-384d-7000-a429-e00697781e56--a95da8e27e36c338c",
+} as const satisfies Subagent
