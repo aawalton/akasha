76668ec1fce4bd9d14679789856bbbb4c839fb8b
Akasha <akasha@alanwalton.com>
2026-09-12 04:05:24 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/passive-aggressive-reverse-portal-invasion-litrpg-0077-book-2-chapter-2/passive-aggressive-reverse-portal-invasion-litrpg-0077-book-2-chapter-2.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const passiveAggressiveReversePortalInvasionLitrpg0077Book2Chapter2 = {
+  id: "01a09514-1e4d-7bf7-999c-ecb949c825ec",
+  type: "story-chapter-read",
+  slug: "passive-aggressive-reverse-portal-invasion-litrpg-0077-book-2-chapter-2",
+  position: 77,
+  publishedAt: "2026-09-12",
+  unit: "words",
+  externalId: "3947994",
+  source: "royal-road",
+  title: "Book 2, Chapter 2",
+  story: "story-read/passive-aggressive-reverse-portal-invasion-litrpg",
+  ownLength: 1968,
+  externalLink:
+    "https://www.royalroad.com/fiction/170563/passive-aggressive-reverse-portal-invasion-litrpg/chapter/3947994/book-2-chapter-2",
+  prose: "txt",
+} as const satisfies StoryChapterRead
