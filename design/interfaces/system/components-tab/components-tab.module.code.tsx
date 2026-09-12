01982cb20b039003d01c8adf1ac3d3fd2ee0f41b
Akasha <akasha@alanwalton.com>
2026-09-12 12:25:01 -0600

    apply 9 files

--- design/interfaces/system/components-tab/components-tab.module.code.tsx
+++ design/interfaces/system/components-tab/components-tab.module.code.tsx
@@ -3,10 +3,6 @@
 import { PageTabHeader } from "akasha/design/interfaces/layout/page-tab-header/page-tab-header.module.code.tsx"
 import { ResponsiveColumns } from "akasha/design/interfaces/layout/responsive-columns/responsive-columns.module.code.tsx"
 import { TabsContent } from "akasha/design/interfaces/patterns/tabs/tabs.module.code.tsx"
-import { ComponentsCorePanels } from "akasha/design/interfaces/system/components-core-panels/components-core-panels.module.code.tsx"
-import { ComponentsFormControlsPanels } from "akasha/design/interfaces/system/components-form-controls-panels/components-form-controls-panels.module.code.tsx"
-import { ComponentsInputCompositePanels } from "akasha/design/interfaces/system/components-input-composite-panels/components-input-composite-panels.module.code.tsx"
-import { ComponentsInputPanels } from "akasha/design/interfaces/system/components-input-panels/components-input-panels.module.code.tsx"
 import { ComponentsItemDisplayPanels } from "akasha/design/interfaces/system/components-item-display-panels/components-item-display-panels.module.code.tsx"
 import { ComponentsListGridPanels } from "akasha/design/interfaces/system/components-list-grid-panels/components-list-grid-panels.module.code.tsx"
 import { ComponentsMenuPanels } from "akasha/design/interfaces/system/components-menu-panels/components-menu-panels.module.code.tsx"
@@ -17,6 +13,10 @@ import { ComponentsSortFilterPanels } from "akasha/design/interfaces/system/comp
 import { ComponentsTextDisplayPanels } from "akasha/design/interfaces/system/components-text-display-panels/components-text-display-panels.module.code.tsx"
 import { ComponentsCompoundNavPanels } from "akasha/design/interfaces/system/modules/components-compound-nav-panels/components-compound-nav-panels.module.code.tsx"
 import { ComponentsCompoundPanels } from "akasha/design/interfaces/system/modules/components-compound-panels/components-compound-panels.module.code.tsx"
+import { ComponentsCorePanels } from "akasha/design/interfaces/system/modules/components-core-panels/components-core-panels.module.code.tsx"
+import { ComponentsFormControlsPanels } from "akasha/design/interfaces/system/modules/components-form-controls-panels/components-form-controls-panels.module.code.tsx"
+import { ComponentsInputCompositePanels } from "akasha/design/interfaces/system/modules/components-input-composite-panels/components-input-composite-panels.module.code.tsx"
+import { ComponentsInputPanels } from "akasha/design/interfaces/system/modules/components-input-panels/components-input-panels.module.code.tsx"
 
 export function ComponentsTabContent() {
   return (
