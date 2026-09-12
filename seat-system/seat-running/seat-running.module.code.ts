0b8bb6a7e8852897326e69835af1e8fd0fb8bf3b
Ryn <ryn@alanwalton.com>
2026-09-12 06:06:12 -0600

    apply agents/seats/modules/defaults/seat-defaults.module.code.ts, agents/seats/modules/defaults/seat-defaults.module.ts, seat-system/seat-running/seat-running.module.code.ts

--- seat-system/seat-running/seat-running.module.code.ts
+++ seat-system/seat-running/seat-running.module.code.ts
@@ -8,6 +8,7 @@ import {
   type Declaration,
   recordedModeOf,
 } from "akasha/agents/attributes/agent-attributes.module.code.ts"
+import { defaultLines } from "akasha/agents/seats/modules/defaults/seat-defaults.module.code.ts"
 import {
   launchOf,
   launchStating,
@@ -32,7 +33,6 @@ import {
 } from "akasha/seat-system/seat-answering/seat-answering.module.code.ts"
 import { type Args, parseArgs } from "akasha/seat-system/seat-args/seat-args.module.code.ts"
 import { attributeFor } from "akasha/seat-system/seat-attribute/seat-attribute.module.code.ts"
-import { defaultLines } from "akasha/seat-system/seat-defaults/seat-defaults.module.code.ts"
 import { nameStanding } from "akasha/seat-system/seat-name-stands/seat-name-stands.module.code.ts"
 import {
   nameableFrom,
