9e4b48ac5478fba1a19b9d3fe1a891fea5281edb
Akasha <akasha@alanwalton.com>
2026-09-12 02:05:27 -0600

    royal road sync 2 page(s)

---
+++ story/chapters-read/pages/bunny-girl-evolution-0035-223-the-prodigal-bunny-princess-returns/bunny-girl-evolution-0035-223-the-prodigal-bunny-princess-returns.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const bunnyGirlEvolution0035223TheProdigalBunnyPrincessReturns = {
+  id: "01a094a6-4ea2-7052-91cd-4ae70e9d7fa1",
+  type: "story-chapter-read",
+  slug: "bunny-girl-evolution-0035-223-the-prodigal-bunny-princess-returns",
+  position: 35,
+  publishedAt: "2026-09-12",
+  unit: "words",
+  externalId: "3947612",
+  source: "royal-road",
+  title: "223 - The Prodigal Bunny Princess Returns",
+  story: "story-read/bunny-girl-evolution",
+  ownLength: 3007,
+  externalLink:
+    "https://www.royalroad.com/fiction/90435/bunny-girl-evolution/chapter/3947612/223-the-prodigal-bunny-princess-returns",
+  prose: "txt",
+} as const satisfies StoryChapterRead
---
+++ story/chapters-read/pages/just-add-mana-0190-185-book-4-chapter-29-the-black-vault-pt-5/just-add-mana-0190-185-book-4-chapter-29-the-black-vault-pt-5.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const justAddMana0190185Book4Chapter29TheBlackVaultPt5 = {
+  id: "01a094a6-4f50-78a1-a28d-d037526ded33",
+  type: "story-chapter-read",
+  slug: "just-add-mana-0190-185-book-4-chapter-29-the-black-vault-pt-5",
+  position: 190,
+  publishedAt: "2026-09-12",
+  unit: "words",
+  externalId: "3947698",
+  source: "royal-road",
+  title: "185 — Book 4, Chapter 29 — The Black Vault, Pt 5",
+  story: "story-read/just-add-mana",
+  ownLength: 3331,
+  externalLink:
+    "https://www.royalroad.com/fiction/125163/just-add-mana/chapter/3947698/185-book-4-chapter-29-the-black-vault-pt-5",
+  prose: "txt",
+} as const satisfies StoryChapterRead
