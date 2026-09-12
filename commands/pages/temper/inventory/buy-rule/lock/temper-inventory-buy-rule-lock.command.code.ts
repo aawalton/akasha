fc0e2c07dbb88effacd9b494ddc599b6c23d35ea
Athena <athena@alanwalton.com>
2026-09-12 03:42:09 -0600

    temper inventory buy rule lock reads its argument through its page

--- commands/pages/temper/inventory/buy-rule/lock/temper-inventory-buy-rule-lock.command.code.ts
+++ commands/pages/temper/inventory/buy-rule/lock/temper-inventory-buy-rule-lock.command.code.ts
@@ -1,20 +1,18 @@
+import { takenFor } from "akasha/commands/arguments/argument-taking/argument-taking.module.code.ts"
+import { buyRuleId } from "akasha/commands/arguments/pages/buy-rule-id.argument.ts"
 import { answering } from "akasha/commands/modules/answering/command-answering.module.code.ts"
 import type { Answer, Given } from "akasha/commands/modules/calling/calling.module.code.ts"
+import { temperInventoryBuyRuleLock as page } from "akasha/commands/pages/temper/inventory/buy-rule/lock/temper-inventory-buy-rule-lock.command.ts"
 import {
   lockedRule,
-  readIn,
   refusedAll,
-  shapeOf,
 } from "akasha/temper/commands/inventory-rule-calling/inventory-rule-calling.module.code.ts"
-
-const SHAPE = shapeOf([], { namesARule: true })
 
 export async function temperInventoryBuyRuleLock(
   argv: readonly string[],
   given: Given
 ): Promise<Answer> {
-  const read = readIn(argv, given.calledAs, SHAPE)
+  const read = takenFor(argv, given.calledAs, page, [buyRuleId])
   if ("refused" in read) return refusedAll(read.refused)
-  const id = read.id ?? ""
-  return await answering(() => lockedRule("buy", id, true))
+  return await answering(() => lockedRule("buy", read.taken.buyRuleId, true))
 }
