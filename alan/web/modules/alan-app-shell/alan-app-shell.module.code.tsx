397fcadedb4b3cdac38717dc11d270bae3bd9138
Akasha <akasha@alanwalton.com>
2026-09-12 12:42:21 -0600

    apply alan/web/modules/alan-app-shell/alan-app-shell.module.code.tsx, pages/ui/components/modules/create-page-dialog/create-page-dialog.module.code.tsx, pages/ui/components/modules/create-page-dialog/create-page-dialog.module.ts

--- alan/web/modules/alan-app-shell/alan-app-shell.module.code.tsx
+++ alan/web/modules/alan-app-shell/alan-app-shell.module.code.tsx
@@ -23,7 +23,7 @@ import { useSidebarState } from "akasha/design/interfaces/layout/use-sidebar-sta
 import { createPage } from "akasha/pages/access/modules/create/create.module.code.ts"
 import { NEVER_MATCH_SLUG } from "akasha/pages/access/sentinels/sentinels.module.code.ts"
 import type { ReadonlyJSONValue } from "akasha/pages/core/schema/pages/pages.module.code.ts"
-import { CreatePageDialog } from "akasha/pages/ui/components/create-page-dialog/create-page-dialog.module.code.tsx"
+import { CreatePageDialog } from "akasha/pages/ui/components/modules/create-page-dialog/create-page-dialog.module.code.tsx"
 import { useActiveQuickAddPageType } from "akasha/pages/ui/components/quick-add/use-active-quick-add-page-type/use-active-quick-add-page-type.module.code.ts"
 import { SortableNavs } from "akasha/pages/ui/components/sortable-navs/sortable-navs.module.code.tsx"
 import { useAppNavItems } from "akasha/pages/ui/components/use-app-nav-items/use-app-nav-items.module.code.tsx"
