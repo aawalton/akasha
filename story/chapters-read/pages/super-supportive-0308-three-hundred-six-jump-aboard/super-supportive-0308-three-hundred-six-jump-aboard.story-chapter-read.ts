ad7abbedc08fde4deab158fbdedeb04f46b52c31
Akasha <akasha@alanwalton.com>
2026-09-12 00:04:01 -0600

    royal road sync 1 page(s)

---
+++ story/chapters-read/pages/super-supportive-0308-three-hundred-six-jump-aboard/super-supportive-0308-three-hundred-six-jump-aboard.story-chapter-read.ts
@@ -0,0 +1,18 @@
+import type { StoryChapterRead } from "akasha/story/chapters-read/story-chapter-read.page-type.types.ts"
+
+export const superSupportive0308ThreeHundredSixJumpAboard = {
+  id: "01a09437-21af-7e75-bea9-854c17f555db",
+  type: "story-chapter-read",
+  slug: "super-supportive-0308-three-hundred-six-jump-aboard",
+  position: 308,
+  publishedAt: "2026-09-12",
+  unit: "words",
+  externalId: "3947412",
+  source: "royal-road",
+  title: "THREE HUNDRED SIX: Jump Aboard",
+  story: "story-read/super-supportive",
+  ownLength: 4663,
+  externalLink:
+    "https://www.royalroad.com/fiction/63759/super-supportive/chapter/3947412/three-hundred-six-jump-aboard",
+  prose: "txt",
+} as const satisfies StoryChapterRead
