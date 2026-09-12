ec19f8044441ba980393c7b440adf3370632ee00
Athena <athena@alanwalton.com>
2026-09-11 23:34:46 -0600

    the change kind property sits under changes, which is what has a change kind

--- changes/change.page-type.types.ts
+++ changes/change.page-type.types.ts
@@ -1,3 +1,4 @@
+import type { ChangeKind } from "akasha/changes/properties/change-kind.relation-property.types.ts"
 import type { ChangeMaxCpuSeconds } from "akasha/changes/properties/change-max-cpu-seconds.number-property.types.ts"
 import type { ChangeMode } from "akasha/changes/properties/change-mode.relation-property.types.ts"
 import type { ChangeTargetSubtype } from "akasha/changes/properties/change-target-subtype.relation-property.types.ts"
@@ -5,7 +6,6 @@ import type { ChangeTargetType } from "akasha/changes/properties/change-target-t
 import type { TakesAtMost } from "akasha/changes/properties/takes-at-most.boolean-property.types.ts"
 import type { Temporary } from "akasha/changes/properties/temporary.boolean-property.types.ts"
 import type { Module } from "akasha/code/modules/module.page-type.types.ts"
-import type { ChangeKind } from "akasha/commands/properties/change-kind.relation-property.types.ts"
 
 export type Change = Module & {
   changeKind: ChangeKind
