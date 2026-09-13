2a26202a46ea302eb79c9fb3c3d37006effc03bc
pages-access <pages-access@alanwalton.com>
2026-09-13 08:58:02 -0600

    patchPage(temper-catalog-domain): collectibles
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/catalog/temper-world/temper-catalog-domains/pages/collectibles.temper-catalog-domain.ts
+++ temper/catalog/temper-world/temper-catalog-domains/pages/collectibles.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const collectibles = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-13T14:55:37.936Z",
+  capturedAt: "2026-09-13T14:57:28.555Z",
   generatorRanForVersion: "eso.live.12.0.6.3274791",
   generatorRanForManifestApiVersion: 101050,
 } as const satisfies TemperCatalogDomain
