089d99c3d6f9dabd20837436d7c2ea3eb213d884
pages-access <pages-access@alanwalton.com>
2026-09-13 08:18:33 -0600

    patchPage(temper-catalog-domain): currency
    Written-by: pages-access <pages-access@alanwalton.com>

--- temper/catalog/temper-world/temper-catalog-domains/pages/currency.temper-catalog-domain.ts
+++ temper/catalog/temper-world/temper-catalog-domains/pages/currency.temper-catalog-domain.ts
@@ -8,7 +8,7 @@ export const currency = {
   icon: "file-text",
   apiVersion: "eso.live.12.0.8.3288357",
   manifestApiVersion: 101050,
-  capturedAt: "2026-09-13T14:14:39.752Z",
+  capturedAt: "2026-09-13T14:17:33.371Z",
   generatorRanForVersion: "eso.live.11.3.6.3240040",
   generatorRanForManifestApiVersion: 101049,
   dormant: true,
