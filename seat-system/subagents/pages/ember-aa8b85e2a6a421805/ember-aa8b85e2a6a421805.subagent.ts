5e055bb716595e54dc8854de23588fe04153a075
Akasha <akasha@alanwalton.com>
2026-09-13 10:50:50 -0600

    ember-aa8b85e2a6a421805: a subagent resuming takes up the page it had

---
+++ seat-system/subagents/pages/ember-aa8b85e2a6a421805/ember-aa8b85e2a6a421805.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const emberAa8b85e2a6a421805 = {
+  id: "01a09ba0-5335-7f48-b5ef-8006eb6b10cb",
+  type: "subagent",
+  slug: "ember-aa8b85e2a6a421805",
+  principalSeatName: "ember",
+  assignmentSlug: "initiative/ember-inventory-rules",
+  dispatchedAs: "general-purpose",
+  agentId: "01a09573-2604-7000-98dd-c04bec8e0696--aa8b85e2a6a421805",
+} as const satisfies Subagent
