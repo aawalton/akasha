ac7649eddd77c3b9061e98a334b1ce6133c306e6
01a09573-2604-7000-98dd-c04bec8e0696 <01a09573-2604-7000-98dd-c04bec8e0696@alanwalton.com>
2026-09-13 08:42:42 -0600

    upsertPages(temper-inventory-rule): rule-research-stolen
    Written-by: 01a09573-2604-7000-98dd-c04bec8e0696 <01a09573-2604-7000-98dd-c04bec8e0696@alanwalton.com>

--- temper/progressions/temper-inventory-rules/pages/rule-research-stolen/rule-research-stolen.temper-inventory-rule.ts
+++ temper/progressions/temper-inventory-rules/pages/rule-research-stolen/rule-research-stolen.temper-inventory-rule.ts
@@ -11,7 +11,7 @@ export const ruleResearchStolen = {
   conditions: "jsonl",
   accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   categoryId: "equipment",
-  displayOrder: 40,
+  displayOrder: 41,
   action: "fence-launder",
   active: true,
   updatedAt: "2026-05-04T16:04:31.132Z",
