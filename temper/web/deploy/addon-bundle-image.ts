967ab095c71ee2599515a1fabbb1f36c2c415e8c
Akasha <akasha@alanwalton.com>
2026-09-13 12:04:43 -0600

    the addon bundle image the cluster pulls, named by the content it was built from

--- temper/web/deploy/addon-bundle-image.ts
+++ temper/web/deploy/addon-bundle-image.ts
@@ -1,5 +1,5 @@
 export const ADDON_BUNDLE_CONTENT_HASH =
-  "cfbf635f36d9ee7bb8204744a31347ea5674df25e10513adf516cbfce53b8d01"
+  "2228b5074d12d5b0f6b0c904eaa919b50194c654e486089e418a4be25789b2f9"
 
 export const ADDON_BUNDLE_IMAGE =
-  "registry.registry.svc.cluster.local:5000/cluster/temper-addons:cfbf635f36d9ee7bb8204744a31347ea5674df25e10513adf516cbfce53b8d01"
+  "registry.registry.svc.cluster.local:5000/cluster/temper-addons:2228b5074d12d5b0f6b0c904eaa919b50194c654e486089e418a4be25789b2f9"
