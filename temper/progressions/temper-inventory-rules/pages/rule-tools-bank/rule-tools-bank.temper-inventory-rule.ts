515c8b659abcc4047957a1853e513c95e412bfa5
01a09573-2604-7000-98dd-c04bec8e0696--aa73e91bd5b3e71a2 <01a09573-2604-7000-98dd-c04bec8e0696--aa73e91bd5b3e71a2@alanwalton.com>
2026-09-13 08:45:53 -0600

    upsertPages(temper-inventory-rule): rule-tools-bank
    Written-by: 01a09573-2604-7000-98dd-c04bec8e0696--aa73e91bd5b3e71a2 <01a09573-2604-7000-98dd-c04bec8e0696--aa73e91bd5b3e71a2@alanwalton.com>

--- temper/progressions/temper-inventory-rules/pages/rule-tools-bank/rule-tools-bank.temper-inventory-rule.ts
+++ temper/progressions/temper-inventory-rules/pages/rule-tools-bank/rule-tools-bank.temper-inventory-rule.ts
@@ -11,7 +11,7 @@ export const ruleToolsBank = {
   stockScope: "any-character",
   accountPage: "9ba554f7-cb18-48bb-a709-ec935a895ca7",
   categoryId: "tools",
-  displayOrder: 59,
+  displayOrder: 60,
   action: "stock",
   active: true,
   updatedAt: "2026-07-05T13:20:53.393Z",
