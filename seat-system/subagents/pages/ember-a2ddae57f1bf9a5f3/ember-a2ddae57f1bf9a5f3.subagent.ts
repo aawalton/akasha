e44fc30e5c8f60704051d5bf95104fa2883cc04a
Akasha <akasha@alanwalton.com>
2026-09-13 09:43:10 -0600

    ember-a2ddae57f1bf9a5f3: a subagent states the agent id it acts under

---
+++ seat-system/subagents/pages/ember-a2ddae57f1bf9a5f3/ember-a2ddae57f1bf9a5f3.subagent.ts
@@ -0,0 +1,11 @@
+import type { Subagent } from "akasha/seat-system/subagents/subagent.page-type.types.ts"
+
+export const emberA2ddae57f1bf9a5f3 = {
+  id: "01a09b6f-b8e9-71e1-9b9e-37aa10412e3d",
+  type: "subagent",
+  slug: "ember-a2ddae57f1bf9a5f3",
+  principalSeatName: "ember",
+  assignmentSlug: "initiative/ember-inventory-rules",
+  dispatchedAs: "Explore",
+  agentId: "01a09573-2604-7000-98dd-c04bec8e0696--a2ddae57f1bf9a5f3",
+} as const satisfies Subagent
