04bc61461677bb29ac465f10213e7c1b766c4ee7
Alan Walton <aawalton@gmail.com>
2026-09-14 08:39:32 -0600

    land changes

--- temper/progressions/temper-tasks/pages/lorebooks/lorebooks.temper-task.ts
+++ temper/progressions/temper-tasks/pages/lorebooks/lorebooks.temper-task.ts
@@ -18,5 +18,5 @@ export const lorebooks = {
   lastCompletedAt: "2026-09-13T14:35:08.000Z",
   progress: "jsonl",
   progressTotal: 4220,
-  progressCurrent: 571,
+  progressCurrent: 572,
 } as const satisfies TemperTask
